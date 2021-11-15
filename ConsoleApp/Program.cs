92c34c5bdffb6670f488cda0cf02c34cafec7d35
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2021-11-15 20:49:46 +0200

    some function

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -26,6 +26,58 @@ Console.WriteLine(x * x * x + 2 * x * x + 5 * x + 6);
 decimal y = 3.6M;
 Console.WriteLine(y * y * y + 2 * y * y + 5 * y + 6);
 
+// with math
+
+double z = 4;
+Console.WriteLine(Math.Pow(z, 3)+2*Math.Pow(z,2)+5*z+6);
+
 int incr = 5;
 int result1 = incr++;
 int result2 = ++incr;
+
+Console.WriteLine($"some result {result1} and {result2}");
+
+//boolean
+
+var bool1 = true;
+var bool2 = false;
+
+
+Console.WriteLine($"AND {bool1 &&bool2}");
+Console.WriteLine($"OR {bool1 || bool2}");
+Console.WriteLine($"EQUAL {bool1 == bool2}");
+Console.WriteLine($"NOT bool1 {!bool1}");
+Console.WriteLine($"NOT bool2 {!bool2}");
+Console.WriteLine($"XOR {bool1^bool2}");
+
+//comparison
+
+int first = 1;
+int second=2;
+int third = 1;
+Console.WriteLine($"first >second: {first > second}");
+Console.WriteLine($"first <second: {first < second}");
+Console.WriteLine($"first ==third: {first ==third}");
+Console.WriteLine($"first !=third: {first != third}");
+Console.WriteLine($"first !=second: {first != second}");
+Console.WriteLine($"first >=third: {first >= third}");
+Console.WriteLine($"first >=second: {first >= second}");
+
+//bool
+var bool3 = true;
+var bool4 = false;
+var bool5 = false;
+Console.WriteLine($"AND (bool): {bool3 & bool4 & bool5}");
+Console.WriteLine($"OR (bool): {bool3 | bool4 | bool5}");
+
+//int bool;
+
+int CoolVar1 = 10; 
+int CoolVar2 = 3;
+Console.WriteLine(CoolVar1 & CoolVar2);
+Console.WriteLine(CoolVar1 | CoolVar2);
+Console.WriteLine(CoolVar1 << CoolVar2); //10<<3;
+Console.WriteLine(CoolVar1 >> CoolVar2); //10<<3;
+
+
+
