d7abc6d750f7a04e892c4f5bb62e07ccae10ee76
Oleg <oleg.karman@gmail.com>
2021-11-15 20:49:15 +0200

    codewar task

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string test = "asdasd";
+﻿using System;
 
-char[] testNew = test.ToCharArray();
+class Arge {
+    
+    public static int NbYear(int p0, double percent, int aug, int p) {
+        int totalPeople = p0;
+        int cycle = 0;
+        while (totalPeople < p) {
+          totalPeople = (int)(totalPeople + totalPeople * (percent/100) + aug);
+          cycle+=1;
+        }
+        return cycle;
+    }
+}
