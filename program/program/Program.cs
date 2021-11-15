656f8e0802701742f868a748d6bbfd4b2eafdcea
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2021-11-15 22:35:56 +0200

    Solved the equations

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -1,71 +1,21 @@
-﻿byte aLongLongName = 5;
-Console.WriteLine(aLongLongName);
+﻿//HOME WORK
+Console.WriteLine("Homework:");
+
+//-6*x^3+5*x^2-10*x+15
+int p = 5;
+Console.WriteLine($"При p={p}");
+Console.WriteLine($"-6*p^3+5*p^2-10*p+15={ -6 * Math.Pow(p, 3) + 5 * Math.Pow(p, 2) - 10 * p + 15}");
+
+//abs(x) * sin(x)
+Console.WriteLine($"abs(p)*sin(p)={Math.Abs(p)*Math.Sin(p)}");
+
+//2 * pi * x
+Console.WriteLine($"2*pi*p={2*Math.PI*p}");
+
+int f =4;
+//max(p, f)
+Console.WriteLine($"max(p,f)={Math.Max(p,f)}");
 
 
-byte a = 3;
-byte b = 4;
-int c = a + b;
-Console.WriteLine(c);
-
-int mul = a / b;
-Console.WriteLine(mul);
-
-int i = 42;
-int j = 90;
-Console.WriteLine(j / i);
-Console.WriteLine(j%i);
-
-
-double x = 3.6;
-Console.WriteLine(x * x * x + 2 * x * x + 5 * x + 6);
-
-decimal y = 3.6m;
-Console.WriteLine(y * y * y + 2 * y * y + 5 * y + 6);
-
-double z = 4;
-Console.WriteLine(Math.Pow(z, 3) + 2 * Math.Pow(z, 2) + 5 * z + 6);
-
-
-int incr = 5;
-int result1 = incr++;
-int result2 = ++incr;
-Console.WriteLine($"{result1} , {result2} , {incr}");
-
-
-var bool1 = true;
-var bool2 = false;
-Console.WriteLine($"AND: {bool1&&bool2}");
-Console.WriteLine($"OR: {bool1 || bool2}");
-Console.WriteLine($"EQUAL: {bool1 == bool2}");
-Console.WriteLine($"NOT bool1: {!bool1}");
-Console.WriteLine($"NOT bool2: {!bool2}");
-Console.WriteLine($"XOR: {bool1 ^ bool2}");
-
-
-int first = 1;
-int second = 2;
-int third = 1;
-Console.WriteLine($"first > second: {first>second}");
-Console.WriteLine($"first < second: {first < second}");
-Console.WriteLine($"first == third: {first == third}");
-Console.WriteLine($"first != third: {first != third}");
-Console.WriteLine($"first != second: {first != second}");
-Console.WriteLine($"first > second: {first > second}");
-
-
-int aa = 8;
-int bb = 2;
-
-aa = aa + bb;
-aa +=bb;
-
-Console.WriteLine(aa);
-
-aa = 10;
-bb = 5;
-
-int temp = aa;
-aa = bb;
-bb = temp;
 
 
