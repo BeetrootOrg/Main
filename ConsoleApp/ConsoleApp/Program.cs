84579f458ca9bfb66686c59842e2ea0f8a7151e9
d.misik <misik.dmitriy@gmail.com>
2021-11-15 20:11:30 +0200

    first

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// variable name - 'a long long name'
+byte aLongLongName = 5;
+Console.WriteLine(aLongLongName);
+
+// define two variables
+byte a = 3;
+byte b = 4;
+int c = a + b;
+Console.WriteLine(c);
+
+int mul = a / b;
+Console.WriteLine(mul);
+
+// next divide example
