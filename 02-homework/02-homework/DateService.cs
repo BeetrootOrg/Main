c046dac7e7537d1c05aa73227c1c0636c0be6bf3
Evhenii Tuhushev <tugush3v@gmail.com>
2021-11-17 13:36:40 +0200

    full  02 homework

--- 02-homework/02-homework/DateService.cs
+++ 02-homework/02-homework/DateService.cs
@@ -10,17 +10,17 @@ namespace _02_homework
     {
         
 
-        //public DateService()
-        //{
-        //    var nowDate = DateTime.Now;
-        //    var prevNewYear = new DateTime(nowDate.Year, 1, 1);
-        //    var nextNewYear = new DateTime(nowDate.Year + 1, 1, 1);
-        //    var daysPased = (nowDate - prevNewYear).Days;
-        //    var daysLeft = (nextNewYear - nowDate).Days;
-        //    Console.WriteLine("----------------------Days task----------------");
-        //    Console.WriteLine($"Days passed: {daysPased}");
-        //    Console.WriteLine($"Days left: {daysLeft}");
+        public DateService()
+        {
+            var nowDate = DateTime.Now;
+            var prevNewYear = new DateTime(nowDate.Year, 1, 1);
+            var nextNewYear = new DateTime(nowDate.Year + 1, 1, 1);
+            var daysPased = (nowDate - prevNewYear).Days;
+            var daysLeft = (nextNewYear - nowDate).Days;
+            Console.WriteLine("----------------------Days task----------------");
+            Console.WriteLine($"Days passed: {daysPased}");
+            Console.WriteLine($"Days left: {daysLeft}");
 
-        //}
+        }
     }
 }
--- 02-homework/02-homework/Program.cs
+++ 02-homework/02-homework/Program.cs
@@ -84,120 +84,32 @@ Console.WriteLine(r);
 int d = l / s;
 Console.WriteLine(d);
 
+// result of several math functions
 
-
+Console.WriteLine($"----------result of several math functions----------");
+//-6*x^3+5*x^2-10*x+15
+int x = 10;
+// first method
+Console.WriteLine($"1st method result if x = 10 -6*x^3+5*x^2-10*x+15 = {-6 * x * x * x + 5 * x * x - 10 * x + 15}");
+// second method
+Console.WriteLine($"2nd method result if x = 10 -6*x^3+5*x^2-10*x+15 = {-6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15}");
+//abs(x) * sin(x)
+Console.WriteLine($"abs(x) * sin(x) = {Math.Abs(x) * Math.Sin(x)}");
+//2 * pi * x
+double pi = Math.PI;
+Console.WriteLine($"2 * pi * x = {2 * pi * x}");
+//max(x, y)
+Console.WriteLine(Math.Max(369, 6852));
 
 // DATETIME TASK
 
+Console.WriteLine($"-----------------DateTime Task-----------------");
 var nowDate = DateTime.Now;
 var prevNewYear = new DateTime(nowDate.Year, 1, 1);
 var nextNewYear = new DateTime(nowDate.Year + 1, 1, 1);
 var daysPassed = (nowDate - prevNewYear).Days;
 var daysLeft = (nextNewYear - nowDate).Days;
-Console.WriteLine($"-----------------DateTime Task-----------------");
 Console.WriteLine($"{daysPassed} days passed from New Year");
 Console.WriteLine($"{daysLeft} days left to New Year");
-
-
-
-
-
-
-
-
-
-// variable name 'a Long Long Name'
-byte aLongLongName = 5;
-Console.WriteLine(aLongLongName);
-
-// define two variables
-byte a = 3;
-byte b = 4;
-int c = a + b;
-Console.WriteLine(c);
-
-
-int mul = a / b;
-Console.WriteLine(mul);
-
-int i = 42;
-int j = 90;
-Console.WriteLine(j / i);
-Console.WriteLine(j % i);
-
-// y^3 + 2*x^2 + 5*x + 6 
-
-double x = 3.6;
-Console.WriteLine(x * x * x + 2 * x * x + 5 * x + 6);
-
-// the same with math
-
-double z = 4;
-Console.WriteLine(z * z * z + 2 * z * z + 5 * z + 6);
-Console.WriteLine(Math.Pow(z, 3) + 2 * Math.Pow(z, 2) + 5 * z + 6);
-
-
-decimal y = 3.6m;
-Console.WriteLine(y * y * y + 2 * y * y + 5 * y + 6);
-
-// increment/decrement
-
-int incr = 5;
-int result1 = incr++;
-int result2 = ++incr;
-Console.WriteLine($"{result1}, {result2}, {incr}");
-
-// bool logic
-var bool1 = true;
-var bool2 = false;
-Console.WriteLine($"AND: {bool1 && bool2}");
-Console.WriteLine($"OR: {bool1 || bool2}");
-Console.WriteLine($"EQUAL: {bool1 == bool2}");
-Console.WriteLine($"NOT BOOL1: {!bool1}");
-Console.WriteLine($"NOT BOOL1: {!bool2}");
-Console.WriteLine($"XOR: {bool1 ^ bool2}");
-
-// math equality & comparison
-
-int first = 1;
-int second = 2;
-int third = 1;
-Console.WriteLine($"first > second: {first > second}");
-Console.WriteLine($"first < second: {first < second}");
-Console.WriteLine($"first == second: {first == second}");
-Console.WriteLine($"first != third: {first != third}");
-Console.WriteLine($"first != second: {first != second}");
-Console.WriteLine($"first >= third: {first >= third}");
-Console.WriteLine($"first <= second: {first <= second}");
-
-// bool complex logic
-int coolVar1 = 10;
-int coolVar2 = 3;
-Console.WriteLine(coolVar1 & coolVar2);
-Console.WriteLine(coolVar1 | coolVar2);
-Console.WriteLine(coolVar1 << coolVar2);
-Console.WriteLine(coolVar1 >> coolVar2);
-
-
-// assign
-int aa = 18;
-int bb = 12;
-aa = aa + bb;// 30
-Console.WriteLine(aa);
-aa += bb;// 42 aa + bb
-Console.WriteLine(aa);
-aa %= bb; // 6 aa & bb
-Console.WriteLine(aa);
-aa -= bb; // -6 aa - bb
-Console.WriteLine(aa);
-
-// temp
-aa = 10;
-bb = 5;
-int temp = aa; // assign 10
-aa = bb; //assign 5
-bb = temp; //assign 10
-Console.WriteLine(aa);
-Console.WriteLine(bb);
 
 
