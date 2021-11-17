c72b2aa3ebadfa5ea7c0791de6a1f1ff05ccd0b6
Oleg <oleg.karman@gmail.com>
2021-11-17 19:12:42 +0200

    ; added

--- classwork/Program.cs
+++ classwork/Program.cs
@@ -5,4 +5,4 @@ Console.WriteLine("First task - opposite value of enter number is {0}", -value);
 Console.WriteLine("Second task - always negative value of number is {0}", -Math.Abs(value));
 
 int sqrtOfValue = (int)Math.Sqrt(value);
-Console.WriteLine("Third task - if SQRT - {0}", sqrtOfValue * sqrtOfValue == value)
+Console.WriteLine("Third task - if SQRT - {0}", sqrtOfValue * sqrtOfValue == value);
