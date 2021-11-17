0daf5f9e17840513ce55bde9bb1f0f598e56c3be
TemptationHenry <72220654+TemptationHenry@users.noreply.github.com>
2021-11-17 19:28:13 +0200

    task

---
+++ ConsoleApp03classwork/ConsoleApp03classwork/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Insert value");
+int value = int.Parse(Console.ReadLine());
+
+//1
+Console.WriteLine(-value);
+//2
+Console.WriteLine(-Math.Abs(value));
+//3
+double sqrt = Math.Sqrt(value);
+Console.WriteLine(Math.Round(sqrt)==sqrt);
+
