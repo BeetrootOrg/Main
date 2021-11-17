0f913328dcd11f146653a0f0b08cbeb5c05c29b2
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2021-11-17 20:02:07 +0200

    213

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -1,4 +1,65 @@
-﻿int x = -5;
-int y = x * (-1);
-Console.WriteLine(y);
+﻿string str = "another";
 
+if (str == "Dima")
+{
+    Console.WriteLine("Hello, Dima");
+}
+else if (str == "dima")
+{
+    Console.WriteLine("Hello, Dima");
+}
+else
+{
+    Console.WriteLine($"Good Bye");
+}
+
+
+int a = 10;
+int b;
+
+if (a == 10)
+{
+    b = 10;
+}
+else
+{
+    b = 0;
+}
+
+Console.WriteLine(b);
+
+b = a == 10 ? 10 : 0;
+Console.WriteLine(b);
+
+
+int d;
+switch (a)
+{
+    case 0:
+        d = 42;
+        break;
+    case 1:
+        d = 43;
+        break;
+    case 2:
+        d = 1;
+        break;
+    default:
+        d = 0;
+        break;
+
+}
+
+Console.WriteLine(d);
+
+
+int e = a switch
+{
+    0 => 42,
+    1 => 43,
+    2 => 1,
+    _ => 0,
+
+};
+
+Console.WriteLine(e);
