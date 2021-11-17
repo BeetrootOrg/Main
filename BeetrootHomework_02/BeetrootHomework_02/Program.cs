e3b369f9d057ad3eab88040c676a04a701b6982e
Legion59 <legion.59mk@gmail.com>
2021-11-17 13:11:04 +0200

    Homework-02

---
+++ BeetrootHomework_02/BeetrootHomework_02/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//This some examples with Double type
+double xDouble = 7.56;
+double yDouble = 8;
+Console.WriteLine("This is DOUBLE numbers:");
+Console.WriteLine(-6 * Math.Pow(xDouble, 3) + 5 * Math.Pow(xDouble, 2) - 10 * xDouble + 15);
+Console.WriteLine(Math.Abs(xDouble) * Math.Sin(xDouble));
+Console.WriteLine(2 * Math.PI * xDouble);
+Console.WriteLine(Math.Max(xDouble, yDouble) + "\n\n");
+
+
+
+//This some examples with Float type
+float xFloat = 7.56f;
+float yFloat = 8;
+Console.WriteLine("This is FLOAT numbers:");
+Console.WriteLine(-6 * Math.Pow(xFloat, 3) + 5 * Math.Pow(xFloat, 2) - 10 * xFloat + 15);
+Console.WriteLine(Math.Abs(xFloat) * Math.Sin(xFloat));
+Console.WriteLine(2 * Math.PI * xFloat);
+Console.WriteLine(Math.Max(xFloat, yFloat) + "\n\n");
+
+
+//This some examples with Int type
+int xInt = 7;
+int yInt = 8;
+Console.WriteLine("This is INT numbers:");
+Console.WriteLine(-6 * Math.Pow(xInt, 3) + 5 * Math.Pow(xInt, 2) - 10 * xInt + 15);
+Console.WriteLine(Math.Abs(xInt) * Math.Sin(xInt));
+Console.WriteLine(2 * Math.PI * xInt);
+Console.WriteLine(Math.Max(xInt, yInt) + "\n\n");
+
+
+//This some examples with Decimal type
+int xDecimal = 7;
+int yDecimal = 8;
+Console.WriteLine("This is DECIMAL numbers:");
+Console.WriteLine(-6 * Math.Pow(xDecimal, 3) + 5 * Math.Pow(xDecimal, 2) - 10 * xDecimal + 15);
+Console.WriteLine(Math.Abs(xDecimal) * Math.Sin(xDecimal));
+Console.WriteLine(2 * Math.PI * xDecimal);
+Console.WriteLine(Math.Max(xDecimal, yDecimal) + "\n\n");
+
+
+//Some work with Decimal
+//In Interstellar main character was on planet where time was very slowed down, and 1 hour on those planet equal 7 years on Earth.
+//Also if you move at a speed close to the speed of light, your time will be slower too. Let's count on what speed our time will be slow like on planet from Interstellar.
+
+const int C = 299792458; //light speed m/s
+decimal t0 = 3600;       // 1 hour in second
+decimal t = 220924800;   // 7 years in second
+
+decimal v = C*Sqrt(1.0m - Decimal.Divide(t0, t) * Decimal.Divide(t0, t));
+
+Console.WriteLine($"Our speed need to be: {Decimal.Round(v, 9)} m/s \n\n");
+
+static decimal Sqrt(decimal x)
+{
+    decimal a = 0.9m;
+
+    for (int i = 0; i <= 10; i++)
+    {
+        a = 0.5m * (a + x / a);
+    }
+    return a;
+}
+
+//How many days are left until the new year???
+DateTime newYear = new DateTime(2022, 01, 01);
+TimeSpan diff = newYear - DateTime.Now;
+Console.WriteLine($"До Нового Года осталось: {diff.Days} дней");
---
+++ FirstBeetRootConsole/FirstBeetRootConsole/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+int LongName = 5;
+Console.WriteLine(LongName);
+
+byte a = 3;
+byte b = 4;
+byte c = (byte)(a + b);
+Console.WriteLine(c);
+
+
+int mul = a / b;
+Console.WriteLine(mul);
+
+int i = 42;
+int j = 90;
+Console.WriteLine(j / i);
+Console.WriteLine(i % j);
+
+double x = 3.6;
+Console.WriteLine(x * x * x + 2 * x * x + 5 * x + 6);
+
+decimal y = 3.6m;
+Console.WriteLine(y * y * y + 2 * y * y + 5 * y + 6);
+
+double z = 3.6;
+Console.WriteLine(Math.Pow(z, 3) + 2 * Math.Pow(z, 2) + 5 * z + 6);
+
+int incr = 5;
+int result1 = incr++;
+int result2 = ++incr;
+Console.WriteLine($"{result1}, {result2}, {incr}");
+
+
+bool bool1 = true;
+bool bool2 = false;
+Console.WriteLine($"AND: {bool1 && bool2}");
+Console.WriteLine($"OR: {bool1 || bool2}");
+Console.WriteLine($"EQUAL: {bool1 == bool2}");
+Console.WriteLine($"NOT: {!bool1}");
+Console.WriteLine($"NOT: {!bool2}");
+Console.WriteLine($"XOR: {bool1 ^ bool2}");
+
+
+int first = 1;
+int second = 2;
+int third = 1;
+Console.WriteLine($"first > second: {first > second}");
+Console.WriteLine($"first < second: {first < second}");
+Console.WriteLine($"first == third: {first == third}");
+Console.WriteLine($"first != third: {first != third}");
+Console.WriteLine($"first != second: {first != second}");
+Console.WriteLine($"first >= third: {first >= third}");
+Console.WriteLine($"first >= second: {first >= second}");
+
+
+int aa = 8;
+int bb = 2;
+
+aa = aa + bb;
+aa += bb;
+Console.WriteLine(aa);
