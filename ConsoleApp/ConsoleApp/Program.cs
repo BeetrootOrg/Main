89b354557ec85cca736a20dc3edddd7691899e48
oleksandrmartynenko <81743146+oleksandrmartynenko@users.noreply.github.com>
2021-11-17 18:33:00 +0200

    hw upload

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,85 +1,28 @@
-﻿byte aLOngLongName = 5;
-Console.WriteLine(aLOngLongName);
+﻿try
+{
+    Console.WriteLine("Please define X variable");
+    int x = Int32.Parse(Console.ReadLine());
+    Console.WriteLine("Please define Y variable");
+    int y = Int32.Parse(Console.ReadLine());
+    int sum = 0;
 
-byte a = 3;
-byte b = 4;
-int c = a + b;
-Console.WriteLine(c);
+    if (x == y)
+    {
+        sum = x;
+    }
+    else
+    {
 
-int mul = a / b;
-Console.WriteLine(mul);
-
-int i = 42;
-int j = 90;
-Console.WriteLine(j / i);
-Console.WriteLine(j % i);
-
-double x = 3.6;
-Console.WriteLine(x*x*x+2*x*x+5*x+6);
-
-decimal y = 3.6m;
-Console.WriteLine(y*y*y+2*y*y+5*y+6);
-
-double z = 4;
-Console.WriteLine(Math.Pow(z, 3) + 2*Math.Pow(z, 2)+5*z+6);
-
-int incr = 5;
-int result1 = incr++;
-int result2 = ++incr;
-Console.WriteLine($"{result1}, {result2}, {incr}");
-
-var bool1 = true;
-var bool2 = false;
-Console.WriteLine($"AND: {bool1 && bool2}");
-Console.WriteLine($"AND (bool): {bool1 & bool2}");
-Console.WriteLine($"OR: {bool1 || bool2}");
-Console.WriteLine($"OR (bool): {bool1 | bool2}");
-Console.WriteLine($"EQUAL: {bool1 == bool2}");
-Console.WriteLine($"NOT: {!bool1}");
-Console.WriteLine($"NOT: {!bool2}");
-Console.WriteLine($"XOR: {bool1 ^ bool2}");
-
-int first = 1;
-int second = 2;
-int third = 1;
-Console.WriteLine($"first > second: {first > second}");
-Console.WriteLine($"first < second: {first < second}");
-Console.WriteLine($"first == third: {first == third}");
-Console.WriteLine($"first != third: {first != third}");
-Console.WriteLine($"first != second: {first != second}");
-Console.WriteLine($"first >= third: {first >= third}");
-Console.WriteLine($"first >= second: {first >= second}");
-
-var bool3 = true;
-var bool4 = false;
-var bool5 = false;
-Console.WriteLine($"AND (bool): {bool3 & bool4 & bool5}");
-Console.WriteLine($"OR (bool): {bool3 | bool4 | bool5}");
-
-int coolVar1 = 10;
-int coolVar2 = 3;
-Console.WriteLine(coolVar1 & coolVar2);
-Console.WriteLine(coolVar1 | coolVar2);
-Console.WriteLine(coolVar1 << coolVar2);
-Console.WriteLine(coolVar1 >> coolVar2);
-
-int aa = 8;
-int bb = 2;
-aa = aa + bb;
-Console.WriteLine(aa);
-aa += bb;
-aa -= bb;
-
-aa *= bb;
-
-aa /= bb;
-aa %= bb;
-Console.WriteLine(aa);
-
-aa = 10;
-bb = 5;
-int temp = aa;
-aa = bb;
-bb = temp;
-Console.WriteLine(aa);
-Console.WriteLine(bb);
+        int minVal = Math.Min(x, y);
+        int maxVal = Math.Max(x, y);
+        for (int i = minVal; i <= maxVal; i++)
+        {
+            sum += i;
+        }
+    }
+    Console.WriteLine($"The sum of the integers between these two numbers is {sum}");
+}
+catch (FormatException)
+{
+    Console.WriteLine($"Invalid input");
+}
