2c6967a73455cfa272147d5b7fe5c9f9c44d4e07
d.misik <misik.dmitriy@gmail.com>
2021-11-17 20:46:40 +0200

    parse

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -173,3 +173,10 @@ for (int i = 0; i < 10; ++i)
 Console.WriteLine("Write your name:");
 string? name = Console.ReadLine();
 Console.WriteLine($"Hello, {name}");
+
+Console.WriteLine("Write number:");
+string? strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+num = Convert.ToInt32(strNum); // the same as above
+Console.WriteLine($"{num}^2={num * num}");
+Console.WriteLine($"{num}^2.5={Math.Pow(num, 2.5)}");
