3a695155e787b024101eaf2de4364aabe3b0f388
Ivan Chicha <94204093+VanSSHH@users.noreply.github.com>
2021-11-17 20:03:15 +0200

    sdfs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,57 @@
-﻿// first task
-var a = -4;
-Console.WriteLine(a* -1);
+﻿string str = "dima";
 
-// second task
-var b = -5;
-Console.WriteLine();
+if (str == "Dima" || str == "dima")
+{
+    Console.WriteLine("Hello Dima");
+}
+else
+{
+    Console.WriteLine("Good Bye");
+}
+
+int a = 10;
+int b;
+
+if (a == 10)
+{
+    b = 10;
+}
+else
+{
+    b = 0;
+}
+
+b = a == 10 ? 10 :0;
+Console.WriteLine(b);
+
+
+int c = a == 10 ? 10 : 0;
+
+int d;
+a = 3;
+switch (a)
+{
+    case 0:
+        d = 42;
+        break;
+    case 1:
+        d = 43;
+        break;
+    case 2:
+        d = 1;
+        break;
+    default:
+        d = 0;
+        break;
+}
+Console.WriteLine(d);
+
+int e = a switch
+{
+    0 => 42,
+    1 => 43,
+    2 => 1,
+    _ => 0,
+
+};
+Console.WriteLine(d);
