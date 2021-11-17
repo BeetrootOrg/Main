6a7c99377f2d8fe43c22d0d6a8980c8222a044e9
Mour-Yam <70475271+Mour-Yam@users.noreply.github.com>
2021-11-17 19:15:04 +0200

    i.safontev/other/03-kata

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,39 +7,19 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
 
-            double x = Convert.ToDouble(Console.ReadLine());
-            // x^3+2*x^2+5*x+6
-            //Console.WriteLine(x * x * x + 2 * x * x + 5 * x + 6);
-            Console.WriteLine(Math.Pow(x, 2) + 2 * Math.Pow(x, 2) + 5 * x + 6);
-
-            int incr = 5;
-            int res1 = ++incr;//incr=6 res1=6
-            int res2 = incr++;//res2=6 incr=7
-
-            //bool logic
-            bool bool1 = true;
-            var bool2 = false;
-            Console.WriteLine($"And: {bool1 && bool2}");
-            Console.WriteLine($"Or: {bool1 || bool2}");
-            Console.WriteLine($"Equal: {bool1 == bool2}");
-            Console.WriteLine($"Not Equal: {bool1 != bool2}");
-            Console.WriteLine($"Xor: {bool1 ^ bool2}");
-
-            //math equality & comparison
-            int first = 1;
-            int second = 2;
-            Console.WriteLine($"{first} > {second}: {first > second}");
-            Console.WriteLine($"{first} <= {second}: {first <= second}");
-            Console.WriteLine($"{first} == {second}: {first == second}");
-            Console.WriteLine($"{first} != {second}: {first != second}");
-
-            //int bool logic
-            int coolVar1 = 3; //in bin = 1010
-            int coolVar2 = 10;//in bin = 0011
-                              //and    = 0010 -> 2
-                              //or     = 1011 -> 11
-            Console.WriteLine(coolVar1 & coolVar2);
-            Console.WriteLine(coolVar1 | coolVar2);
+            /*1. написати код, що буде виводити у консоль число з протилежним знаком, наприклад для 5 результат будет -5, для -1 результат буде 1
+              2. написати код, що буде виводити у консоль число зі знаком мінус, наприклад для 5 результат будет -5, для -1 результат буде -1
+              3. написати код, що буде виводити у консоль чи є число квадратом якогось цілого числа, наприклад 25 -> true (5^2), 24 -> false,
+                 9 -> true (3^2), 1 -> true (1^2), 0 -> true (0^2), -1 -> false
+            */
+            //1
+            Console.WriteLine("Enter the x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(-x);
+            //2
+            Console.WriteLine(-Math.Abs(x));
+            //3
+            Console.WriteLine(Math.Sqrt(x) * Math.Sqrt(x) == x) ;
 
         }
     }
