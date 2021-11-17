50942877cba9b15fa296218349f96a59006575c0
Viktor-Prokhorov <vicmmx@gmail.com>
2021-11-17 15:13:06 +0200

    forgot to copy a few lines

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,14 @@
-﻿int x = 3;
+﻿int x = 2;
 byte a = 34;
 short b = 77;
 int c = 6;
 long d = 4567;
 bool e = true;
-char f = 'P';
+char f = 'R';
 float g = 45.65f;
 double h = 7665d;
 decimal i = 52.34m;
-string j = "russia";
+string j = "ussia";
 float y1;
 double y2;
 double y3;
@@ -20,9 +20,9 @@ Console.WriteLine(y2);
 Console.WriteLine(y3);
 Console.WriteLine(Math.Max(Convert.ToSingle(i), g));
 Console.WriteLine(f + j);
-Console.WriteLine(b + c);
-//by the way the bitwise "not" exists 
-Console.WriteLine(Convert.ToString(a, 2));
-Console.WriteLine(Convert.ToString(~a, 2));
-//New Year is soon!!!
+
 DateTime date1 = DateTime.Now;
+DateTime date2 = new DateTime(2022, 1, 1, 0, 0, 0);
+TimeSpan interval = date2 - date1;
+
+Console.WriteLine("{1} {0}", "days left to New Year", (interval.Days).ToString());
