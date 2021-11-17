813c7f03e780129218c573a3af707d1fe48d9994
d.misik <misik.dmitriy@gmail.com>
2021-11-17 19:51:20 +0200

    switch

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -27,3 +27,29 @@ Console.WriteLine(b);
 // second option
 b = a == 10 ? 10 : 0;
 Console.WriteLine(b);
+
+// third option
+int c = a == 10 ? 10 : 0;
+
+// impossible
+// str == "Dima" ? Console.WriteLine("Hello, Dima") : Console.WriteLine("Hello, Another");
+
+int d;
+a = 3;
+switch (a)
+{
+    case 0:
+        d = 42;
+        break;
+    case 1:
+        d = 43;
+        break;
+    case 2:
+        d = 1;
+        break;
+    default:
+        d = 0;
+        break;
+}
+
+Console.WriteLine(d);
