9aa5cb35c068926d2844602b78e74d5bb52dae07
Oleg <oleg.karman@gmail.com>
2021-11-18 19:29:07 +0200

    o.karmanskyi/homework/4-methods

---
+++ 04homework/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Homework4
+{
+    using System;
+
+    class Homework4
+    {
+        static int MaxValue(int x, int y)
+        {
+            return Math.Max(x, y);
+        }
+
+        static int MaxValue(params int[] numbers)
+        {
+            return numbers.Max();
+        }
+
+        static int MinValue(int x, int y)
+        {
+            return Math.Min(x, y);
+        }
+
+        static int MinValue(params int[] numbers)
+        {
+            return numbers.Min();
+        }
+
+        static bool TrySumIfOdd(int x, int y)
+        {
+            int xValue = x, yValue = y;
+            long sum = xValue;
+            while (xValue != yValue)
+            {
+                sum += xValue > yValue ? --xValue : ++xValue;
+            }
+            return sum % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxValue(2,3,4,8,2));
+            Console.WriteLine(TrySumIfOdd(6, 9));
+        }
+    }
+}
