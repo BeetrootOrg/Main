67fdc9e66182730d00f0e6dc150f16e2665cf8e9
SilaSamuraya <arthurgolban@gmail.com>
2021-11-20 10:25:12 +0200

    фибо(полностью)

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -1,8 +1,9 @@
-﻿int n = 4;
+﻿int n = 2;
 int f1=0, f2=1;
 int sum = 0;
-
-for (int i = 2; i < n; i++)
+if (n == 1) sum = f1;
+else if (n == 2) sum = f2;
+for (int i = 2; i <= n; i++)
 {
     sum = f1 + f2;
     f1 = f2;
