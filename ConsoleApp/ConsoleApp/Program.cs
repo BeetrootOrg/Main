e584cc360240c83e714f8e49df0738259bf8c532
d.misik <misik.dmitriy@gmail.com>
2021-11-20 11:12:25 +0200

    ref

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -9,9 +9,29 @@ namespace ConsoleApp
             double result = FuncWithABigName(5.5);
             Console.WriteLine(result);
             Concat("Hello", "Dima");
+
+            int ai = 0;
+            Increment(ai);
+            Console.WriteLine($"In Main: {ai}");
+
+            ai = 0;
+            IncrementRef(ref ai);
+            Console.WriteLine($"In Main: {ai}");
         }
 
         static double FuncWithABigName(double x) => x * x * x + 5 * x * x + 6;
         static void Concat(string str1, string str2) => Console.WriteLine($"{str1}, {str2}");
+
+        static void Increment(int i)
+        {
+            ++i;
+            Console.WriteLine($"In Increment: {i}");
+        }
+
+        static void IncrementRef(ref int i)
+        {
+            ++i;
+            Console.WriteLine($"In IncrementRef: {i}");
+        }
     }
 }
