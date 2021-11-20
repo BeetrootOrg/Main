d0c60f839473363a100681401e1aebee1ba79005
DMSamael <dmitriy.sulym@gmail.com>
2021-11-20 10:56:01 +0200

    concat

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            double result = FuncWithABigName(5.5);
+            System.Console.WriteLine($"{result}");
+            ConCat("Hello", "Dima");
+        }
+
+        static double FuncWithABigName(double x)
+        {
+            return x * x * x + 5 * x * x + 6;
+        }
+
+        static void ConCat(string str1,string str2)
+        {
+            System.Console.WriteLine(str2+" , "+str1);
+        }
+    }
+}
