1752775dffb5dece97678b481d01b7e0d1a35e4e
Oleg <oleg.karman@gmail.com>
2021-11-20 15:43:40 +0200

    TryMulIfDividedByThree method added

--- 04homework/Program.cs
+++ 04homework/Program.cs
@@ -4,19 +4,10 @@
 
     class Homework4
     {
-        static int MaxValue(int x, int y)
-        {
-            return Math.Max(x, y);
-        }
 
         static int MaxValue(params int[] numbers)
         {
             return numbers.Max();
-        }
-
-        static int MinValue(int x, int y)
-        {
-            return Math.Min(x, y);
         }
 
         static int MinValue(params int[] numbers)
@@ -24,16 +15,24 @@
             return numbers.Min();
         }
 
-        static long TrySumIfOdd(int x, int y, out bool isOdd)
+        static int TrySumIfOdd(int x, int y, out bool isOdd)
         {
             int xValue = x, yValue = y;
-            long sum = xValue;
+            int sum = xValue;
             while (xValue != yValue)
             {
                 sum += xValue > yValue ? --xValue : ++xValue;
             }
             isOdd = sum % 2 != 0;
             return sum;
+        }
+
+        static bool TryMulIfDividedByThree(int x, int y, out int multiplication)
+        {
+            bool canDivideX = x % 3 == 0;
+            bool canDivideY = y % 3 == 0;
+            multiplication = canDivideX && canDivideY ? x * y : 0;
+            return canDivideX || canDivideY;
         }
 
         //Is this from CodeWars?
@@ -44,10 +43,15 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine(MaxValue(2,3,4,8,2));
+            Console.WriteLine(MaxValue(2, 3, 4, 8, 2));
+            Console.WriteLine(MinValue(2, 3, 4, 8, -2));
 
             TrySumIfOdd(6, 9, out bool isOdd);
             Console.WriteLine((isOdd) ? "The sum is odd" : "The sum is even");
+
+            bool canDivideAtLeastOne = TryMulIfDividedByThree(5, 9, out int multi);
+            Console.WriteLine((canDivideAtLeastOne) ? "At least one number can be divided by 3" : "No numbers can be divided by 3");
+            Console.WriteLine($"The multiplication of numbers is {multi}");
 
             Console.WriteLine(Repeat("test", 5));
         }
