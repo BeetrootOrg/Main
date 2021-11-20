be353828aca87329b1d14c3c47d7a3b1382c1968
Oleg <oleg.karman@gmail.com>
2021-11-20 11:12:34 +0200

    classwork

---
+++ 04classwork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Classwork4
+{
+
+    class Classwork4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+
+            int ai = 0;
+            Increment(ai);
+            Console.WriteLine(ai);
+        }
+
+        static void Increment(int i)
+        {
+            ++i;
+            Console.WriteLine(i);
+        }
+
+        static void IncrementRef(ref int i)
+        {
+            ++i;
+            Console.WriteLine(i);
+        }
+    }
+}
