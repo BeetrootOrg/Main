faae7d08b5437a9127931361a94931eb21a2a028
DMSamael <dmitriy.sulym@gmail.com>
2021-11-20 10:15:23 +0200

    1

---
+++ Kata-04/Program.cs
@@ -0,0 +1,18 @@
+﻿int x = 5;
+int result = 0;
+int prm;
+
+if (x == 0) { result = 0; }
+else if (x == 1) { result = 1; }
+else
+{
+    for(int i = 2;i <= x; i++)
+    {
+        int y = i - 1;
+        int z = i;
+        if (result == 0) result = y;
+        result = result * z;
+    }
+
+}
+Console.WriteLine(result);
