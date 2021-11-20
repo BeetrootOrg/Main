a2a80041979b93bbbd74f20d2d214f0e4ecd32a8
Anastasiia Koronenko <koronenko.nastya@gmail.com>
2021-11-20 08:54:24 +0200

    dil

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// vriable name - 'a long long name'
+byte alongLongName = 5;
+
+Console.WriteLine(alongLongName);
+
+// define two variables
+
+byte a = 3;
+byte b = 4;
+int c = a + b;
+
+Console.WriteLine(c);
+
+int mul1 = a / b;
+Console.WriteLine(mul);
+
