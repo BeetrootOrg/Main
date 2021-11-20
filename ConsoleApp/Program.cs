7fdeb24d4d3f0408dbd8ae47ee687f1515228339
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2021-11-20 08:27:05 +0200

    end

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,39 +1,49 @@
-﻿/*
-C# Conditions and If Statements
-Less than: a < b
-Less than or equal to: a <= b
-Greater than: a > b
-Greater than or equal to: a >= b
-Equal to a == b
-Not Equal to: a != b
-*/
-
-//first equation
-
-int x = 5;
-int y = 11;
-
-int sumResult = 0;
-
-while (x < y)
+﻿int num;
+Boolean success;
+string strNum;
+do
 {
-    sumResult = sumResult + x + y;
-    x++;
+    Console.WriteLine("Write value of X, please (it must be number):");
+    strNum = Console.ReadLine();
+    success = Int32.TryParse(strNum, out num);
+} while (!success);
+int x,y;
+
+x = Convert.ToInt32(strNum);
+
+do
+{
+    Console.WriteLine("Write value of Y, please (it must be number):");
+    strNum = Console.ReadLine();
+    success = Int32.TryParse(strNum, out num);
+} while (!success) ;
+
+y = Convert.ToInt32(strNum);
+
+Console.WriteLine($"X is {x}, Y is {y}");
+
+if (x > y) 
+{
+    int tempInt = x;
+    x = y;
+    y = tempInt;
 
 }
-Console.WriteLine($"full sum of iteration x and y is {sumResult}");
+
+int sumResult =0;
+int z = x;
+do
+{
+    sumResult = sumResult + z++;
+
+} while (z <= y);
+
+Console.WriteLine($" sum of iteration X till Y by operator do...while is {sumResult}");
 
 sumResult = 0;
-for (int z = 0; z < y; z++)
+for (z=x; z<= y; z++)
 {
-    sumResult = sumResult + z + y;
+    sumResult = sumResult +z;
 }
-Console.WriteLine($"some sum of iteration z and y {sumResult}");
+Console.WriteLine($"sum x and y be operator for is {sumResult}");
 
-Console.WriteLine("write some X");
-double tX = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("You need to write some Y");
-double tY = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine($"your x is {tX} and your y is {tY}");
