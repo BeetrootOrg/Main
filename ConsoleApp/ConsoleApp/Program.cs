76361034a09d4ea4d3bf7ea5a86d60d365c4f76e
d.misik <misik.dmitriy@gmail.com>
2021-11-20 11:21:12 +0200

    addifodd

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -14,9 +14,15 @@ namespace ConsoleApp
             Increment(ai);
             Console.WriteLine($"In Main: {ai}");
 
-            ai = 0;
             IncrementRef(ref ai);
             Console.WriteLine($"In Main: {ai}");
+
+            string str = "Hello";
+            Add(str, ", Dima");
+            Console.WriteLine($"In Main: {str}");
+
+            AddRef(ref str, ", Dima");
+            Console.WriteLine($"In Main: {str}");
         }
 
         static double FuncWithABigName(double x) => x * x * x + 5 * x * x + 6;
@@ -33,5 +39,21 @@ namespace ConsoleApp
             ++i;
             Console.WriteLine($"In IncrementRef: {i}");
         }
+
+        static void Add(string str1, string str2)
+        {
+            str1 += str2;
+            Console.WriteLine($"In Add: {str1}");
+        }
+
+        static void AddRef(ref string str1, string str2)
+        {
+            str1 += str2;
+            Console.WriteLine($"In AddRef: {str1}");
+        }
+
+        static int AddIfOdd(int i1, int i2) => i1 % 2 == 0 && i2 % 2 == 0
+            ? i1 + i2
+            : 0;
     }
 }
