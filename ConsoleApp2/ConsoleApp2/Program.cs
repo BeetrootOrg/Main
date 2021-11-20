aad1b6f2644c6b4382fa73a916bb47c865ae10b3
Anastasiia Koronenko <koronenko.nastya@gmail.com>
2021-11-20 10:58:22 +0200

    func string

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,10 +7,16 @@ namespace ConsoleApp
         {
             double result = FuncWithBigName(5.5);
             Console.WriteLine(result);
+            Concat("Hello", "Dima");
         }
 
         static double FuncWithBigName(double x) {
             return x * x * x + 5 * x * x + 6;
         }
+
+        // => FuncWithBigName(double x) =>  x * x * x + 5 * x * x + 6;
+
+        static void Concat(string str1, string str2) => Console.WriteLine($"{str1}, {str2}");
+
     }
 }
