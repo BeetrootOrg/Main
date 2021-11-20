bc6d3c9dafcf824578a50517cc59108294a01276
TemptationHenry <72220654+TemptationHenry@users.noreply.github.com>
2021-11-20 10:15:41 +0200

    Fiba

---
+++ ConsoleApp04classwork/ConsoleApp04classwork/Program.cs
@@ -0,0 +1,17 @@
+﻿int value = 20;
+int prev = 0;
+int current = 1;
+int temp;
+
+Console.WriteLine(prev);
+
+for (int i = 0; i <= value; i++)
+{
+    Console.WriteLine(current);
+    temp = current;
+    current = current + prev;
+    prev = temp;
+}
+Console.ReadKey();
+    
+
