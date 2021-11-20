5dd2a80f9b151664cc810ef7feae0a980ac252c9
Anastasiia Koronenko <koronenko.nastya@gmail.com>
2021-11-20 09:06:47 +0200

    increment/decrement

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,6 +14,24 @@ int c = a + b;
 
 Console.WriteLine(c);
 
-int mul1 = a / b;
+int mul = a / b;
 Console.WriteLine(mul);
 
+// next divide example
+
+int i = 42;
+int j = 90;
+Console.WriteLine(j / i);
+Console.WriteLine(j % i);
+
+// x^3+2*x^2+5*x+6
+
+double x = 3.6;
+Console.WriteLine(x*x*x+2*x*x+5*x+6);
+
+// the same with decimal
+
+decimal y = 3.6m;
+Console.WriteLine(y*y*y+2*y*y+5*y+6);
+
+// increment/decrement
