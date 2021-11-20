05f74690dc052761a62d18431cdf33efe74f2c83
Viktor-Prokhorov <vicmmx@gmail.com>
2021-11-20 09:40:43 +0200

    homework

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,3 +1,3 @@
-﻿int x = 1;
-int y = 3;
+﻿int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine((Enumerable.Range(Math.Min(x,y), Math.Max(x, y) - Math.Min(x, y) + 1)).Sum());
