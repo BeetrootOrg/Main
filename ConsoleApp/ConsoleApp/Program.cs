5bc8f06bbc43ab4077f65f7c838e283e865c5076
d.misik <misik.dmitriy@gmail.com>
2021-11-20 10:55:53 +0200

    concat

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -8,8 +8,10 @@ namespace ConsoleApp
         {
             double result = FuncWithABigName(5.5);
             Console.WriteLine(result);
+            Concat("Hello", "Dima");
         }
 
         static double FuncWithABigName(double x) => x * x * x + 5 * x * x + 6;
+        static void Concat(string str1, string str2) => Console.WriteLine($"{str1}, {str2}");
     }
 }
