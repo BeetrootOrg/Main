e4f4a9634bd6a28b04eec76595cd4a70d23b38d9
Legion59 <legion.59mk@gmail.com>
2021-11-21 19:36:47 +0200

    Check

--- BeetrootClasswork_04/BeetrootClasswork_04/Program.cs
+++ BeetrootClasswork_04/BeetrootClasswork_04/Program.cs
@@ -1,19 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 
-int n = 20;
-int sum = 0;
-int x0 = 0;
-int x1 = 1111;
-
-Console.WriteLine(x0);
-Console.WriteLine(x1);
-
-for (int i = 0; i < n - 2; i++)
+namespace ConsoleApp
 {
-    sum = x0 + x1;
-    x0 = x1;
-    x1 = sum;
-    Console.WriteLine(sum);
-}
-Console.WriteLine($"{x1} / {x0} = {(double)x1 / (double)x0}");
+    public class Program
+    {
+        static void Main()
+        {
+            /*
+            int n = 20;
+            int sum = 0;
+            int x0 = 0;
+            int x1 = 1111;
 
+            Console.WriteLine(x0);
+            Console.WriteLine(x1);
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                sum = x0 + x1;
+                x0 = x1;
+                x1 = sum;
+                Console.WriteLine(sum);
+            }
+            Console.WriteLine($"{x1} / {x0} = {(double)x1 / (double)x0}");
+            */
+
+            double result = FunkWithABigName(5.5);
+            Console.WriteLine($"F(5.5) = {result}");
+        }
+        static double FunkWithABigName(double x) => x * x * x + 5 * x * x + 6;
+        }
+    }
+}
