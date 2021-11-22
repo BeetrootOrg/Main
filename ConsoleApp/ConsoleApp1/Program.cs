e37229632c21c2082d1d4dd99c5e64f8415e80c8
DMSamael <dmitriy.sulym@gmail.com>
2021-11-22 19:14:37 +0200

    1

---
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(Gcd(25,10));
+
+static int RoundToNext5(int n)
+{
+	int x = 0;
+    while ((n + x) % 5 != 0)
+    {
+        x++;
+    }
+    return n + x;
+}
+
+static int Gcd(int a, int b)
+{
+    int c = a > b ? a : b; 
+    while(a%c !=0 && b%c !=0 )
+    {
+        c--;
+    }
+    return c;
+}
