4d9a70a79d5ba7959e509d3dff2d8e6fb58758f2
Oleg <oleg.karman@gmail.com>
2021-11-22 19:14:13 +0200

    second task

--- other05/Program.cs
+++ other05/Program.cs
@@ -3,3 +3,15 @@
     return Math.Ceiling((number + 1) / 5) * 5;
 }
 
+int SecondTask (int a, int b)
+{
+    int dec = 0;
+    for (int i = Math.Min(a, b); i > 0; i--)
+    {
+        if (dec != 0 && a % i == 0 && b % i == 0)
+        {
+            dec = i;
+        }
+    }
+    return dec;
+}
