44686ef3f5b4881c27f298b812b10d28ebb24e37
d.misik <misik.dmitriy@gmail.com>
2021-11-22 20:20:20 +0200

    now

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -8,6 +8,11 @@ namespace ConsoleApp
         {
             DateTime date = new DateTime(2021, 10, 12, 15, 05, 10);
             Console.WriteLine(date);
+            Console.WriteLine($"{date:f}");
+            Console.WriteLine($"{date:yyyy-MM/dd}");
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
         }
     }
 }
