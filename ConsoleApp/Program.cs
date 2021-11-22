8c5c47389a907e99390b0fc9bb5bcb2b85373ce8
a.tkachenko <your_email@whatever.com>
2021-11-22 21:46:32 +0200

    update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,73 +3,66 @@
     using System;
     class ConsoleApp
     {
-        static int GetMax(int a, int b, int c)
+
+        // 1.округлити до наступного цілого, що ділиться на 5, наприклад 0-> 5; 1-> 5; 5-> 10; 6-> 10; 9-> 10; 10-> 15
+        static int RoundToNext5(int n)
         {
-            return Math.Max(Math.Max(a, b), c);
+            return ((n / 5) + 1) * 5;
         }
-        static int GetMin(int a, int b, int c)
+        // 2.найбільший спільний дільник для a &b, наприклад, (30, 12)-> 6; (8, 9)-> 1, (1, 1)-> 1
+        static int Gcd(int a, int b)
         {
-            return Math.Min(Math.Min(a, b), c);
+            int min = Math.Min(a, b);
+            int max = Math.Max(a, b);
+            int result = 0;
+            int i = min;
+            Console.WriteLine("Min: {0}", min);
+            do
+            {
+                if ((min % i == 0) && (max % i == 0))
+                {
+                    result = i;
+                    break;
+                }
+                else
+                {
+                    result = 1;
+                }
+            } while (--i > 0);
+            return result;
         }
-        static bool TrySumifOdd(int a, int b, out int result)
-        {
-            result = a + b;
-            if ((result & 1) == 1)
-            {                
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        static void ShowSum(bool executResult, int sum)
-        {
-            if(executResult == true)
-            {
-                Console.WriteLine("{0} // Sum is Odd", sum);
-            }
-            else
-            {
-                Console.WriteLine("{0} // Sum is Even", sum);
-            }
-        }
-        static string Repeat(string str, int repeatTimes)
-        {
-            if (repeatTimes-- > 0)
-            {
-                return str + Repeat(str, repeatTimes);
-            }
-            else
-            {
-                return null;
-            }
-        }
+
+        //static int Gcd(int a, int b)
+        //{
+        //    for (int i = Math.Min(a, b); i > 1; --i)
+        //    {
+        //        if (a % i == 0 && b % i == 0)
+        //        {
+        //            return i;
+        //        }
+        //    }
+
+        //    return 1;
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("\r\n a.tkachenko/homework/04-methods \r\n"); 
-            
-            Console.WriteLine("Get Max/Min:");
-            int a = -5;
-            int b = 3;
-            int c = 15;
-            Console.WriteLine("Enter numbers: {0} {1} {2}", a, b, c);
-            Console.WriteLine("Max {0}", GetMax(a, b, c));
-            Console.WriteLine("Min {0}\r\n", GetMin(a, b, c));
+            Console.WriteLine("\r\n a.tkachenko/classrom/05-debug \r\n");
 
-
-            Console.WriteLine("Calculate Sum:");
-            int d = -5;
-            int e = 4;
-            int oddResult;
-            Console.Write("{0} + {1} = ", d, e);
-            ShowSum(TrySumifOdd(d, e, out oddResult), oddResult);
+            Console.WriteLine(RoundToNext5(0));
+            Console.WriteLine(RoundToNext5(1));
+            Console.WriteLine(RoundToNext5(5));
+            Console.WriteLine(RoundToNext5(6));
+            Console.WriteLine(RoundToNext5(10));
+            Console.WriteLine(RoundToNext5(15));
+            Console.WriteLine(RoundToNext5(20));
 
             Console.Write("\r\n");
-            string _str = "str ";
-            int repeatTimes = 3;
-            Console.WriteLine("Repeat \"{0}\" {1} times:", _str, repeatTimes);
-            Console.Write("{0}", Repeat(_str, repeatTimes));
+
+            Console.WriteLine(Gcd(30, 12));
+            Console.WriteLine(Gcd(8, 9));
+            Console.WriteLine(Gcd(1, 1));
+            Console.WriteLine(Gcd(15, 5));
 
             Console.Write("\r\n");
         }
