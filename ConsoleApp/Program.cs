5d92aa9fa1356bdf8c88f07b0589eb7a996a1083
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2021-11-22 18:18:23 +0200

    hw

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿const int n = 10;
+int res=0;
+int a = 1;
+int b = 0;
+int c;
+
+switch (n)
+{
+    case < 0: res = 0; break;
+    case 0: res = 0; break;
+    case 1: res = 1; break;
+    case 2: res = 1; break;
+    default: for (int i = 2; i < n; i++)
+        {
+            c = a;
+            a = a + b;
+            b = c;
+            res = a + b;
+           //Console.WriteLine($"{i} try of Fibonacci is {res}");
+        }
+        break;
+
+}
+Console.WriteLine($"{n} of Fibonacci is {res}" );
