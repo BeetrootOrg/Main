7215e54f1967bf9e0c6e3514f18c02134f9ca855
Legion59 <legion.59mk@gmail.com>
2021-11-22 19:40:32 +0200

    Try to fix

---
+++ BeetrootClasswork_05/BeetrootClasswork_05/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int n = 7;
+Console.WriteLine(RoundToNext5(n));
+
+static int RoundToNext5(int n)
+{
+    n /= 5;
+    ++n;
+    n *= 5;
+    
+    return n;
+}
+
+// NEXT PLEASE
+
+int a = 6;
+int b = 18;
+Console.WriteLine(Gcd(a,b));
+
+static int Gcd(int a, int b)
+{
+    int gcd = 1;
+
+    for (int i = 1; i <= Math.Min(a,b); ++i)
+    {
+        if(a%i == 0 && b%i==0)
+        {
+            gcd = i;
+        }
+    }
+
+    return gcd;
+}
