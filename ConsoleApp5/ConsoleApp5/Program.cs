df174c326bcc30cde5dc4f70ea5e476b9193f8a3
Anastasiia Koronenko <koronenko.nastya@gmail.com>
2021-11-22 19:29:48 +0200

    RoundToNext5

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,6 +1,10 @@
-﻿
-using System;
-
+﻿using System;
+/// <summary>
+/// 1. округлити до наступного цілого, що ділиться на 5,
+/// наприклад 0 -> 5; 1 -> 5; 5 -> 10; 6 -> 10; 9 -> 10; 10 -> 15
+///static int RoundToNext5(int n){}
+/// </summary>
+/// 
 namespace MyApp
 {
     public class Program
@@ -8,9 +12,9 @@ namespace MyApp
         static void Main() 
         {
             int x = 0;
-            Console.WriteLine(RoundNumber(x));
+            Console.WriteLine(RoundToNext5(x));
         }
-        public static double RoundNumber(double x)
+        public static double RoundToNext5(double x)
         {
             return ((x / 5) +1) * 5 + ((x % 5) > 0 ? 5 : 0);
         }
