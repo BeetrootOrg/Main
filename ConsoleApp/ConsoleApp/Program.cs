cb243d6f5c66e3bc35f0b21ccc36a3bd36cd693b
d.misik <misik.dmitriy@gmail.com>
2021-11-22 20:24:08 +0200

    min

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,13 +6,19 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            DateTime date = new DateTime(2021, 10, 12, 15, 05, 10);
+            DateTime date = new DateTime(1921, 10, 12, 15, 05, 10);
             Console.WriteLine(date);
             Console.WriteLine($"{date:f}");
             Console.WriteLine($"{date:yyyy-MM/dd}");
 
             DateTime now = DateTime.Now;
             Console.WriteLine(now);
+
+            Console.WriteLine(now.Year);
+            Console.WriteLine(date.Ticks);
+
+            DateTime min = DateTime.MinValue;
+            Console.WriteLine(min);
         }
     }
 }
