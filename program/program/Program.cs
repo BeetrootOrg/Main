1b1d46398393daa8d9c6ef472e926c744712ec44
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2021-11-24 21:20:41 +0200

    123

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -1,61 +1,11 @@
 ﻿using System;
-namespace  ConsoleApp
+
+namespace ConsoleApp
 {
     class Program
     {
         static void Main()
-        {           
-            //first task
-            Console.WriteLine("First task:");
-            Console.WriteLine($"F(n)={ sumF(1, 0, 5)}");
-            Console.WriteLine($"M(n)={ sumM(1, 0, 8)}");
-
-            //second task
-
-            Console.WriteLine("Third task:");
-            Pow(1, 4);
-
-            //third task
-            
-            Console.WriteLine($"Count number:");
-            Count(1,10);
-
-
-        }
-        //first task
-        static int sumF(int F, int M, int n)
-        {             
-             return n - M * (F * (n - 1));           
-        }
-
-        static int sumM(int F, int M, int n)
-        {            
-            return n - F * (M * (n - 1));
-        }
-
-        //second task
-        //second task
-        static int Pow(int x, int y)
         {
-            Console.WriteLine(x);
-        }
-        }
-        }
-        //third task
-            Console.WriteLine(rand.NextInt64());
-        static void Count(int i, int l)
-        {
-            Console.WriteLine(i);
-            {                
-                if (i >= l)
-                    return;
-                i++;
-
-                Count(i,l);
-                }
-            }
-
-
 
         }
     }
