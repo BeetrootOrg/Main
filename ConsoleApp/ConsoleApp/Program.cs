f204c3743f2c68c57d666faec126541f305c377b
oleksandrmartynenko <81743146+oleksandrmartynenko@users.noreply.github.com>
2021-11-24 18:40:37 +0200

    branch create

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,44 +6,9 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            //F and M methods
-            Console.WriteLine("F and M methods");
-            Console.WriteLine(F(5));
-            Console.WriteLine(M(4));
-            //Pow method
-            Console.WriteLine("Pow method");
-            Console.WriteLine(Pow(2, 5));
 
-            //AllNumbers method
-            Console.WriteLine("AllNumbers method");
-            AllNumbers(5);
         }
 
-        //F and M methods
-        static int F(int n)
-        {
-            if (n == 0) return 1;
-            return n - M(F(n - 1));
-        }
-        static int M(int n)
-        {
-            if (n == 0) return 0;
-            return n - F(M(n - 1));
-        }
 
-        //Pow method
-        static int Pow(int x, int y)
-        {
-            if (y == 0) return 1;
-            if (y == 1) return x;
-            return x > 0 ? x * Pow(x, y - 1) : -x * Pow(x, y - 1);
-        }
-
-        //AllNumbers method
-        static void AllNumbers(int n)
-        {
-            if (n > 1) AllNumbers(n - 1);
-            Console.WriteLine(n);
-        }
     }
 }
