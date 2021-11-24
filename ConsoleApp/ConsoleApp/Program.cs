7bf9c3dae26b21fb16c688dd41dce5223e579659
d.misik <misik.dmitriy@gmail.com>
2021-11-24 19:40:07 +0200

    array in methods

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,11 +6,23 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            int[] array = { 1, 2, 3, 4, 5, 6 };
-            array = new[] { 1, 2, 3 };
-            array = new int[] { 1, 2, 3 };
-            array = new int[4] { 1, 2, 3, 4 };
+            // init array (all same options)
+            // 1 option
+            int[] array = { 1, 2, 3 };
 
+            // 2 option
+            array = new[] { 1, 2, 3 };
+
+            // 3 option
+            array = new int[] { 1, 2, 3 };
+
+            // 4 option
+            array = new int[3] { 1, 2, 3 };
+
+            UpdateFirstElement(array);
+            PrintArray(array);
+
+            UpdateFirstElementRef(ref array);
             PrintArray(array);
         }
 
@@ -20,6 +32,18 @@ namespace ConsoleApp
             {
                 Console.WriteLine(arr[i]);
             }
+        }
+
+        static void UpdateFirstElement(int[] arr)
+        {
+            arr[0] = 42;
+            arr = new[] { 5, 6, 7 };
+        }
+
+        static void UpdateFirstElementRef(ref int[] arr)
+        {
+            arr[0] = 42;
+            arr = new[] { 5, 6, 7 };
         }
     }
 }
