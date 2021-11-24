aa842d3377279b9533a5048c7ff44b3efe9ce992
SilaSamuraya <arthurgolban@gmail.com>
2021-11-24 21:12:26 +0200

    //

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -1,62 +1,41 @@
-﻿// ФИБОНАЧЧИ
-
-//int n = 2;
-//int f1=0, f2=1;
-//int sum = 0;
-//if (n == 1) sum = f1;
-//else if (n == 2) sum = f2;
-//for (int i = 2; i <= n; i++)
-//{
-//    sum = f1 + f2;
-//    f1 = f2;
-//    f2 = sum;
-//}
-//Console.WriteLine(sum);
-
-using System;
+﻿using System;
 namespace FirstProject
 {
-
     class Program
     {
         static void Main()
         {
-            double result = Foo(5.5);
-            Console.WriteLine(result);
-            Concat("Hello", "Dima");
-            string str = "Hello";
-            Console.WriteLine(AddIfOdd(2,4));
-        }
-        static double Foo(double x) => x * x * x + 5 * x * x + 6;
 
-
-        static void Concat(string str1, string str2)
-        {
-            Console.WriteLine($"{str1},{str2}");
+            PrintArr(Sort(new[] {3,4,5,6,7,2}));
         }
-        static void Increment(int i)
+        static void PrintArr(int[] arr)
         {
-            i++;
-            Console.WriteLine(i);
+            for(int i=0; i<arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);  
+            }
         }
-        static void Add(string str1, string str2)
+    
+        static int[] Sort(int[] original)
         {
-            str1 += str2;
-            Console.WriteLine($"In add:{ str1}");
-
-        }
-        static void AddRef(ref string str1, string str2)
-        {
-            str1 += str2;
-            Console.WriteLine($"In Ref add:{ str1}");
-
-        }
-        static int AddIfOdd(int i1,int i2)
-        {
-            return i1 % 2 == 0 && i2 % 2 == 0 ? i1+i2 :  0;
-            
+            var copy = new int[original.Length];
+            Array.Copy(original, copy, original.Length);
+            for(int i =0;i< copy.Length; i++)
+            {
+                int minIndex = i;
+                for(int j=i+1;j<copy.Length; ++j)
+                {
+                    if(copy[j]<copy[minIndex])
+                    {
+                        minIndex = j;   
+                    }
+                }
+                var temp = copy[i];
+                copy[i]= copy[minIndex];
+                copy[minIndex] = temp;
+            }
+            return copy;
         }
     }
-
 }
 
