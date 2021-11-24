f84793d10585a928689d6b308bf1d60bd207001f
Mour-Yam <70475271+Mour-Yam@users.noreply.github.com>
2021-11-24 19:19:42 +0200

    i.safontev/other/06-kata

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -4,34 +4,30 @@ namespace ConsoleApp
 {
     class Program
     {
-
-        static void Main(string[] args)
+        static long Func1(int n)
         {
-            int a, b, c;
-            Console.WriteLine("Enter the a: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(RoundToNext5(a));
-
-            Console.WriteLine("Enter the b and c: ");
-            b = Convert.ToInt32(Console.ReadLine());
-            c = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Gcd(b, c));
-
+            return n == 0 ? 1 : n * Func1(n - 1);
         }
-
-        static int RoundToNext5(int n)
-        {
-            n = n - n % 5 + 5;
-            return n;
-        }
-
         static int Gcd(int a, int b)
         {
-            for (int i = Math.Min(a, b); i > 0; i--) 
-            {
-                if (a % i == 0 && b % i == 0) { return i; }
-            }
-            return 0;
+            return Gcd(a, b, Math.Min(a, b));
         }
+        static int Gcd(int a, int b,int possibleGcd)
+        {
+            return a % possibleGcd == 0 && b % possibleGcd == 0 ? possibleGcd : Gcd(a, b, possibleGcd - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the nubmer n : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Factorial {n} = {Func1(n)}");
+
+            Console.WriteLine("Enter the nubmers a, b : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Gcd( {a},{b}) = {Gcd(a, b)}");
+        }
+
     }
 }
