f2adbabe3a8d5774d0d797732873419fadace760
oleksandrmartynenko <81743146+oleksandrmartynenko@users.noreply.github.com>
2021-11-24 13:17:46 +0200

    to check

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,80 +6,44 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            DateTime date = new DateTime(2021, 10, 12, 15, 05, 10);
-            Console.WriteLine(date);
-            Console.WriteLine($"{date:f}");
+            //F and M methods
+            Console.WriteLine("F and M methods");
+            Console.WriteLine(F(5));
+            Console.WriteLine(M(4));
+            //Pow method
+            Console.WriteLine("Pow method");
+            Console.WriteLine(Pow(2, 5));
 
-            DateTime now = DateTime.Now;
-            Console.WriteLine(now);
-            Console.WriteLine(now.DayOfWeek);
-
-            Console.WriteLine(date.Ticks);
-
-            DateTime min = DateTime.MinValue;
-
-            Console.WriteLine(now > min);
-
-            TimeSpan timeSpan = new TimeSpan(10, 12, 32, 52, 999);
-
-            Console.WriteLine(timeSpan);
-
-            TimeSpan diff = now - date;
-            Console.WriteLine(diff);
-            Console.WriteLine(diff.Days);
-            Console.WriteLine(diff.Seconds);
-            Console.WriteLine(diff.TotalSeconds);
-
-            //add something to datetime
-            //1 -call AddSMTHNG
-            DateTime date1 = date.AddDays(2);
-            Console.WriteLine(date);
-            Console.WriteLine(date1);
-
-            date1 = date.AddMonths(1).AddDays(2).AddHours(4);
-            Console.WriteLine(date1);
-
-            //2 - with TimeSpan
-            date1 = date1 + timeSpan;
-
-
-            //3 - with TimeSpan
-            date1 = date1.Add(timeSpan);
-
-            //4 - substract
-            date1 = date1.Subtract(diff);
-
-            Console.WriteLine(date1);
-
-
-            //Random
-            Random rand = new Random((int)DateTime.Now.Ticks);
-
-            Console.WriteLine(rand.Next());
-            Console.WriteLine(rand.Next(5));
-            Console.WriteLine(rand.Next(10, 48));
-            Console.WriteLine(rand.NextDouble());
-            Console.WriteLine(rand.NextSingle());
-            Console.WriteLine(rand.NextInt64());
-
-            Console.WriteLine("Start the game.");
-            while (true)
-            {
-                Console.ReadLine();
-                int random = rand.Next(0, 6);
-
-                if (random == 0)
-                {
-                    Console.WriteLine("You Killed.");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"You got {random}. Continue...");
-                }
-            }
+            //AllNumbers method
+            Console.WriteLine("AllNumbers method");
+            AllNumbers(5);
         }
 
+        //F and M methods
+        static int F(int n)
+        {
+            if (n == 0) return 1;
+            return n - M(F(n - 1));
+        }
+        static int M(int n)
+        {
+            if (n == 0) return 0;
+            return n - F(M(n - 1));
+        }
 
+        //Pow method
+        static int Pow(int x, int y)
+        {
+            if (y == 0) return 1;
+            if (y == 1) return x;
+            return x > 0 ? x * Pow(x, y - 1) : -x * Pow(x, y - 1);
+        }
+
+        //AllNumbers method
+        static void AllNumbers(int n)
+        {
+            if (n > 1) AllNumbers(n - 1);
+            Console.WriteLine(n);
+        }
     }
 }
