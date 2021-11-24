373b6afa5af1d771029829f64a303197f1fe9a80
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2021-11-24 19:05:06 +0200

    start

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -1,85 +1,62 @@
 ﻿using System;
-
-namespace ConsoleApp
+namespace  ConsoleApp
 {
     class Program
     {
         static void Main()
+        {           
+            //first task
+            Console.WriteLine("First task:");
+            Console.WriteLine($"F(n)={ sumF(1, 0, 5)}");
+            Console.WriteLine($"M(n)={ sumM(1, 0, 8)}");
+
+            //second task
+
+            Console.WriteLine("Third task:");
+            Pow(1, 4);
+
+            //third task
+            
+            Console.WriteLine($"Count number:");
+            Count(1,10);
+
+
+        }
+        //first task
+        static int sumF(int F, int M, int n)
+        {             
+             return n - M * (F * (n - 1));           
+        }
+
+        static int sumM(int F, int M, int n)
+        {            
+            return n - F * (M * (n - 1));
+        }
+
+        //second task
+        //second task
+        static int Pow(int x, int y)
         {
-            DateTime date = new DateTime(1921, 10, 12, 15, 05, 10);
-            Console.WriteLine(date);
-            Console.WriteLine($"{date:f}");
-            Console.WriteLine($"{date:yyyy-MM/dd}");
-
-            DateTime now = DateTime.Now;
-            Console.WriteLine(now);
-
-            Console.WriteLine(now.Year);
-            Console.WriteLine(date.Ticks);
-
-            DateTime min = DateTime.MinValue;
-            Console.WriteLine(min);
-
-            Console.WriteLine(now > min);
-
-            TimeSpan timeSpan = new TimeSpan(10, 12, 32, 56, 999);
-            Console.WriteLine(timeSpan);
-
-            TimeSpan diff = now - date;
-            Console.WriteLine(diff);
-            Console.WriteLine(diff.Days);
-            Console.WriteLine(diff.Seconds);
-            Console.WriteLine(diff.TotalSeconds);
-            Console.WriteLine(diff.TotalMinutes);
-
-            TimeSpan small = new TimeSpan(0, 0, 1);
-            Console.WriteLine(small.TotalMinutes);
-
-            // how to add something to datetitme
-            // option 1 - call AddSMTHG()
-            DateTime date1 = date.AddDays(5);
-            Console.WriteLine(date);
-            Console.WriteLine(date1);
-
-            date1 = date.AddMonths(1).AddDays(-2).AddMinutes(5);
-            Console.WriteLine(date1);
-            Console.WriteLine(date1.AddMonths(1));
-
-            // option 2 - with TimeSpan
-            date1 = date1 + timeSpan;
-
-            // option 3 - the same
-            date1 = date1.Add(timeSpan);
-
-            // subtract
-            date1 = date1.Subtract(diff);
-            Console.WriteLine(date1);
-
-            // RANDOM
-            Random rand = new Random((int)DateTime.Now.Ticks);
-            Console.WriteLine(rand.Next());
-            Console.WriteLine(rand.Next(10));
-            Console.WriteLine(rand.Next(15, 20));
-
-            Console.WriteLine(rand.NextDouble());
-            Console.WriteLine(rand.NextSingle());
+            Console.WriteLine(x);
+        }
+        }
+        }
+        //third task
             Console.WriteLine(rand.NextInt64());
+        static void Count(int i, int l)
+        {
+            Console.WriteLine(i);
+            {                
+                if (i >= l)
+                    return;
+                i++;
 
-            Console.WriteLine("Start the game");
-            while (true)
-            {
-                Console.ReadLine();
-                int random = rand.Next(0, 6);
-                if (random == 0)
-                {
-                    Console.WriteLine("You killed");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"You got {random}. Continue...");
+                Count(i,l);
                 }
             }
+
+
+
         }
     }
 }
