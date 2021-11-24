413b413e1731716ac194edb0124acc54d937de92
d.misik <misik.dmitriy@gmail.com>
2021-11-24 18:53:32 +0200

    roulette

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,78 +6,21 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            DateTime date = new DateTime(1921, 10, 12, 15, 05, 10);
-            Console.WriteLine(date);
-            Console.WriteLine($"{date:f}");
-            Console.WriteLine($"{date:yyyy-MM/dd}");
-
-            DateTime now = DateTime.Now;
-            Console.WriteLine(now);
-
-            Console.WriteLine(now.Year);
-            Console.WriteLine(date.Ticks);
-
-            DateTime min = DateTime.MinValue;
-            Console.WriteLine(min);
-
-            Console.WriteLine(now > min);
-
-            TimeSpan timeSpan = new TimeSpan(10, 12, 32, 56, 999);
-            Console.WriteLine(timeSpan);
-
-            TimeSpan diff = now - date;
-            Console.WriteLine(diff);
-            Console.WriteLine(diff.Days);
-            Console.WriteLine(diff.Seconds);
-            Console.WriteLine(diff.TotalSeconds);
-            Console.WriteLine(diff.TotalMinutes);
-
-            TimeSpan small = new TimeSpan(0, 0, 1);
-            Console.WriteLine(small.TotalMinutes);
-
-            // how to add something to datetitme
-            // option 1 - call AddSMTHG()
-            DateTime date1 = date.AddDays(5);
-            Console.WriteLine(date);
-            Console.WriteLine(date1);
-
-            date1 = date.AddMonths(1).AddDays(-2).AddMinutes(5);
-            Console.WriteLine(date1);
-            Console.WriteLine(date1.AddMonths(1));
-
-            // option 2 - with TimeSpan
-            date1 = date1 + timeSpan;
-
-            // option 3 - the same
-            date1 = date1.Add(timeSpan);
-
-            // subtract
-            date1 = date1.Subtract(diff);
-            Console.WriteLine(date1);
-
-            // RANDOM
-            Random rand = new Random((int)DateTime.Now.Ticks);
-            Console.WriteLine(rand.Next());
-            Console.WriteLine(rand.Next(10));
-            Console.WriteLine(rand.Next(15, 20));
-
-            Console.WriteLine(rand.NextDouble());
-            Console.WriteLine(rand.NextSingle());
-            Console.WriteLine(rand.NextInt64());
+            Random rand = new((int)DateTime.Now.Ticks);
 
             Console.WriteLine("Start the game");
             while (true)
             {
-                Console.ReadLine();
-                int random = rand.Next(0, 6);
+                var name = Console.ReadLine();
+                var random = rand.Next(0, 6);
                 if (random == 0)
                 {
-                    Console.WriteLine("You killed");
+                    Console.WriteLine($"{name}, you killed!");
                     break;
                 }
                 else
                 {
-                    Console.WriteLine($"You got {random}. Continue...");
+                    Console.WriteLine($"{name} continues game...");
                 }
             }
         }
