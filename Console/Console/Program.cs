4183fc3ce0650990f48ec1cc600644d3848d74d7
DMSamael <dmitriy.sulym@gmail.com>
2021-11-27 10:45:46 +0200

    char

--- Console/Console/Program.cs
+++ Console/Console/Program.cs
@@ -3,66 +3,29 @@
 
 namespace Program
 {
-
+    using System;
     class Program
     {
         static void Main()
         {
-            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, };
-            array = new[] { 1, 2, 3, 4 };
-            array = new int[] { 1, 2 };
-            array = new int[4] { 1, 2, 3, 4 };
-
-            int[] array1 = array;
-            PrintArray(array);
-
-            Array.Resize(ref array, 10);
-            PrintArray(array);
-            PrintArray(array1);
-
-            Program[] programs = new Program[2];
-            array = new int[4] { 1, 2, 3, 4 };
-            PrintArray(Sort(array));
-            array = new int[4] { 4, 3, 2, 1 };
-            PrintArray(Sort(array));
-            array = new int[4] { 122, 22, 33, 4 };
-            PrintArray(Sort(array));
-        }
-
-
-        static void PrintArray(int[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
+            char c = (char)0x41;
+            char[] arr = new[] { (char)0x41, (char)65, 'A', '\u0041','a','9' , '\u5678'};
+            foreach (char c2 in arr)
             {
-                Console.WriteLine(array[i]);
+                Console.WriteLine($"Symbol {c2}");
+                Console.WriteLine($"Is lower { Char.IsLower(c2)}");
+                Console.WriteLine($"Is upper {Char.IsUpper(c2)}");
+                Console.WriteLine($"Is number { Char.IsNumber(c2)}");
+                Console.WriteLine($"Is ASCII { Char.IsAscii(c2)}");
+                Console.WriteLine($"Is Letter { Char.IsLetter(c2)}");
+                Console.WriteLine($"ToLower  { Char.ToLower(c2)}");
+                Console.WriteLine($"ToUpper  { Char.ToUpper(c2)}");
             }
+            char res;
+            Console.WriteLine($"Try parse {char.TryParse("b",out res)}");
         }
 
-        static void UpdateFirstElement(int[] arr)
-        {
-            arr[0] = 42;
-        }
 
-        static int[] Sort(int[] original)
-        {
-            int[] copy = new int[original.Length];
-            Array.Copy(original, copy, original.Length);
-
-            for(int i = 0; i < copy.Length-1; i++)
-            {
-                int minIndex = i;
-                for(int j = 0; j < copy.Length; j++)
-                {
-                    if(copy[j] < copy[minIndex])
-                    {
-                        minIndex = j;
-                    }
-                }
-                var temp = copy[i];
-                copy[i] = copy[minIndex];
-                copy[minIndex] = temp;
-            }
-            return copy;
-        }
+  
     }
 }
