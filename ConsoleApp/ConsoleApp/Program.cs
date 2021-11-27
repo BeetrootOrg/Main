9cd2f2246adb38cf12300b0e7fb289b2389c0384
d.misik <misik.dmitriy@gmail.com>
2021-11-27 10:42:24 +0200

    bool methods

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,11 +6,16 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            char[] arr = new[] { (char)65, 'A', (char)0x41, '\u0041' }; // the same symbol 'A'
+            char[] arr = new[] { (char)65, 'A', (char)0x41, '\u0041', 'a', '9', '\u1234' }; // first four -> the same symbol 'A'
 
             foreach (var item in arr)
             {
-                Console.WriteLine(item);
+                Console.WriteLine($"Symbol: {item}");
+                Console.WriteLine($"Is Lower: {char.IsLower(item)}"); // false
+                Console.WriteLine($"Is Upper: {char.IsUpper(item)}"); // true
+                Console.WriteLine($"Is Number: {char.IsNumber(item)}"); // false
+                Console.WriteLine($"Is Ascii: {char.IsAscii(item)}"); // true
+                Console.WriteLine($"Is Letter: {char.IsLetter(item)}"); // true
             }
         }
     }
