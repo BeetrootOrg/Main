7b5ee7c8d8dfc57ec775aeb82b2437e9c209c22c
d.misik <misik.dmitriy@gmail.com>
2021-11-27 10:34:09 +0200

    char

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,24 +6,8 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            var source = new[] { 1, 2, 3 };
-            var destination = new int[source.Length];
+            char[] arr = new[] { (char)65, 'A', (char)0x41, '\u0041' }; // the same symbol 'A'
 
-            Copy(source, destination);
-
-            Print(destination);
-        }
-
-        static void Copy(int[] source, int[] destination)
-        {
-            for (int i = 0; i < source.Length; i++)
-            {
-                destination[i] = source[i];
-            }
-        }
-
-        static void Print(int[] arr)
-        {
             foreach (var item in arr)
             {
                 Console.WriteLine(item);
