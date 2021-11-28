0d7a800785da923fca22832411ac45ed6ab1511d
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2021-11-28 18:48:26 +0200

    dsf12

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,21 +7,24 @@ namespace ConsoleApp
         static void Main()
         {
             //пузырьковый метод сортировки
-            
+
             //declare array at first
 
-            int[] array = { 5,6,2,1,0,23,12,32 };
+            //int[] array = { 5,6,2,1,0,23,12,32 };
+            int[] array = new[] { 1, 7, 8, 1, 2, 3 };
             Console.WriteLine("Array before:");
-            PrintArray(array);
+            Console.WriteLine(string.Join(" ",array));
+            //PrintArray(array);
             var sortedArray = Sort(array);
-            PrintArray(sortedArray);
+            Console.WriteLine(string.Join(" ", sortedArray));
+            //PrintArray(sortedArray);
 
             static void PrintArray(int[] arr)
             {
                 string str = "";
                 for (int i = 0; i < arr.Length; i++)
                 {
-                    str=str+" "+(arr[i]);
+                    str+=" "+(arr[i]);
                 }
                 Console.WriteLine(str);
             }
@@ -31,21 +34,18 @@ namespace ConsoleApp
                 int[] copy = new int[original.Length];
                 Array.Copy(original, copy, original.Length);
 
-                for (int j=0; j<copy.Length;++j)//проверить длину перебора
-                {
-               
-                for (int i =0; i < j ; ++i) //проверить длину перебора
-                {
+                for (int j= copy.Length-1; j>0;--j)//проверить длину перебора
+                {     
+                    for (int i =0; i < j ; ++i) //проверить длину перебора
+                    {
                         if (copy[i] > copy[i+1])
                         {
                             int tempInt = copy[i];
                             copy[i]=copy[i+1];
                             copy[i + 1] = tempInt;
-                        };
+                        }
+                    }
                 }
-                }
-
-
                 return copy;
             }
 
