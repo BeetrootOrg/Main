53f75c43bd9ca8023fa7e462eacd4fe5db472d01
Viktor-Prokhorov <vicmmx@gmail.com>
2021-11-29 18:50:56 +0200

    my #5 homework

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace ConsoleApp
+{
+    using System;
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Pow(2, 8));
+            Print(3, 8);
+        }
+        public static int Pow(int x, int y)
+        {
+            if (y == 0 && x > 0) return 1;
+            else if (y == 1) return x;
+            else return x * Pow(x, y - 1);
+
+        }
+
+        public static void Print(int x, int y)
+        {
+            if (x <= y)
+            {
+                Console.WriteLine(x);
+                Print(++x, y);
+            }
+        }
+    }
+
+
+}
