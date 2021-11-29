143a02c007c43fe1ada9c49bf4d8ef5d1a43ecb3
Oleg <oleg.karman@gmail.com>
2021-11-29 20:41:41 +0200

    08 text/file work

--- course/Program.cs
+++ course/Program.cs
@@ -4,77 +4,84 @@ namespace Course
 {
     class Course
     {
-        static bool Compare(string strF, string strS)
-        {
-            if (strF.Length != strS.Length)
-            {
-                return false;
-            }
+        const string FileName = "phonebook.csv";
 
-            for (int i = 0; i < strF.Length; i++)
-            {
-                if (strF[i] != strS[i])
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-        static void Analyze(string text, out int alpbets, out int digits, out int specialC)
-        {
-            alpbets = 0;
-            digits = 0;
-            specialC = 0;
-
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (Char.IsLetter(text[i]))
-                {
-                    alpbets++;
-                } else if (Char.IsDigit(text[i]))
-                {
-                    digits++;
-                } else
-                {
-                    specialC++;
-                }
-            }
-        }
-        static string Sort(string textToSort)
-        {
-            char[] letters = textToSort.ToLower().ToCharArray();
-            Array.Sort(letters);
-            return new string(letters);
-        }
-        static char[] Duplicate(string textToCheck)
-        {
-            StringBuilder stringBuilder = new StringBuilder("");
-            string sortedString = Sort(textToCheck);
-            for (int i = 0; i < sortedString.Length - 1; i++)
-            {
-                if (sortedString[i] == sortedString[i + 1] && stringBuilder.ToString().IndexOf(sortedString[i]) == -1)
-                {
-                    stringBuilder.Append(sortedString[i]);
-                }
-            }
-            return stringBuilder.ToString().ToCharArray();
-        }
-        static void PrintArray(char[] arrToPrint)
-        {
-            Array.ForEach(arrToPrint, (el) => Console.WriteLine(el));
-        }
         public static void Main()
         {
-            Console.WriteLine(Compare("Everything is fine", "Everything is Fine"));
+            Menu();
+        }
 
-            Analyze("Some random stuff 4 you to analyzE!@#", out int alphabet, out int digits, out int specialC);
-            Console.WriteLine($"Text contain {alphabet} letters, {digits} digits and {specialC} special characters");
+        static void Menu()
+        {
+            Console.WriteLine("Welcome to the club buddy!\n");
+            Console.WriteLine("\tMenu");
+            Console.WriteLine("\t1. Show all phone book");
+            Console.WriteLine("\t2. Create phone record");
+            Console.WriteLine("\t0. EXIT");
 
-            Console.WriteLine(Sort("Wonderfull"));
+            ConsoleKeyInfo ck = Console.ReadKey();
 
-            Console.WriteLine("Duplicates in word `Hello`:");
-            PrintArray(Duplicate("Hello"));
+            switch (ck.Key)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    ShowAllNumbers();
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    CreatePhoneNumber();
+                    break;
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    CreatePhoneNumber();
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        static void CreatePhoneNumber()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter first name...");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter second name...");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter phone number...");
+            var phoneNumber = Console.ReadLine();
+
+            File.AppendAllLines(FileName, new[] {$"{firstName},{lastName},{phoneNumber}"});
+        }
+
+        static void ShowAllNumbers()
+        {
+            Console.Clear();
+
+            Console.WriteLine($"{"First name",-15} {"Last name",-15} {"Phone number",-15}");
+            string[] lines = File.ReadAllLines(FileName);
+
+            foreach (string line in lines)
+            {
+                string[] splitted = line.Split(',');
+                Console.WriteLine($"{splitted[0], -15} {splitted[1], -15} {splitted[2], -15}");
+            }
+        }
+
+        static (string firstName, string lastName, string phoneNumber)[] ReadPhoneBook()
+        {
+            string[] lines = File.ReadAllLines(FileName);
+
+            var phoneBook = new (string, string, string)[lines.Length - 1];
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] splitted = lines[i].Split(',');
+                phoneBook[i] = (splitted[0], splitted[1], splitted[2]);
+            }
+
+            return phoneBook;
         }
     }
 }
