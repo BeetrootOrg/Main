8b57aee7e183cfa9a80d0b55fb0f26d025b385f4
d.misik <misik.dmitriy@gmail.com>
2021-11-29 19:13:35 +0200

    kata

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,125 +7,19 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            char[] arr = new[] { (char)65, 'A', (char)0x41, '\u0041', 'a', '9', '\u1234' }; // first four -> the same symbol 'A'
+        }
 
-            foreach (var item in arr)
+        static int Compare(string str1, string str2) => SumOfSymbols(str1) - SumOfSymbols(str2);
+
+        static int SumOfSymbols(string str)
+        {
+            int sum = 0;
+            foreach (char symbol in str)
             {
-                Console.WriteLine($"Symbol: {item}");
-                Console.WriteLine($"Is Lower: {char.IsLower(item)}");
-                Console.WriteLine($"Is Upper: {char.IsUpper(item)}");
-                Console.WriteLine($"Is Number: {char.IsNumber(item)}");
-                Console.WriteLine($"Is Ascii: {char.IsAscii(item)}");
-                Console.WriteLine($"Is Letter: {char.IsLetter(item)}");
-                Console.WriteLine($"Is Letter: {char.IsControl(item)}");
-
-                Console.WriteLine($"To Lower: {char.ToLower(item)}");
-                Console.WriteLine($"To Upper: {char.ToUpper(item)}");
+                sum += symbol;
             }
 
-            char result;
-
-            Console.WriteLine($"TryParse 'b': {char.TryParse("b", out result)}. Result: {result}");
-            Console.WriteLine($"TryParse 'bb': {char.TryParse("bb", out result)}. Result: {result}");
-
-            Console.WriteLine("STRING\n");
-
-            string formatted = "Hello, {0}";
-            Console.WriteLine(string.Format(formatted, "Dima"));
-
-            formatted = "{0} = {1}";
-            Console.WriteLine(formatted, "3^2", 3 * 3);
-
-            formatted = "{0} says: 'I am {0}'";
-            Console.WriteLine(formatted, "Dima");
-
-            formatted = "Some stuff: {0}, {1}, {2}, {0}, {3}, {5}";
-            Console.WriteLine(formatted, "Dima", 42, 'c', true, 0, -5);
-
-            formatted = "{0}, {4}";
-            Console.WriteLine(formatted, "Dima", null, null, null, -5);
-            Console.WriteLine($"{42} is a number");
-
-            string str = "Thes is string"; // string with error
-
-            char c = str[2];
-            Console.WriteLine($"Error in symbol '{c}'");
-
-            string substring = str[0..^5];
-            Console.WriteLine($"Substring 0..^5: {substring}");
-
-            string[] arrayOfString = { "1", "42", "68" };
-            int[] parsed = new int[arrayOfString.Length];
-            for (int i = 0; i < arrayOfString.Length; i++)
-            {
-                parsed[i] = int.Parse(arrayOfString[i]);
-            }
-
-            string str1 = "Dima";
-            string str2 = "dima";
-
-            Console.WriteLine("String comparison");
-            Console.WriteLine(str1 == str2); // false, by default Ordinal
-            Console.WriteLine(str1.Equals(str2, StringComparison.OrdinalIgnoreCase)); // true
-            Console.WriteLine("c".Equals("с", StringComparison.InvariantCulture)); // false
-            Console.WriteLine("11.01".Equals("11,01", StringComparison.InvariantCulture)); // false
-
-            str1 = "abc";
-            str2 = "dce";
-
-            // if result <0 then str1 < str2
-            // if result ==0 then str1 == str2
-            // if result >0 then str1 > str2
-            Console.WriteLine(str1.CompareTo(str2)); // -1
-
-            str1 = "ABC";
-            str2 = "abc";
-            Console.WriteLine(str1.CompareTo(str2)); // 1
-
-            str1 = "This is a string";
-            Console.WriteLine($"Ends with: {str1.EndsWith("string")}"); // true
-            Console.WriteLine($"Starts with: {str1.StartsWith("This is")}"); // true
-            Console.WriteLine($"Contains: {str1.Contains("is a")}"); // true
-            Console.WriteLine($"Index of 'i': {str1.IndexOf('i')}"); // 2
-            Console.WriteLine($"Index of 'is': {str1.IndexOf("is")}"); // 2
-            Console.WriteLine($"Last index of 'is': {str1.LastIndexOf("is")}"); // 5
-            Console.WriteLine($"Last index of 'IS': {str1.LastIndexOf("IS")}"); // -1
-            Console.WriteLine($"Last index of 'IS' (ignore case): {str1.LastIndexOf("IS", StringComparison.OrdinalIgnoreCase)}"); // 5
-            Console.WriteLine($"Index of 'i' or 'h': {str1.IndexOfAny(new[] { 'i', 'h' })}"); // 1
-
-            Console.WriteLine($"To Lower: {str1.ToLower()}");
-            Console.WriteLine($"To Upper: {str1.ToUpper()}");
-
-            int[] intArray = new int[50];
-            for (int i = 0; i < intArray.Length; i++)
-            {
-                intArray[i] = i;
-            }
-
-            // 1. Join
-            Console.WriteLine(string.Join(", ", intArray));
-
-            // 2. Using string concatenation (the worst)
-            string concatResult = string.Empty;
-            foreach (var item in intArray)
-            {
-                // 1. we create 50 new string each assigment
-                // 2. extra comma in the end
-                concatResult += item + ", ";
-            }
-
-            // problem - we need to trim
-            Console.WriteLine(concatResult.Trim().TrimEnd(','));
-
-            // 3. using StringBuilder
-            StringBuilder sb = new StringBuilder();
-            foreach (var item in intArray)
-            {
-                // 1. extra comma
-                sb.Append(item).Append(", ");
-            }
-
-            Console.WriteLine(sb.ToString().Trim().TrimEnd(','));
+            return sum;
         }
     }
 }
