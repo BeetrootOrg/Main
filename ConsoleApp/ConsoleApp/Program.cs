d0c918c9c29cf3a77ae7f42408d59530faaead3c
Legion59 <legion.59mk@gmail.com>
2021-11-29 21:16:35 +0200

    Update

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,38 +1,97 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace ConsoleApp
 {
     public class Program
     {
+        const string FileName = "phonebook.csv";
         static void Main()
         {
-            string strA = "AD";
-            string strB = "DD";
-
-            Console.WriteLine(Compare(strA, strB));
+            while (true)
+            {
+                Menu();
+            }
         }
 
-        static int Compare(string a, string b)
+        static void Menu()
         {
-            int sumA = 0;
-            int sumB = 0;
+            Console.WriteLine("Welkome to Phone Book Aplication!\n");
+            Console.WriteLine("\tMemu");
+            Console.WriteLine("\t1. Show all phone book");
+            Console.WriteLine("\t2. Create phone record");
+            Console.WriteLine("\t3. Search by name");
+            Console.WriteLine("\t0. Exit");
 
-            for (int i = 0; i < a.Length; i++)
+            ConsoleKeyInfo ck = Console.ReadKey();
+
+            switch (ck.Key)
             {
-                sumA += a[i];
-                sumB += b[i];
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                     ShowAllNumbers();
+                     break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                     CreatePhoneNumber();
+                     break;
+                case ConsoleKey.D3:
+                     ConsoleKey.NumPad3:
+                     ReadPhoneBook();
+                     break;
+                case ConsoleKey.D0:
+                case ConsoleKey.NumPad0:
+                     Environment.Exit(0);
+                     break;
             }
 
-            if (sumA == sumB)
+            static void CreatePhoneNumber()
             {
-                return 0;
+                Console.Clear();
+                Console.WriteLine("Enter First Name...");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter Last Name...");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Phone Number...");
+                string phoneNumber = Console.ReadLine();
+
+                File.AppendAllLines(FileName, new[] { $"{firstName},{lastName},{phoneNumber}" }); ;
             }
-            else if (sumA > sumB)
+
+            static void ShowAllNumbers()
             {
-                return 1;
+                Console.Clear();
+                string[] lines = File.ReadAllLines("phonebook.csv");
+
+                foreach (string line in lines)
+                {
+                    string[] splitted = line.Split(',');
+
+                    foreach (string item in splitted)
+                    {
+                        Console.Write($"{item,-15}");
+                    }
+
+                    Console.WriteLine();
+                }
             }
-            else return -1;
+
+            static (string , string , string)[] ReadPhoneBook()
+            {
+                string[] lines = File.ReadAllLines(FileName);
+
+                var phoneBook = new (string, string, string)[lines.Length - 1];
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] splitted = lines[i].Split(',');
+                    phoneBook[i-1] = (splitted[0], splitted[1], splitted[2]);
+                }
+                return phoneBook;
+            }
+
         }
     }
 }
