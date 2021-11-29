bcc0a17927b4a7586d96e8c4865d8604fff08c45
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2021-11-29 14:06:08 +0200

    forth one

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,85 +1,136 @@
 ﻿using System;
-using System.Text;
 
 namespace ConsoleApp
 {
+
     class Program
     {
         static void Main()
         {
+            /*1. Compare that will return true if 2 strings are equal, otherwise false, but do not use build -in method
+            2. Analyze that will return number of alphabetic chars in string, digits and another special characters
+            3. Sort that will return string that contains all characters from input string sorted in alphabetical order(e.g. 'Hello'-> 'ehllo')
+            4. Duplicate that will return array of characters that are duplicated in input string(e.g. 'Hello and hi'-> ['h', 'l'])
+            */
 
-            char[] arr = new[] { (char)65, 'A', (char)0x41, '\u0041','a', '9','\u1234' }; //same symbol A
-            foreach (var item in arr)
+            string str1 = "Abcde";
+            string str2 = "abcde";
+            string formatted = "1. Comparing strings: {0} and {1}. Result is {2}.\n";
+            Console.WriteLine(formatted,str1,str2,Compare(str1,str2));
+
+            string str3 = "123Asdvsd!$%^";
+
+            Console.WriteLine($"2. Analysing string {str3} for numbers types of symbols");
+            Analyse(str3);
+
+            string str4 = "Hello how are you";
+            formatted = "3. Sort some random string:\n {0}.\n Get all symbols in alphabetical order:\n{1}\n.";
+            Console.WriteLine(formatted,str4, SortCharInString(str4));
+
+            string str5 ="1.some test string with some words 1 and 2";
+
+            
+
+            Console.WriteLine($"4.Analyse random string:\n {str5}.\n Found next dublicated symbols:");
+            Duplicate(str5);
+
+            static void Duplicate(string str)
             {
-                Console.WriteLine(item);
-                Console.WriteLine($"Char is lower: {Char.IsLower(item)}");//false
-                Console.WriteLine($"Char is number: {Char.IsNumber(item)}"); //false
-                Console.WriteLine($"Char is Letter: {Char.IsLetter(item)}"); //false
-                Console.WriteLine($"Char is Ascii: {Char.IsAscii(item)}"); //false
+                bool triple;
+                int j = 0;
+                char [] array= new char[str.Length-1];
+                for (int i = 0; i < str.Length - 1; i++)
+                {
+                    //Console.WriteLine(str[i]);
 
-                Console.WriteLine($"Char to lower: {Char.ToLower(item)}"); 
-                Console.WriteLine($"Char to upper: {Char.ToUpper(item)}"); 
+                    if ((str.IndexOf(str[i],i+1)!=-1)&& (Char.IsWhiteSpace(str[i])==false))
+                    {
+                        //Console.WriteLine($"duplicate: {str[i]} on positon {i} and {str.IndexOf(str[i], i + 1)}");
+                        triple = false;
+                        for (int h = 0; h < array.Length - 1; h++)
+                        {
+                            if (str[i] == array[h])
+                                triple = true;
+                        }
+                        if (triple == false)
+                        {
+                            array[j] = str[i];
+                            j++;
+                        }
+                    }
+                }
+                //string str = string.Join(", ", array);
+                Array.Resize(ref array, j);
+                PrintArray(array);
+                //Console.WriteLine("array of duplicate");
+                
+
             }
-            char result;
-            Console.WriteLine($"try parse'b':{char.TryParse("b",out result)}.Result:{result}");
-            Console.WriteLine($"try parse'bb':{char.TryParse("bb", out result)}.Result:{result}");
-
-            Console.WriteLine("Strings");
-            string formatted = "Hello,{0}";
-            Console.WriteLine(string.Format(formatted,"Rus"));
-            formatted = "{0}={1}";
-            Console.WriteLine(formatted, "3^2", 3*3);
-            formatted = "Some stuff: {0},{1},{2},{3},{5}";
-            Console.WriteLine(formatted, "Rus",42,'c',true,0,-3);
-            formatted = "{0},{4}";
-            Console.WriteLine(formatted,1,2,3,4,5,6);
-
-            string str = "thes is string";//string with error
-            var c = str[2];
-            Console.WriteLine($"Error in symbol: {c}");
-
-            var substring = str[0..^5];
-            Console.WriteLine($"Substring 0..^5: {substring}");
-
-            string[] arrayOfString = { "1", "42", "35" };
-            int[] parsed = new int[arrayOfString.Length];
-            for (int i = 0; i < arrayOfString.Length; i++)  
+            static void PrintArray(char[] arr)
             {
-                parsed[i] = int.Parse(arrayOfString[i]);
-            }
-            string str1 = "Dima";
-            string str2 = "dima";
-            Console.WriteLine("String comparison");
-            Console.WriteLine(str1==str2); //false
-            Console.WriteLine(str1.Equals(str2,StringComparison.OrdinalIgnoreCase)); //true
-            Console.WriteLine("c".Equals("с",StringComparison.InvariantCulture)); //false
-            Console.WriteLine("12.1".Equals("12,1", StringComparison.InvariantCulture)); //false
-
-            str1 = "1abd";
-            str2 = "abd";
-            //if result<0 then str1<str2;
-            //if result==0 then str1==str2;
-            //if result>0 then str1>str2;
-            Console.WriteLine(str1.CompareTo(str2));
-
-            int[] intArray = new int[50];
-            for (int i = 0; i < intArray.Length; i++)
-            {
-                intArray[i] = i;
-            }
-           
-            Console.WriteLine(string.Join(", ",intArray));
-
-            // 3. using StringBuilder
-            StringBuilder sb = new StringBuilder();
-            foreach (var item in intArray)
-            {
-                // 1. extra comma
-                sb.Append(item).Append(", ");
+                string str=string.Join(", ", arr);
+                str =str.TrimEnd(',');
+                Console.WriteLine(str);
             }
 
-            Console.WriteLine(sb.ToString().Trim().TrimEnd(','));
 
+            static string SortCharInString(string str)
+            { 
+            char[] array = new char [str.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = Char.ToLower(str[i]);
+                }
+                //Array.Sort(array); =)
+                for (int j = str.Length; j > 0; j--)
+                {
+                    char tempChar;
+                    for (int i = 0; i < j-1; i++)
+                    {
+                        if (array[i] > array[i + 1])
+                        {
+                            tempChar = array[i];
+                            array[i] = array[i + 1];
+                            array[i + 1] = tempChar;
+                        }
+                    }
+                }
+
+                return new string(array);
+            }
+
+            static bool Compare(string first, string second)
+            {
+                if (first.Length != second.Length)
+                    return false;
+                else
+                {
+                    for (int i = 0; i < first.Length; ++i)
+                    {
+                        char a = first[i];
+                        char b = second[i];
+                        if (a != b)
+                            return false;
+                    }
+                    return true;
+                }
+            }
+
+            static void Analyse(string str)
+            {
+                int digits=0;
+                int letter = 0;
+                int symbol = 0;
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    char c = str[i];
+                    if (Char.IsDigit(c)) digits += 1;
+                    if (Char.IsLetter(c)) letter += 1;
+                    if (Char.IsSymbol(c)) symbol += 1;
+                }
+                Console.WriteLine($"Result of analyse: digits:{digits}, letters: {letter}, symbols: {symbol}\n");
+            }
         }
     }
 }
