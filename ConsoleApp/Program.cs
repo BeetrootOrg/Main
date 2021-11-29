0ca16e4e4ea73b47d07c992bf387bc2207fd064e
a.tkachenko <your_email@whatever.com>
2021-11-29 19:05:08 +0200

    classroom/08-text

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,68 +3,45 @@
     using System;
     class ConsoleApp
     {
+        static int GetStringSum(ref string str)
+        {
+            int sum = 0;
 
-        // 1.округлити до наступного цілого, що ділиться на 5, наприклад 0-> 5; 1-> 5; 5-> 10; 6-> 10; 9-> 10; 10-> 15
-        static int RoundToNext5(int n)
-        {
-            return ((n / 5) + 1) * 5;
-        }
-        // 2.найбільший спільний дільник для a &b, наприклад, (30, 12)-> 6; (8, 9)-> 1, (1, 1)-> 1
-        static int Gcd(int a, int b)
-        {
-            int min = Math.Min(a, b);
-            int max = Math.Max(a, b);
-            int result = 0;
-            int i = min;
-            Console.WriteLine("Min: {0}", min);
-            do
+            for (int i = 0; i < str.Length; i++)
             {
-                if ((min % i == 0) && (max % i == 0))
-                {
-                    result = i;
-                    break;
-                }
-                else
-                {
-                    result = 1;
-                }
-            } while (--i > 0);
-            return result;
+                sum += str[i];
+            }
+
+            return sum;
         }
-
-        //static int Gcd(int a, int b)
-        //{
-        //    for (int i = Math.Min(a, b); i > 1; --i)
-        //    {
-        //        if (a % i == 0 && b % i == 0)
-        //        {
-        //            return i;
-        //        }
-        //    }
-
-        //    return 1;
-        //}
+        static int Compare(string str1, string str2)
+        {
+            Console.WriteLine("Compare: {0} and {1}:", str1, str2);
+            int resultStr1 = GetStringSum(ref str1);
+            int resultStr2 = GetStringSum(ref str2);
+            Console.WriteLine("sum Str1: {0}, sum Str2 {1}", resultStr1, resultStr2);
+            if (resultStr1 > resultStr2)
+            {
+                Console.WriteLine("{0} > {1}", str1, str2);
+                return 1;
+            }
+            else if (resultStr1 < resultStr2)
+            {
+                Console.WriteLine("{0} < {1}", str1, str2);
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("{0} == {1}", str1, str2);
+                return 0;
+            }
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("\r\n a.tkachenko/classrom/05-debug \r\n");
+            Console.WriteLine("\r\n a.tkachenko/classrom/08-Text \r\n");
 
-            Console.WriteLine(RoundToNext5(0));
-            Console.WriteLine(RoundToNext5(1));
-            Console.WriteLine(RoundToNext5(5));
-            Console.WriteLine(RoundToNext5(6));
-            Console.WriteLine(RoundToNext5(10));
-            Console.WriteLine(RoundToNext5(15));
-            Console.WriteLine(RoundToNext5(20));
-
-            Console.Write("\r\n");
-
-            Console.WriteLine(Gcd(30, 12));
-            Console.WriteLine(Gcd(8, 9));
-            Console.WriteLine(Gcd(1, 1));
-            Console.WriteLine(Gcd(15, 5));
-
-            Console.Write("\r\n");
+            Console.WriteLine("Result: {0}", Compare("str1", "str2"));
         }
     }
 }
