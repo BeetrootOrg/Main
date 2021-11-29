9b343be405e92f3246922fa32aa8d0cbac16b372
d.misik <misik.dmitriy@gmail.com>
2021-11-29 20:34:40 +0200

    refactor

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -22,24 +22,47 @@ namespace ConsoleApp
             Console.WriteLine("\tMenu");
             Console.WriteLine("\t1. Show all phone book");
             Console.WriteLine("\t2. Create phone record");
+            Console.WriteLine("\t3. Search by name");
             Console.WriteLine("\t0. Exit");
 
             ConsoleKeyInfo ck = Console.ReadKey();
-            
-            if (ck.Key == ConsoleKey.D1 || ck.Key == ConsoleKey.NumPad1)
+
+            switch (ck.Key)
             {
-                ShowAllNumbers();
-                Console.WriteLine("To back to menu type Enter...");
-                Console.ReadLine();
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    ShowAllNumbers();
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    CreatePhoneNumber();
+                    break;
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    SearchByName();
+                    break;
+                case ConsoleKey.D0:
+                case ConsoleKey.NumPad0:
+                    Exit();
+                    break;
             }
-            else if (ck.Key == ConsoleKey.D2 || ck.Key == ConsoleKey.NumPad2)
-            {
-                CreatePhoneNumber();
-            }
-            else if (ck.Key == ConsoleKey.D0 || ck.Key == ConsoleKey.NumPad0)
-            {
-                Environment.Exit(0);
-            }
+        }
+
+        private static void SearchByName()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter First Name...");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name...");
+            var lastName = Console.ReadLine();
+
+            // ToDo: finish it
+        }
+
+        private static void Exit()
+        {
+            Environment.Exit(0);
         }
 
         static void CreatePhoneNumber()
@@ -60,19 +83,33 @@ namespace ConsoleApp
         static void ShowAllNumbers()
         {
             Console.Clear();
-            string[] lines = File.ReadAllLines(Filename);
+            // show header
+            Console.WriteLine($"{"First Name",-15}{"Last Name",-15}{"Phone Number",-15}");
 
-            foreach (string line in lines)
+            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
             {
-                string[] splitted = line.Split(',');
-
-                foreach (var item in splitted)
-                {
-                    Console.Write($"{item,-15}");
-                }
-
+                Console.Write($"{firstName,-15}");
+                Console.Write($"{lastName,-15}");
+                Console.Write($"{phoneNumber,-15}");
                 Console.WriteLine();
             }
+
+            Console.WriteLine("To back to menu type Enter...");
+            Console.ReadLine();
+        }
+
+        static (string, string, string)[] ReadPhoneBook()
+        {
+            string[] lines = File.ReadAllLines(Filename);
+
+            var phoneBook = new (string, string, string)[lines.Length - 1];
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] splitted = lines[i].Split(',');
+                phoneBook[i - 1] = (splitted[0], splitted[1], splitted[2]);
+            }
+
+            return phoneBook;
         }
     }
 }
