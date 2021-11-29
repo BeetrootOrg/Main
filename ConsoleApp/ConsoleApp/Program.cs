949c1518283d9e0cb0cf1167d7521ffca1f6715d
Legion59 <legion.59mk@gmail.com>
2021-11-29 19:03:23 +0200

    Download

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,76 +7,32 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            string strA = "ABC";
-            string strB = "  ^hEL  LOhe^ll9o8";
+            string strA = "AD";
+            string strB = "DD";
 
-            Console.WriteLine($"Equals {strA} & {strB}: {StringCompare(strA, strB)}");
-
-            Console.WriteLine(SortString(strB) + "\n");
-
-            Console.WriteLine(strB);
-            StringAnalyze(strB, out int letterCount, out int numberCount, out int spchCount);
-            Console.WriteLine($"Numbers of letters in string: {letterCount}");
-            Console.WriteLine($"Numbers of digits in string: {numberCount}");
-            Console.WriteLine($"Number of special character: {spchCount}");
+            Console.WriteLine(Compare(strA, strB));
         }
 
-        static bool StringCompare(string a, string b)
+        static int Compare(string a, string b)
         {
-            if(a == b)
+            int sumA = 0;
+            int sumB = 0;
+
+            for (int i = 0; i < a.Length; i++)
             {
-                return true;
+                sumA += a[i];
+                sumB += b[i];
             }
-            return false;
-        }
 
-        //Bubble Sort for string
-        static string SortString(string str)
-        {
-            //Delete all spaces in string and change to lower register
-            char[] strCopy = str.Replace(" ", "").ToLower().ToCharArray();
-
-            for (int i = 0; i < strCopy.Length; i++)
+            if (sumA == sumB)
             {
-                for (int j = 0; j < strCopy.Length - 1 - i; j++)
-                {
-                    if (strCopy[j] > strCopy[j + 1])
-                    {
-                        char temp = strCopy[j + 1];
-                        strCopy[j + 1] = strCopy[j];
-                        strCopy[j] = temp;
-                    }
-                }
+                return 0;
             }
-            return String.Join("", strCopy);
-        }
-
-        static void StringAnalyze(string str, out int letterCount, out int numberCount, out int spchCount)
-        {
-            char[] strCopy = str.Replace(" ", "").ToCharArray();
-
-            letterCount = 0;
-            numberCount = 0;
-            spchCount = 0;
-
-            for (int i = 0; i < strCopy.Length; i++)
+            else if (sumA > sumB)
             {
-                if(Char.IsLetter(strCopy[i]))
-                {
-                    letterCount++;
-                }
-                else if(Char.IsDigit(strCopy[i]))
-                {
-                    numberCount++;
-                }
-                else spchCount++;
+                return 1;
             }
-        }
-
-        static char[] Duplicate(string str)
-        {
-            char[] strCopy = str.Replace(" ", "").ToLower().ToCharArray();
-
+            else return -1;
         }
     }
 }
