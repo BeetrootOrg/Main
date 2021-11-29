0080355b4c0fa870c0691601501bf04340f9a6d4
d.misik <misik.dmitriy@gmail.com>
2021-11-29 20:19:05 +0200

    create record

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -5,6 +5,8 @@ namespace ConsoleApp
 {
     class Program
     {
+        const string Filename = @"phonebook.csv";
+
         static void Main()
         {
             while (true)
@@ -19,6 +21,7 @@ namespace ConsoleApp
             Console.WriteLine("Welcome to Phone Book Application!\n");
             Console.WriteLine("\tMenu");
             Console.WriteLine("\t1. Show all phone book");
+            Console.WriteLine("\t2. Create phone record");
             Console.WriteLine("\t0. Exit");
 
             ConsoleKeyInfo ck = Console.ReadKey();
@@ -29,16 +32,35 @@ namespace ConsoleApp
                 Console.WriteLine("To back to menu type Enter...");
                 Console.ReadLine();
             }
+            else if (ck.Key == ConsoleKey.D2 || ck.Key == ConsoleKey.NumPad2)
+            {
+                CreatePhoneNumber();
+            }
             else if (ck.Key == ConsoleKey.D0 || ck.Key == ConsoleKey.NumPad0)
             {
                 Environment.Exit(0);
             }
         }
 
+        static void CreatePhoneNumber()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter First Name...");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name...");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number...");
+            var phoneNumber = Console.ReadLine();
+
+            File.AppendAllLines(Filename, new[] { $"{firstName},{lastName},{phoneNumber}" });
+        }
+
         static void ShowAllNumbers()
         {
             Console.Clear();
-            string[] lines = File.ReadAllLines("phonebook.csv");
+            string[] lines = File.ReadAllLines(Filename);
 
             foreach (string line in lines)
             {
