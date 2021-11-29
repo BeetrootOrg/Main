ef101d4763df83e4015fb93878537726fa1869a3
d.misik <misik.dmitriy@gmail.com>
2021-11-29 19:38:58 +0200

    console menu

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,26 +6,22 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            (int, string) tuple1 = (42, "string");
-            Console.WriteLine(tuple1);
+            Menu();
+        }
 
-            tuple1.Item1 = 1;
-            tuple1.Item2 = "str";
+        static void Menu()
+        {
+            Console.WriteLine("Welcome to Phone Book Application!\n");
+            Console.WriteLine("\tMenu");
+            Console.WriteLine("\t1. Show all phone book");
 
-            (int number, string str) tuple2 = (42, "string");
-            tuple2.number = 5;
-            tuple2.str = "str3";
-
-            Console.WriteLine(tuple2);
-
-            // var (a, b) = tuple - the same
-            (int a, string b) = tuple1;
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-
-            (int, bool, string, decimal) bigTuple = (1, true, "str1", 0.42m);
-            Console.WriteLine(bigTuple);
+            ConsoleKeyInfo ck = Console.ReadKey();
+            
+            if (ck.Key == ConsoleKey.D1) // '1' == D1
+            {
+                Console.Clear();
+                Console.WriteLine("You entered 1");
+            }
         }
     }
 }
