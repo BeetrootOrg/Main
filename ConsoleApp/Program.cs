bef063ebb748674ad39d5d397997e5446d90a1a9
a.tkachenko <your_email@whatever.com>
2021-12-01 22:21:16 +0200

    /01-cs-info

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,11 +1,12 @@
 ﻿namespace ConsoleApp
 {
     using System;
+
     class ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\r\n a.tkachenko/homework/08-Text \r\n");
+            Console.WriteLine("\r\n a.tkachenko/homework/01-cs-info \r\n");
 
 
             Console.WriteLine("Hello world!");
