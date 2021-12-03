7198ab35f895e910f24fc18904a224ffb8463ee8
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2021-12-04 00:45:06 +0200

    find and modify

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,6 +23,8 @@ namespace ConsoleApp
             Console.WriteLine("\t1. Show all phone book");
             Console.WriteLine("\t2. Create phone record");
             Console.WriteLine("\t3. Search by name");
+            Console.WriteLine("\t4. Search by number");
+            Console.WriteLine("\t5. Update number");
             Console.WriteLine("\t0. Exit");
 
             ConsoleKeyInfo ck = Console.ReadKey();
@@ -39,7 +41,13 @@ namespace ConsoleApp
                     break;
                 case ConsoleKey.D3:
                 case ConsoleKey.NumPad3:
-                    SearchByName();
+                case ConsoleKey.D4:
+                case ConsoleKey.NumPad4:
+                    SearchBy();
+                    break;
+                case ConsoleKey.D5:
+                case ConsoleKey.NumPad5:
+                    TryUpdateNumber();
                     break;
                 case ConsoleKey.D0:
                 case ConsoleKey.NumPad0:
@@ -48,10 +56,60 @@ namespace ConsoleApp
             }
         }
 
-        private static void SearchByName()
+        private static void TryUpdateNumber()
         {
             Console.Clear();
-            Console.WriteLine("Search condition - if first/last name contains search term you will see it");
+            Console.WriteLine("You must enter first and last name correct to update phone number");
+
+            Console.WriteLine("Enter first name...");
+            var searchFirstName= Console.ReadLine();
+            Console.WriteLine("Enter last name...");
+            var searchLastName = Console.ReadLine();
+
+            bool found = false;
+            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
+            {
+                if (searchFirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase) &&
+                    searchLastName.Equals(lastName, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"You can update phone nubmer. Please write it:");
+                    var newPhoneNumber= Console.ReadLine();
+                    Modify(firstName, lastName,newPhoneNumber);
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Not such users");
+            }
+
+            Wait();
+        }
+
+        static void Modify(string firstName, string lastName, string newPhoneNumber)
+        {
+            string[] lines = File.ReadAllLines(Filename);
+            string[] newPhoneBook = new string [lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                
+                string[] splitted = lines[i].Split(',');
+                if (splitted[0]==firstName && splitted[1]==lastName)
+                { 
+                    lines[i]= String.Join(",",splitted[0], splitted[1], newPhoneNumber);
+                }
+                newPhoneBook[i] = lines[i];
+
+            }
+                File.WriteAllLines(Filename, newPhoneBook);
+        }
+
+        private static void SearchBy(bool phone = true)
+        {
+            Console.Clear();
+            Console.WriteLine("Search condition - if first/last name/phone number contains search term you will see it");
 
             Console.WriteLine("Enter Search Term...");
             var searchTerm = Console.ReadLine();
@@ -60,7 +118,8 @@ namespace ConsoleApp
             foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
             {
                 if (firstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
-                    lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                    lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                    phoneNumber.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
                     found = true;
@@ -126,23 +185,10 @@ namespace ConsoleApp
             for (int i = 1; i < lines.Length; i++)
             {
                 string[] splitted = lines[i].Split(',');
-                phoneBook[i - 1] = (splitted[0], splitted[1], splitted[2]);
+                phoneBook[i-1] = (splitted[0], splitted[1], splitted[2]);
             }
 
             return phoneBook;
-        }
-
-        static string[] ConvertToText((string, string, string)[] data)
-        {
-            var content = new string[data.Length + 1];
-            content[0] = "FirstName,LastName,PhoneNumber";
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                content[i + 1] = $"{data[i].Item1},{data[i].Item2},{data[i].Item3}";
-            }
-
-            return content;
         }
     }
 }
