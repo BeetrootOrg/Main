e9ec3eaab25192a7b86a795eed317157e794e52b
Legion59 <legion.59mk@gmail.com>
2021-12-04 00:07:24 +0200

    Download

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,11 +7,23 @@ namespace ConsoleApp
     public class Program
     {
         const string FileName = "phonebook.csv";
+        const string Header = "FirstName,LastName,PhoneNumber";
+        const int MaxStringLenght = 14;
+
         static void Main()
         {
             while (true)
             {
-                Menu();
+                try
+                {
+                    Menu();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+                
             }
         }
 
@@ -56,46 +68,73 @@ namespace ConsoleApp
                     Environment.Exit(0);
                     break;
             }
+        }
 
-            static void CreatePhoneNumber()
+        static void CreatePhoneNumber()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter First Name...");
+            string firstName = Console.ReadLine();
+
+            if(firstName.Length > MaxStringLenght)
             {
-                Console.Clear();
-                Console.WriteLine("Enter First Name...");
-                string firstName = Console.ReadLine();
-
-                Console.WriteLine("Enter Last Name...");
-                string lastName = Console.ReadLine();
-
-                Console.WriteLine("Enter Phone Number...");
-                string phoneNumber = Console.ReadLine();
-
-                File.AppendAllLines(FileName, new[] { $"{firstName},{lastName},{phoneNumber}" }); ;
+                throw new ArgumentException($"Max lenght of first name is {MaxStringLenght}", nameof(firstName));
             }
 
-            static void ShowAllNumbers()
+            Console.WriteLine("Enter Last Name...");
+            string lastName = Console.ReadLine();
+
+            if (lastName.Length > MaxStringLenght)
             {
-                Console.Clear();
-                string[] lines = File.ReadAllLines("phonebook.csv");
+                throw new ArgumentException($"Max lenght of first name is {MaxStringLenght}", nameof(lastName));
+            }
 
-                foreach (string line in lines)
+            Console.WriteLine("Enter Phone Number...");
+            string phoneNumber = Console.ReadLine();
+
+            if (phoneNumber.Length > MaxStringLenght)
+            {
+                throw new ArgumentException($"Max lenght of first name is {MaxStringLenght}", nameof(phoneNumber));
+            }
+
+            File.AppendAllLines(FileName, new[] { $"{firstName},{lastName},{phoneNumber}" }); ;
+        }
+
+        static void ShowAllNumbers()
+        {
+            Console.Clear();
+
+            var phoneBook = ReadPhoneBook();
+
+            if(phoneBook == null)
+            {
+                Console.WriteLine("PhoneBook is empty");
+                Console.ReadLine();
+            }
+
+            string[] lines = File.ReadAllLines("phonebook.csv");
+
+            foreach (string line in lines)
+            {
+                string[] splitted = line.Split(',');
+
+                foreach (string item in splitted)
                 {
-                    string[] splitted = line.Split(',');
-
-                    foreach (string item in splitted)
-                    {
-                        Console.Write($"{item,-15}");
-                    }
-
-                    Console.WriteLine();
+                    Console.Write($"{item,-15}");
                 }
 
-                Wait();
+                Console.WriteLine();
             }
 
-            static (string, string, string)[] ReadPhoneBook()
+            Wait();
+        }
+
+
+        static (string, string, string)[] ReadPhoneBook()
+        {
+            try
             {
                 string[] lines = File.ReadAllLines(FileName);
-
                 var phoneBook = new (string, string, string)[lines.Length - 1];
                 for (int i = 1; i < lines.Length; i++)
                 {
@@ -105,8 +144,21 @@ namespace ConsoleApp
 
                 return phoneBook;
             }
+            catch (IOException)
+            {
+                File.WriteAllText(FileName, $"{Header}\n");
+                return new (string, string, string)[0];
+            }
+            catch (Exception)
+            {
+                return null;
+            }
 
-            static void SearchByName()
+        }
+
+        static void SearchByName()
+        {
+            try
             {
                 Console.Clear();
                 Console.WriteLine("Search condition - if first/last name contains search term you will see it");
@@ -129,84 +181,89 @@ namespace ConsoleApp
                 {
                     Console.WriteLine("Not such users");
                 }
-
+            }
+            finally
+            {
                 Wait();
             }
+            
 
-            //Search by Phone Number
-            //Similar to SearchByName() 
-            static void SearchByPhoneNumber()
+            
+        }
+
+        //Search by Phone Number
+        //Similar to SearchByName() 
+        static void SearchByPhoneNumber()
+        {
+            Console.Clear();
+            Console.WriteLine("Search condition - if phone number contains search digits we will find phone number ");
+
+            Console.WriteLine("Enter numbers...");
+            var searchTerm = Console.ReadLine();
+            bool found = false;
+
+            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
             {
-                Console.Clear();
-                Console.WriteLine("Search condition - if phone number contains search digits we will find phone number ");
-
-                Console.WriteLine("Enter numbers...");
-                var searchTerm = Console.ReadLine();
-                bool found = false;
-
-                foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
+                if (phoneNumber.Contains(searchTerm))
                 {
-                    if (phoneNumber.Contains(searchTerm))
-                    {
-                        Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
-                        found = true;
-                    }
+                    Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
+                    found = true;
                 }
-
-                if (!found)
-                {
-                    Console.WriteLine("Not such users");
-                }
-
-                Wait();
             }
 
-            static void UpdatePhoneNumber()
+            if (!found)
             {
-                Console.Clear();
-
-                Console.WriteLine("Write the FirstName and LastName where we will change the phone ");
-                string lookFirstName = Console.ReadLine();
-                string lookLastName = Console.ReadLine();
-
-                Console.WriteLine("\nWrite new PHONE NUMBER:");
-                string newPhoneNumber = Console.ReadLine();
-
-                bool found = false;
-                (string firstName, string lastName, string phoneNumber)[] phoneBook = ReadPhoneBook();
-
-                for(int i = 0; i < phoneBook.Length; i++)
-                {
-                    if (phoneBook[i].firstName.ToLower() == lookFirstName.ToLower() && phoneBook[i].lastName.ToLower() == lookLastName.ToLower())
-                    {
-                        phoneBook[i].phoneNumber = newPhoneNumber;
-                        found = true;
-                    }
-                }
-
-                if (found)
-                {
-                    string[] newPhoneBook = new string[phoneBook.Length + 1];
-                    newPhoneBook[0] = "FirstName,LastName,PhoneNumber";
-
-                    for (int i = 0; i < phoneBook.Length; i++)
-                    {
-                        newPhoneBook[i + 1] = String.Join(',', phoneBook[i].firstName, phoneBook[i].lastName, phoneBook[i].phoneNumber);
-                    }
-
-                    File.WriteAllLines(FileName, newPhoneBook);
-                }
-                else Console.WriteLine("This user not exist!!!");
-
-                Wait();
+                Console.WriteLine("Not such users");
             }
 
+            Wait();
+        }
 
-            static void Wait()
+        static void UpdatePhoneNumber()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Write the FirstName and LastName where we will change the phone ");
+            string lookFirstName = Console.ReadLine();
+            string lookLastName = Console.ReadLine();
+
+            Console.WriteLine("\nWrite new PHONE NUMBER:");
+            string newPhoneNumber = Console.ReadLine();
+
+            bool found = false;
+            (string firstName, string lastName, string phoneNumber)[] phoneBook = ReadPhoneBook();
+
+            for (int i = 0; i < phoneBook.Length; i++)
             {
-                Console.WriteLine("To back to menu type Enter...");
-                Console.ReadLine();
+                if (phoneBook[i].firstName.ToLower() == lookFirstName.ToLower() && phoneBook[i].lastName.ToLower() == lookLastName.ToLower())
+                {
+                    phoneBook[i].phoneNumber = newPhoneNumber;
+                    found = true;
+                }
             }
+
+            if (found)
+            {
+                string[] newPhoneBook = new string[phoneBook.Length + 1];
+                newPhoneBook[0] = Header;
+
+                for (int i = 0; i < phoneBook.Length; i++)
+                {
+                    newPhoneBook[i + 1] = String.Join(',', phoneBook[i].firstName, phoneBook[i].lastName, phoneBook[i].phoneNumber);
+                }
+
+                File.WriteAllLines(FileName, newPhoneBook);
+            }
+            else Console.WriteLine("This user not exist!!!");
+
+            Wait();
+        }
+
+
+        static void Wait()
+        {
+            Console.WriteLine("To back to menu type Enter...");
+            Console.ReadLine();
         }
     }
 }
