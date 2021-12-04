27f5c3d1ee28ef413083a4b1af0ba174ebb5389f
d.misik <misik.dmitriy@gmail.com>
2021-12-04 10:54:37 +0200

    phone numbers

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -50,6 +50,34 @@ namespace ConsoleApp
         }
     }
 
+    class User
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+
+    class PhoneNumberRecord
+    {
+        public User User { get; init; }
+        public string PhoneNumber { get; init; }
+
+        public string Show() => $"{User.FullName} -> {PhoneNumber}";
+    }
+
+    class PhoneNumberBook
+    {
+        public PhoneNumberRecord[] Book { get; init; }
+
+        public void ShowAll()
+        {
+            foreach (var record in Book)
+            {
+                Console.WriteLine(record.Show());
+            }
+        }
+    }
+
     class Program
     {
         static void Main()
@@ -94,6 +122,46 @@ namespace ConsoleApp
             Console.WriteLine(person.PhoneNumberUpdatedCounter);
             Console.WriteLine($"Weight Index = {person.WeightIndex()}");
             Console.WriteLine($"Weight Index = {person1.WeightIndex()}");
+
+            var users = new User[]
+            {
+                new User
+                {
+                    FirstName = "A",
+                    LastName = "B"
+                },
+                new User
+                {
+                    FirstName = "C",
+                    LastName = "D"
+                }
+            };
+
+            var phoneNumbers = new PhoneNumberRecord[]
+            {
+                new PhoneNumberRecord
+                {
+                    User = users[0],
+                    PhoneNumber = "+1234"
+                },
+                new PhoneNumberRecord
+                {
+                    User = users[1],
+                    PhoneNumber = "+1235"
+                },
+                new PhoneNumberRecord
+                {
+                    User = users[0],
+                    PhoneNumber = "126"
+                }
+            };
+
+            var book = new PhoneNumberBook
+            {
+                Book = phoneNumbers
+            };
+
+            book.ShowAll();
         }
     }
 }
