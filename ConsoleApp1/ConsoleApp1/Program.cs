c43ca30a304091dfc9014669bf5e33f94ea992f6
DMSamael <dmitriy.sulym@gmail.com>
2021-12-04 11:33:48 +0200

    UML

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,16 @@
             var person = new Person();
             Console.WriteLine(person.Age);
             Console.WriteLine(person);
+
+            person.FirstName = "Dima";
+            person.LastName = "Sulym";
+            person.Age = 32;
+            person.PhoneNumber = "+380631669127";
+            person.Height = 202;
+            person.Weight = 108;
+            Console.WriteLine(person.WeightIndex());
         }
+        
     }
     public enum Gender
     {
@@ -26,7 +35,28 @@
         public string PhoneNumber { get; set; }
         public Gender Gender { get; set; }
         public int Height { get; set; }
+        public int Weight { get; set; }
+        public double WeightIndex()
+        {
+            return Weight/(Height*Height/10000);
+        }
+    }
 
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }  
+    }
+
+    public class PhoneNumberRecord
+    {
+        public User User { get; set; }
+        public string phoneNumber { get; set; }
+    }
+
+    public class PhoneBook
+    {
+        public PhoneNumberRecord[] phoneNumberRecords { get; set; }
     }
 
 }
