ffa39833b27c5e7362cfe8494a0d45e049f4ffde
d.misik <misik.dmitriy@gmail.com>
2021-12-04 10:20:56 +0200

    class person

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace ConsoleApp
+﻿using System;
+
+namespace ConsoleApp
 {
+    enum Gender
+    {
+        Male,
+        Female
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public Gender Gender { get; set; }
+        public int Height { get; set; }
+    }
+
     class Program
     {
         static void Main()
         {
+            var person = new Person();
+            Console.WriteLine(person);
+            Console.WriteLine(person.Age);
         }
     }
 }
