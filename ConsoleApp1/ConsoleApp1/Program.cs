942481557e3b78499926eabbbf1d1eeaa8d5bab5
DMSamael <dmitriy.sulym@gmail.com>
2021-12-05 09:47:14 +0200

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -18,6 +18,7 @@
             person.PhoneNumber = "+380631669127";
             person.Height = 202;
             person.Weight = 108;
+
             Console.WriteLine(person.WeightIndex());
         }
         
@@ -58,5 +59,5 @@
     {
         public PhoneNumberRecord[] phoneNumberRecords { get; set; }
     }
-
+   
 }
