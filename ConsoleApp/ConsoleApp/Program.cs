c59c7f328c7aa39ca849b2132c1fbc7fd64406e5
Mour-Yam <70475271+Mour-Yam@users.noreply.github.com>
2021-12-06 20:29:33 +0200

    i.safontev/classwork/11-encapsulation

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,106 +1,97 @@
 ﻿using System;
 using System.Text;
+
 namespace ConsoleApp
 {
-    class Program
+    // i.safontev/classwork/11-encapsulation
+    public class User
     {
-        //i.safontev/homework/06-strings
+        public string UserName { get; set; }
+        private string Password { get; set; }
+        public long ID { get; set; }//set- init
+        private string Email { get; set; }
+        public byte[] Avatar { get; set; }
+        public User[] Followers { get; set; }
+
+    }
+
+    public class Feed
+    {
+        public Post[] Posts { get; set; }
+        public User Owner { get; set; }
+
+    }
+
+    public class Post
+    {
+        public User User { get; set; }
+        public long ID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public byte[] Content { get; set; }
+        public string Description { get; set; }
+        public Comment[] Comment { get; set; }
+
+    }
+
+    public class Comment
+    {
+        public User Author{ get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Content { get; set; }
+
+    }
+
+    public class Updatable
+    {
+        public int TimesUpdated { get; private set; }
+        int _fieldA;
+        string _fieldB;
+
+        public int FieldA 
+        {
+            get
+            {
+                return _fieldA;
+            }
+            set
+            {
+                ++TimesUpdated;
+                _fieldA = value;
+            } 
+        }
+        public string FieldB
+        {
+            get
+            {
+                return _fieldB;
+            }
+            set
+            {
+                ++TimesUpdated;
+                _fieldB = value;
+            }
+        }
+        public void ShowValues() => Console.WriteLine($"Updatable has next values{GetFieldA()}; {GetFieldB()}; {GetTimesUpdated()};");
+        string GetFieldA() => $"{nameof(FieldA)} = {FieldA}"; 
+        string GetFieldB() => $"{nameof(FieldB)} = {FieldB}";
+        string GetTimesUpdated() => $"{nameof(TimesUpdated)} = {TimesUpdated}";
+
+    }
+
+    class Program//internal by default
+    {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Enter two strings: ");
-            string a = Console.ReadLine();
-            string b = Console.ReadLine();
-            Console.WriteLine($"Are these two strings equal: {Compare(a, b)}");
+            Updatable obj=new Updatable
+            {
+                FieldA = 1,
+                FieldB = "a",
+            };
+            obj.FieldA = 2;
+            obj.FieldB = "b";
 
-            Console.WriteLine($"\nEnter the line: ");
-            string c = Console.ReadLine();
-            Analyze(c);
-
-            Console.WriteLine($"\nEnter the line: ");
-            string d = Console.ReadLine();
-            Sort(d);
-            Console.WriteLine($"\nAfter sorting: {Sort(d)}");
-
-            Console.WriteLine($"\nEnter the line: ");
-            string e = Console.ReadLine();
-            char[] array1 = Duplicate(e);
-            int charArrayLength = array1.Length;
-            Console.WriteLine($"Duplicated chars is: ");
-            for(int i = 0; i < charArrayLength; i++)
-            {
-                Console.Write($"{array1[i]} ");
-            }
-        }
-        static void Swap(ref char a, ref char b)
-        {
-            char temp = a;
-            a = b;
-            b = temp;
-        }
-        static bool Compare(string a,string b)
-        {
-            if (a.Length != b.Length) { return false; }
-            int length = a.Length;
-            for(int i = 0; i < length; i++)
-            {
-                if (a[i] != b[i]) { return false; }
-            }
-            return true;
-        }
-        static void Analyze(string a)
-        {
-            int length = a.Length;
-            int alphabeticCharUpperCount = 0;
-            int alphabeticCharLowerCount = 0;
-            int digitsCount = 0;
-            int specialCharactersCount = 0;
-            char[] array1 = a.ToCharArray();
-            for (int i = 0; i < length; i++)
-            {
-                if (array1[i] >= 65 && array1[i] <= 90)  { alphabeticCharUpperCount++; }
-                else if (array1[i] >= 97 && array1[i] <= 122) { alphabeticCharLowerCount++; }
-                else if (array1[i] >= 48 && array1[i] <= 57) { digitsCount++; }
-                else { specialCharactersCount++; }
-            }
-            Console.WriteLine($"Count of alphabetic upper chars is: {alphabeticCharUpperCount}");
-            Console.WriteLine($"Count of alphabetic lower chars is: {alphabeticCharLowerCount}");
-            Console.WriteLine($"Count of digits is: {digitsCount}");
-            Console.WriteLine($"Count of other special characters chars is: {specialCharactersCount}");
-        }
-        static string Sort(string a)
-        {
-            int length = a.Length;
-            char[] array1 = a.ToCharArray();
-            for (int i = 1; i < length; i++)
-            {
-                for(int j = 0; j < length - i; j++)
-                {
-                    if (array1[j] > array1[j + 1])
-                    {
-                        Swap(ref array1[j], ref array1[j+1]);
-                    }
-                }
-            }
-            String result = new string(array1); 
-            return result;
-        }
-        static char[] Duplicate(string a)
-        {
-            int length = a.Length;
-            char[] array1 = new char[length/2];
-            int k = 0;
-            for (int i = 0; i < length; i++)
-            {
-                for (int j = i+1; j < length - i; j++)
-                {
-                    if (a[i]==a[j]) 
-                    {
-                        array1[k] = a[i];
-                        k++;
-                    }
-                }
-            }
-            return array1;
+            obj.ShowValues();
+            
         }
     }
 }
