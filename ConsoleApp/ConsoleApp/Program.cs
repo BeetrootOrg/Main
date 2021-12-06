132ff3f92fcacca5fc31328069aef6b2fe05525a
d.misik <misik.dmitriy@gmail.com>
2021-12-06 20:39:05 +0200

    recurssion

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,94 +1,22 @@
-﻿using System;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
-    public class Comment
-    {
-        public User Author { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public string Content { get; set; }
-    }
-
-    public class User
-    {
-        public string Username { get; set; }
-        private string Password { get; set; }
-        public long Id { get; init; }
-        private string Email { get; set; }
-        public byte[] Avatar { get; set; }
-        public User[] Followers { get; set; }
-    }
-
-    public class Post
-    {
-        public User User { get; set; }
-        public long Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public byte[] Content { get; set; }
-        public string Description { get; set; }
-        public Comment[] Comments { get; set; }
-    }
-
-    public class Feed
-    {
-        public Post[] Posts { get; set; }
-        public User Owner { get; set; }
-    }
-
-    public class Updatable
-    {
-        private int _fieldA;
-        private string _fieldB;
-
-        public int FieldA
-        {
-            get
-            {
-                return _fieldA;
-            }
-            set
-            {
-                ++TimesUpdated;
-                _fieldA = value;
-            }
-        }
-
-        public string FieldB
-        {
-            get
-            {
-                return _fieldB;
-            }
-            set
-            {
-                ++TimesUpdated;
-                _fieldB = value;
-            }
-        }
-
-        public int TimesUpdated { get; private set; }
-
-        public void ShowValues() => Console.WriteLine($"Updatable has next values: {GetFieldA()}; {GetFieldB()}; {GetUpdated()}");
-
-        private string GetFieldA() => $"{nameof(FieldA)} = {FieldA}";
-        private string GetFieldB() => $"{nameof(FieldB)} = {FieldB}";
-        private string GetUpdated() => $"{nameof(TimesUpdated)} = {TimesUpdated}";
-    }
-
     class Program
     {
+        static int F(int n) => n == 0 ? 1 : n - M(F(n - 1));
+        static int M(int n) => n == 0 ? 0 : n - F(M(n - 1));
+
+        static int Pow(int x, int y) => y == 1 ? x : x * Pow(x, y - 1);
+
+        static void ShowAllUntil(int n)
+        {
+            if (n > 1) ShowAllUntil(n - 1);
+            System.Console.WriteLine(n);
+        }
+
         static void Main()
         {
-            var obj = new Updatable
-            {
-                FieldA = 42,
-                FieldB = "42"
-            };
-
-            obj.FieldA = 43;
-            obj.FieldB = "45";
-
-            obj.ShowValues();
+            System.Console.WriteLine(Pow(3, 2));
+            ShowAllUntil(3);
         }
     }
 }
