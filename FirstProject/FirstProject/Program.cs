0118eb1dc9621a5c6b1891fb682ce10279feba2e
SilaSamuraya <arthurgolban@gmail.com>
2021-12-06 20:25:58 +0200

    //

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -1,56 +1,95 @@
 ﻿using System;
-using System.IO;
 
-namespace FirstProject
+namespace ConsoleApp
 {
     public class Comment
     {
         public User Author { get; set; }
-        public DateOnly CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
         public string Content { get; set; }
-
     }
+
     public class User
     {
-        public string UserName { get; set; }
+        public string Username { get; set; }
         private string Password { get; set; }
         public long Id { get; init; }
-        public string Email { get; set; }   
+        private string Email { get; set; }
         public byte[] Avatar { get; set; }
-        public User[] Followers { get; set; }   
-        
-
+        public User[] Followers { get; set; }
     }
+
     public class Post
     {
         public User User { get; set; }
-        public long Id  { get; init; }
-
+        public long Id { get; set; }
         public DateTime CreatedAt { get; set; }
-
-        public byte[] Content { get; set; } 
-
+        public byte[] Content { get; set; }
         public string Description { get; set; }
-
         public Comment[] Comments { get; set; }
-
-
     }
+
     public class Feed
     {
         public Post[] Posts { get; set; }
         public User Owner { get; set; }
     }
-    class Program
+
+    public class Updatable
     {
+        private int _fieldA;
+        private string _fieldB;
 
-        static void Main()
+        public int FieldA
         {
-
+            get
+            {
+                return _fieldA;
+            }
+            set
+            {
+                ++TimesUpdated;
+                _fieldA = value;
+            }
         }
 
+        public string FieldB
+        {
+            get
+            {
+                return _fieldB;
+            }
+            set
+            {
+                ++TimesUpdated;
+                _fieldB = value;
+            }
+        }
+
+        public int TimesUpdated { get; private set; }
+
+        public void ShowValues() => Console.WriteLine($"Updatable has next values: {GetFieldA()}; {GetFieldB()}; {GetUpdated()}");
+
+        private string GetFieldA() => $"{nameof(FieldA)} = {FieldA}";
+        private string GetFieldB() => $"{nameof(FieldB)} = {FieldB}";
+        private string GetUpdated() => $"{nameof(TimesUpdated)} = {TimesUpdated}";
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var obj = new Updatable
+            {
+                FieldA = 42,
+                FieldB = "42"
+            };
+
+            obj.FieldA = 43;
+            obj.FieldB = "45";
+
+            obj.ShowValues();
+        }
     }
 }
-
-
 
