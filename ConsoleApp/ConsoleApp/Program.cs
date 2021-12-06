ce8133b4d81b47404af029a10d7cc5b15874dae3
d.misik <misik.dmitriy@gmail.com>
2021-12-06 19:59:33 +0200

    instagram

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,198 +2,43 @@
 
 namespace ConsoleApp
 {
-    enum Gender
+    public class Comment
     {
-        Male,
-        Female
+        public User Author { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Content { get; set; }
     }
 
-    class Person
+    public class User
     {
-        private int _phoneNumberUpdatedCounter;
-        private string _phoneNumber;
-
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string FullName => $"{FirstName} {LastName}";
-        public int Age { get; set; } = 1;
-        public string PhoneNumber
-        {
-            get
-            {
-                return _phoneNumber;
-            }
-            set
-            {
-                _phoneNumber = value;
-                ++_phoneNumberUpdatedCounter;
-            }
-        }
-
-        public Gender Gender { get; set; }
-        public int Height { get; set; }
-
-        public int PhoneNumberUpdatedCounter
-        {
-            get
-            {
-                return _phoneNumberUpdatedCounter;
-            }
-        }
-
-        public int Weight { get; init; }
-
-        public double WeightIndex()
-        {
-            return Weight / (Height * Height / 10000);
-        }
+        public string Username { get; set; }
+        private string Password { get; set; }
+        public long Id { get; init; }
+        public string Email { get; set; }
+        public byte[] Avatar { get; set; }
+        public User[] Followers { get; set; }
     }
 
-    class User
+    public class Post
     {
-        public string FirstName { get; init; }
-        public string LastName { get; init; }
-        public string FullName => $"{FirstName} {LastName}";
+        public User User { get; set; }
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public byte[] Content { get; set; }
+        public string Description { get; set; }
+        public Comment[] Comments { get; set; }
     }
 
-    class PhoneNumberRecord
+    public class Feed
     {
-        public User User { get; init; }
-        public string PhoneNumber { get; init; }
-
-        public string Show() => $"{User.FullName} -> {PhoneNumber}";
-    }
-
-    class PhoneNumberBook
-    {
-        public PhoneNumberRecord[] Book { get; init; }
-
-        public void ShowAll()
-        {
-            foreach (var record in Book)
-            {
-                Console.WriteLine(record.Show());
-            }
-        }
-    }
-
-    class TestStatic
-    {
-        public static string StaticName = "static_name";
-        public string Name;
-
-        static TestStatic()
-        {
-            StaticName = "static";
-            // can't do Name = StaticName;
-        }
-
-        public TestStatic()
-        {
-            StaticName = "non_static";
-        }
-
-        public TestStatic(string name)
-        {
-            StaticName = "non_static_with_param";
-            Name = name;
-        }
-
-        public static string StaticMethod() => "STATIC";
-        public string Method() => "NONSTATIC";
+        public Post[] Posts { get; set; }
+        public User Owner { get; set; }
     }
 
     class Program
     {
         static void Main()
         {
-            var person = new Person();
-            Console.WriteLine(person);
-            Console.WriteLine(person.Age);
-
-            // 1st approach to init
-            person.FirstName = "Dima";
-            person.LastName = "Misik";
-            person.Age = 25;
-            person.PhoneNumber = "+12345";
-            person.Gender = Gender.Male;
-            person.Height = 178;
-
-            Console.WriteLine(person.Age);
-
-            // 2nd approach to init - preferable
-            person = new Person
-            {
-                FirstName = "Dima",
-                LastName = "Misik",
-                Age = 25,
-                PhoneNumber = "+12345",
-                Gender = Gender.Male,
-                Height = 178,
-                Weight = 70
-            };
-
-            var person1 = new Person
-            {
-                Weight = 90,
-                Height = 190
-            };
-
-            Console.WriteLine(person.PhoneNumber);
-            Console.WriteLine(person.Weight);
-            Console.WriteLine(person.FullName);
-
-            person.PhoneNumber = "+12346";
-            Console.WriteLine(person.PhoneNumberUpdatedCounter);
-            Console.WriteLine($"Weight Index = {person.WeightIndex()}");
-            Console.WriteLine($"Weight Index = {person1.WeightIndex()}");
-
-            var users = new User[]
-            {
-                new User
-                {
-                    FirstName = "A",
-                    LastName = "B"
-                },
-                new User
-                {
-                    FirstName = "C",
-                    LastName = "D"
-                }
-            };
-
-            var phoneNumbers = new PhoneNumberRecord[]
-            {
-                new PhoneNumberRecord
-                {
-                    User = users[0],
-                    PhoneNumber = "+1234"
-                },
-                new PhoneNumberRecord
-                {
-                    User = users[1],
-                    PhoneNumber = "+1235"
-                },
-                new PhoneNumberRecord
-                {
-                    User = users[0],
-                    PhoneNumber = "126"
-                }
-            };
-
-            var book = new PhoneNumberBook
-            {
-                Book = phoneNumbers
-            };
-
-            book.ShowAll();
-
-            var obj1 = new TestStatic();
-            var obj2 = new TestStatic("name");
-            obj1.Method();
-            TestStatic.StaticMethod();
-            Console.WriteLine(TestStatic.StaticName);
         }
     }
 }
