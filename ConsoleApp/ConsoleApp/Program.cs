334c2c08eb6a02da3edfbef62b96ea21abfbe649
d.misik <misik.dmitriy@gmail.com>
2021-12-06 20:24:33 +0200

    modifiers

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -35,10 +35,60 @@ namespace ConsoleApp
         public User Owner { get; set; }
     }
 
+    public class Updatable
+    {
+        private int _fieldA;
+        private string _fieldB;
+
+        public int FieldA
+        {
+            get
+            {
+                return _fieldA;
+            }
+            set
+            {
+                ++TimesUpdated;
+                _fieldA = value;
+            }
+        }
+
+        public string FieldB
+        {
+            get
+            {
+                return _fieldB;
+            }
+            set
+            {
+                ++TimesUpdated;
+                _fieldB = value;
+            }
+        }
+
+        public int TimesUpdated { get; private set; }
+
+        public void ShowValues() => Console.WriteLine($"Updatable has next values: {GetFieldA()}; {GetFieldB()}; {GetUpdated()}");
+
+        private string GetFieldA() => $"{nameof(FieldA)} = {FieldA}";
+        private string GetFieldB() => $"{nameof(FieldB)} = {FieldB}";
+        private string GetUpdated() => $"{nameof(TimesUpdated)} = {TimesUpdated}";
+    }
+
     class Program
     {
         static void Main()
         {
+            var obj = new Updatable
+            {
+                FieldA = 42,
+                FieldB = "42"
+            };
+
+            obj.FieldA = 43;
+            obj.FieldB = "45";
+
+            obj.ShowValues();
         }
     }
 }
