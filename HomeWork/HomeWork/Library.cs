d6b732176e185eac2dd05df6cb824540269b6842
DMSamael <dmitriy.sulym@gmail.com>
2021-12-06 18:54:09 +0200

    1

---
+++ HomeWork/HomeWork/Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public static class Library
+    {
+        public static List<Author> authors = new List<Author>();
+        public static List<Book> books = new List<Book>();
+        public static List<Customer> customer = new List<Customer>();
+        public enum SearchBy
+        {
+            Author,
+            BookName
+        }
+
+        public static Book search(SearchBy searchBy, string value)
+        {
+            return null;
+        }
+    }
+}
