747abab6cbcffcd065fbdeabe65042ad5bad5134
TemptationHenry <72220654+TemptationHenry@users.noreply.github.com>
2021-12-07 18:36:22 +0200

    commit

---
+++ ConsoleApp11classworkEncapsulation/ConsoleApp11classworkEncapsulation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp11classworkEncapsulation
+{
+    public class Comment
+    {
+        public User Author { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        private string Password { get; set; }
+        public long Id { get; init; }
+        public string Email { get; set; }
+        public byte[] Avatar { get; set; }
+        public User[] Followers { get; set; }
+    }
+
+    public class Post
+    {
+        public User User { get; set; }
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public byte[] Content { get; set; }
+        public string Description { get; set; }
+        public Comment[] Comments { get; set; }
+    }
+
+    public class Feed
+    {
+        public Post[] Posts { get; set; }
+        public User Owner { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
--- ConsoleAppHomeworkText/ConsoleAppHomeworkText/Program.cs
+++ ConsoleAppHomeworkText/ConsoleAppHomeworkText/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 
 namespace ConsoleApp
 {
@@ -59,53 +58,96 @@ namespace ConsoleApp
         }
         private static void UpdateNumber()
         {
-            Console.Clear();
-            Console.WriteLine("Please insert a new number");
-            string updatedNumber = Console.ReadLine();
-            Console.WriteLine("Search condition - if first/last name contains search term you will see it");
-
-            Console.WriteLine("Enter Search Term...");
-            var searchTerm = Console.ReadLine();
-            bool find = false;
-            var lines = ReadPhoneBook();
-            var linesUpdated = "FirstName,LastName,PhoneNumber\n";
-            for (int i = 0; i < lines.Length; i++)
+            try
             {
-                (string firstName, string lastName, string phoneNumber) current = lines[i];
-                if (current.firstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
-                    current.lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                Console.Clear();
+                Console.WriteLine("Please insert a new number");
+                string updatedNumber = Console.ReadLine();
+                Console.WriteLine("Search condition - if first/last name contains search term you will see it");
+                if (updatedNumber == string.Empty)
                 {
-                    current.phoneNumber = updatedNumber;
-                    Console.WriteLine($"Number for {current.firstName} {current.lastName} successfully changed");
+                    throw new Exception("This string is empty");
                 }
-                linesUpdated += $"{current.firstName},{current.lastName},{current.phoneNumber} \n";
+                Console.WriteLine("Enter Search Term...");
+                var searchTerm = Console.ReadLine();
+                bool find = false;
+                var lines = ReadPhoneBook();
+                var linesUpdated = "FirstName,LastName,PhoneNumber\n";
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    (string firstName, string lastName, string phoneNumber) current = lines[i];
+                    if (current.firstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                        current.lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                    {
+                        current.phoneNumber = updatedNumber;
+                        Console.WriteLine($"Number for {current.firstName} {current.lastName} successfully changed");
+                    }
+                    linesUpdated += $"{current.firstName},{current.lastName},{current.phoneNumber} \n";
+                }
+
+                File.WriteAllText(Filename, linesUpdated);
+
+                if (!find)
+                {
+                    Console.WriteLine("Not such users");
+                    Wait();
+                    return;
+                }
             }
-
-            File.WriteAllText(Filename, linesUpdated);
-
-            if (!find)
+            catch (Exception ex)
             {
-                Console.WriteLine("Not such users");
-                Wait();
-                return;
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                UpdateNumber();
             }
-
-
         }
         //додати можливість оновлювати номер телефону за іменем/прізвищем користувача(ім'я/прізвище вводити повністю, але реєстр не важливий)
-        private static void SearchByNumber()
+        private static bool SearchByNumber(string search = null)
         {
             Console.Clear();
-            var search = Console.ReadLine();
+            if (search == null)
+            {
+                search = Console.ReadLine();
+            }
 
             bool found = false;
             foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
             {
-                if (phoneNumber.Contains(search, StringComparison.OrdinalIgnoreCase))
+                if (phoneNumber.ToLower() == search.ToLower())
                 {
-                    Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
                     found = true;
                 }
+            }
+            if (!found)
+            {
+                Console.WriteLine("Not such users");
+            }
+            return found;
+        }
+        private static void SearchByName()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Search condition - if first/last name contains search term you will see it");
+
+                Console.WriteLine("Enter Search Term...");
+                var searchTerm = Console.ReadLine();
+                if (searchTerm == string.Empty)
+                {
+                    throw new Exception("Your search is empty, please write something");
+                }
+                bool found = false;
+                foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
+                {
+                    if (firstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                        lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
+                        found = true;
+                    }
+                }
+
                 if (!found)
                 {
                     Console.WriteLine("Not such users");
@@ -113,32 +155,13 @@ namespace ConsoleApp
 
                 Wait();
             }
-        }
-        private static void SearchByName()
-        {
-            Console.Clear();
-            Console.WriteLine("Search condition - if first/last name contains search term you will see it");
-
-            Console.WriteLine("Enter Search Term...");
-            var searchTerm = Console.ReadLine();
-
-            bool found = false;
-            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
+            catch (Exception ex)
             {
-                if (firstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
-                    lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
-                    found = true;
-                }
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                SearchByName();
             }
-
-            if (!found)
-            {
-                Console.WriteLine("Not such users");
-            }
-
-            Wait();
+           
         }
 
         private static void Exit()
@@ -148,17 +171,35 @@ namespace ConsoleApp
 
         static void CreatePhoneNumber()
         {
-            Console.Clear();
-            Console.WriteLine("Enter First Name...");
-            var firstName = Console.ReadLine();
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Enter First Name...");
+                var firstName = Console.ReadLine();
 
-            Console.WriteLine("Enter Last Name...");
-            var lastName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name...");
+                var lastName = Console.ReadLine();
 
-            Console.WriteLine("Enter Phone Number...");
-            var phoneNumber = Console.ReadLine();
+                Console.WriteLine("Enter Phone Number...");
+                var phoneNumber = Console.ReadLine();
+                if (firstName == string.Empty || lastName == string.Empty)
+                {
+                    throw new ArgumentException("First or Last Name is empty");
+                }
+                if (SearchByNumber(phoneNumber))
+                {
+                    throw new ArgumentException("This number already exists");
+                }
 
-            File.AppendAllLines(Filename, new[] { $"{firstName},{lastName},{phoneNumber}" });
+                File.AppendAllLines(Filename, new[] { $"{firstName},{lastName},{phoneNumber}" });
+            }
+            catch (ArgumentException exe)
+            {
+                Console.WriteLine(exe.Message);
+                Console.ReadKey();
+                CreatePhoneNumber();
+            }
+           
         }
 
         static void ShowAllNumbers()
