c457a1ec700dd263a541ad8c058691e5b90e54d6
d.misik <misik.dmitriy@gmail.com>
2021-12-08 20:44:18 +0200

    builder

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -5,7 +5,8 @@ namespace ConsoleApp
     enum BinaryOperationType
     {
         Plus,
-        Minus
+        Minus,
+        Multiply
     }
 
     class UnaryOperation
@@ -43,6 +44,11 @@ namespace ConsoleApp
         public static BinaryOperation Create() => new MinusOperation();
     }
 
+    class MultiplyOperation : BinaryOperation
+    {
+        public override int Operation(int num1, int num2) => num1 * num2;
+    }
+
     class Calculator
     {
         public int Calculate(BinaryOperation operation, int num1, int num2) => operation.Operation(num1, num2);
@@ -55,8 +61,78 @@ namespace ConsoleApp
         {
             BinaryOperationType.Plus => new AddOperation(),
             BinaryOperationType.Minus => new MinusOperation(),
+            BinaryOperationType.Multiply => new MultiplyOperation(),
             _ => throw new ArgumentOutOfRangeException(nameof(binaryOperation)),
         };
+    }
+
+    class Animal
+    {
+        public int NumOfPaws { get; set; }
+        public string Type { get; set; }
+
+        public override string ToString() => $"Paws = {NumOfPaws}; Type = {Type}";
+    }
+
+    class AnimalBuilder
+    {
+        private string _type;
+        private int? _numOfPaws;
+
+        public AnimalBuilder SetNumOfPaws(int num)
+        {
+            if (_numOfPaws == null)
+            {
+                if (num < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Num of paws must be positive");
+                }
+
+                _numOfPaws = num;
+            }
+            else
+            {
+                throw new Exception("Num of paws already initialized");
+            }
+
+            return this;
+        }
+
+        public AnimalBuilder SetType(string type)
+        {
+            if (_type != null)
+            {
+                throw new Exception("Type already initialized");
+            }
+
+            _type = type;
+
+            if (type == "cat" || type == "dog")
+            {
+                if (_numOfPaws != null && _numOfPaws != 4)
+                {
+                    throw new Exception("Are you idiot?");
+                }
+                
+                _numOfPaws = 4;
+            }
+
+            return this;
+        }
+
+        public Animal Build()
+        {
+            if (_numOfPaws == null || _type == null)
+            {
+                throw new Exception("Please provide values");
+            }
+
+            return new Animal
+            {
+                NumOfPaws = _numOfPaws.Value,
+                Type = _type
+            };
+        }
     }
 
     class Program
@@ -66,9 +142,17 @@ namespace ConsoleApp
             var calculator = new Calculator();
             var factory = new BinaryFactory();
 
-            Console.WriteLine(calculator.Calculate(factory.CreateOperation(BinaryOperationType.Minus), 
+            Console.WriteLine(calculator.Calculate(factory.CreateOperation(BinaryOperationType.Multiply), 
                 5, 
                 new UnaryMinusOperation().Operation(3)));
+
+            var animalBuilder = new AnimalBuilder();
+
+            Animal animal = animalBuilder
+                .SetType("cat")
+                .Build();
+
+            Console.WriteLine(animal);
         }
     }
 }
