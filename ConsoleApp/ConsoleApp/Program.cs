7a869e5fe63834b2633996a511426e4b4f8d449e
d.misik <misik.dmitriy@gmail.com>
2021-12-08 19:50:25 +0200

    override

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -11,6 +11,41 @@ namespace ConsoleApp
 
         public virtual string MakeNoise() => "Unknown Animal says ???";
         public void Eat() => Console.WriteLine("Unknwon Animal eats ???");
+
+        public override bool Equals(object obj) => Equals(obj as Animal);
+
+        protected virtual bool Equals(Animal animal)
+        {
+            if (animal == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, animal))
+            {
+                return true;
+            }
+
+            if (this.GetType() != animal.GetType())
+            {
+                return false;
+            }
+
+            return NumOfPaws == animal.NumOfPaws &&
+                Length == animal.Length &&
+                HasTale == animal.HasTale &&
+                Color == animal.Color;
+        }
+
+        public override int GetHashCode() => NumOfPaws.GetHashCode() ^ 
+            Length.GetHashCode() ^ 
+            HasTale.GetHashCode() ^ 
+            Color.GetHashCode();
+
+        public override string ToString() => $"{nameof(NumOfPaws)} = {NumOfPaws}; " +
+            $"{nameof(Length)} = {Length}; " +
+            $"{nameof(HasTale)} = {HasTale}; " +
+            $"{nameof(Color)} = {Color}";
     }
 
     class Cat : Animal
@@ -48,6 +83,21 @@ namespace ConsoleApp
             ShowAnimal(cat);
 
             Console.WriteLine(cat.HasTale);
+
+            Console.WriteLine(animal);
+            Console.WriteLine(cat);
+
+            var animal2 = new Animal
+            {
+                Color = "red",
+                HasTale = true,
+                NumOfPaws = 5,
+                Length = -1
+            };
+
+            Console.WriteLine(animal == animal2);
+            Console.WriteLine(animal.Equals(animal2));
+            Console.WriteLine(animal.Equals(new object()));
         }
 
         static void ShowAnimal(Animal animal)
