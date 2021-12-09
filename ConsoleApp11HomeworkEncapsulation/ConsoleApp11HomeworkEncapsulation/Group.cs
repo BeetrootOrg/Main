0c64f398705b86819c6f156fa4ddaa08c44a9e1d
TemptationHenry <72220654+TemptationHenry@users.noreply.github.com>
2021-12-09 19:56:24 +0200

    Commit

---
+++ ConsoleApp11HomeworkEncapsulation/ConsoleApp11HomeworkEncapsulation/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11HomeworkEncapsulation
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public int Grade { get; set; }
+    }
+}
