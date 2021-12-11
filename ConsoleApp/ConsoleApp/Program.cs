181d1fd637a4b774330a9a96d466dff52cb65f9f
oleksandrmartynenko <81743146+oleksandrmartynenko@users.noreply.github.com>
2021-12-11 13:44:28 +0200

    cw

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,96 +1,83 @@
 ﻿using System;
 namespace ConsoleApp
 {
+
+    class Animal
+    {
+        public int NumOfPaws { get; set; }
+        public int Length { get; set; }
+        public virtual bool HasTail { get; set; }
+        public string Color { get; set; }
+
+        public virtual string MakeNoise() => "Unknown animal says ???";
+        public void Eat() => Console.WriteLine("Unknown animal eats ???");
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        protected virtual bool Equals(Animal animal)
+        {
+            if (animal == null)
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, animal))
+            {
+                return true;
+            }
+            if (this.GetType() != animal.GetType())
+            {
+                return false;
+            }
+
+            return NumOfPaws == animal.NumOfPaws && Length == animal.Length && HasTail == animal.HasTail && Color == animal.Color;
+        }
+
+
+
+        public override int GetHashCode() => NumOfPaws.GetHashCode() ^ Length.GetHashCode() ^ HasTail.GetHashCode() ^ Color.GetHashCode();
+
+        public override string ToString() => $"{nameof(NumOfPaws)}={NumOfPaws}";
+    }
+
+    class Cat : Animal
+    {
+        public string Breed { get; set; }
+
+        public override string MakeNoise() => "Cat says 'meow'";
+        public new void Eat() => Console.WriteLine("cat eats fish");
+    }
     class Program
     {
         static void Main()
         {
-            string str1 = "TestD";
-            string str2 = "Testd";
-            Console.WriteLine("Compare Method");
-            Console.WriteLine(Compare(str1, str2));
-            Console.WriteLine("Analyze Method");
-            Console.WriteLine(Analyze("Test string 12345 ???@"));
-            Console.WriteLine("Sort Method");
-            Console.WriteLine(Sort("vbfhavkcjcdj"));
-            Console.WriteLine("Duplicate Method");
-            foreach (char str in Duplicate("AaaaazzzzSFsAA"))
+            var animal = new Animal
             {
-                Console.WriteLine(str);
+                Color = "red",
+                HasTail = true,
+                NumOfPaws = 5,
+                Length = -1
+            };
+            var cat = new Cat
+            {
+                Color = "black",
+                HasTail = true,
+                NumOfPaws = 4,
+                Length = 30
+            };
+
+            Console.WriteLine(cat);
+
+            static void ShowAnimal(Animal animal)
+            {
+                Console.WriteLine(animal.MakeNoise());
+                animal.Eat();
+
             }
+
         }
 
-        //Compare method
-        static bool Compare(string str1, string str2)
-        {
-            if (str1.Length != str2.Length)
-            {
-                return false;
-            }
-            else
-            {
-                for (int i = 0; i < str1.Length; i++)
-                    if (str1[i] != str2[i])
-                        return false;
-                return true;
-            }
-        }
-
-        //Analyze method
-        static string Analyze(string str)
-        {
-            int alphabeticChars = 0;
-            int digits = 0;
-            int specialSymbols = 0;
-
-            foreach (var item in str)
-            {
-                if (Char.IsDigit(item))
-                {
-                    digits++;
-                }
-                else if (Char.IsLetter(item))
-                {
-                    alphabeticChars++;
-                }
-                else
-                {
-                    specialSymbols++;
-                }
-            }
-            string result = $"There are {alphabeticChars} - alpabetical chars, {digits} - digits and {specialSymbols} - special symbols (spaces are included) in this string";
-            return result;
-        }
-
-        //Sort method
-        static string Sort(string str)
-        {
-            char[] loweredCharArr = str.ToLower().ToCharArray();
-
-            for (int i = 0; i < loweredCharArr.Length - 1; i++)
-                for (int j = 0; j < loweredCharArr.Length - i - 1; j++)
-                    if (loweredCharArr[j] > loweredCharArr[j + 1])
-                    {
-                        char temp = loweredCharArr[j];
-                        loweredCharArr[j] = loweredCharArr[j + 1];
-                        loweredCharArr[j + 1] = temp;
-                    }
-            return new string(loweredCharArr);
-        }
-
-        //Duplicate method
-        static char[] Duplicate(string str)
-        {
-            string result = "";
-            string loweredStr = str.ToLower();
-            foreach (char value in loweredStr)
-            {
-                if (result.IndexOf(value) == -1)
-                {
-                    result += value;
-                }
-            }
-            return result.ToCharArray();
-        }
     }
 }
