04fee5f4cc6738c2f40c6e203133d1a013b53cec
Ivan Chicha <94204093+VanSSHH@users.noreply.github.com>
2021-12-11 18:10:59 +0200

    snthng strange

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,96 +1,230 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
+        const string Filename = "phonebook.csv";
+        const string Head = "FirstName,LastName,PhoneNumber";
+
         static void Main()
         {
-            char[] arr = new[] { (char)65, 'A', (char)0x41, '\u0041', 'a', '9', '\u1234' }; // first four -> the same symbol 'A'
-
-            foreach (var item in arr)
+            while (true)
             {
-                Console.WriteLine($"Symbol: {item}");
-                Console.WriteLine($"Is Lower: {char.IsLower(item)}");
-                Console.WriteLine($"Is Upper: {char.IsUpper(item)}");
-                Console.WriteLine($"Is Number: {char.IsNumber(item)}");
-                Console.WriteLine($"Is Ascii: {char.IsAscii(item)}");
-                Console.WriteLine($"Is Letter: {char.IsLetter(item)}");
-                Console.WriteLine($"Is Letter: {char.IsControl(item)}");
-
-                Console.WriteLine($"To Lower: {char.ToLower(item)}");
-                Console.WriteLine($"To Upper: {char.ToUpper(item)}");
+                Menu();
             }
-
-            char result;
-
-            Console.WriteLine($"TryParse 'b': {char.TryParse("b", out result)}. Result: {result}");
-            Console.WriteLine($"TryParse 'bb': {char.TryParse("bb", out result)}. Result: {result}");
-
-            Console.WriteLine("STRING\n");
-
-            string formatted = "Hello, {0}";
-            Console.WriteLine(string.Format(formatted, "Dima"));
-
-            formatted = "{0} = {1}";
-            Console.WriteLine(formatted, "3^2", 3 * 3);
-
-            formatted = "{0} says: 'I am {0}'";
-            Console.WriteLine(formatted, "Dima");
-
-            formatted = "Some stuff: {0}, {1}, {2}, {0}, {3}, {5}";
-            Console.WriteLine(formatted, "Dima", 42, 'c', true, 0, -5);
-
-            formatted = "{0}, {4}";
-            Console.WriteLine(formatted, "Dima", null, null, null, -5);
-            Console.WriteLine($"{42} is a number");
-
-            string str = "Thes is string"; // string with error
-
-            char c = str[2];
-            Console.WriteLine($"Error in symbol '{c}'");
-
-            string substring = str[0..^5];
-            Console.WriteLine($"Substring 0..^5: {substring}");
-
-            string[] arrayOfString = { "1", "42", "68" };
-            int[] parsed = new int[arrayOfString.Length];
-            for (int i = 0; i < arrayOfString.Length; i++)
-            {
-                parsed[i] = int.Parse(arrayOfString[i]);
-            }
-
-            string str1 = "Dima";
-            string str2 = "dima";
-
-            Console.WriteLine("String comparison");
-            Console.WriteLine(str1 == str2); // false, by default Ordinal
-            Console.WriteLine(str1.Equals(str2, StringComparison.OrdinalIgnoreCase)); // true
-            Console.WriteLine("c".Equals("с", StringComparison.InvariantCulture)); // false
-            Console.WriteLine("11.01".Equals("11,01", StringComparison.InvariantCulture)); // false
-
-            str1 = "abc";
-            str2 = "dce";
-
-            // if result <0 then str1 < str2
-            // if result ==0 then str1 == str2
-            // if result >0 then str1 > str2
-            Console.WriteLine(str1.CompareTo(str2)); // -1
-
-            str1 = "ABC";
-            str2 = "abc";
-            Console.WriteLine(str1.CompareTo(str2)); // 1
-
-            str1 = "This is a string";
-            Console.WriteLine($"Ends with: {str1.EndsWith("string")}"); // true
-            Console.WriteLine($"Starts with: {str1.StartsWith("This is")}"); // true
-            Console.WriteLine($"Contains: {str1.Contains("is a")}"); // true
-            Console.WriteLine($"Index of 'i': {str1.IndexOf('i')}"); // 2
-            Console.WriteLine($"Index of 'is': {str1.IndexOf("is")}"); // 2
-            Console.WriteLine($"Last index of 'is': {str1.LastIndexOf("is")}"); // 5
-            Console.WriteLine($"Last index of 'IS': {str1.LastIndexOf("IS")}"); // -1
-            Console.WriteLine($"Last index of 'IS' (ignore case): {str1.LastIndexOf("IS", StringComparison.OrdinalIgnoreCase)}"); // 5
-            Console.WriteLine($"Index of 'i' or 'h': {str1.IndexOfAny(new[] { 'i', 'h' })}"); // 1
         }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Phone Book Application!\n");
+            Console.WriteLine("\tMenu");
+            Console.WriteLine("\t1. Show all phone book");
+            Console.WriteLine("\t2. Create phone record");
+            Console.WriteLine("\t3. Search by name");
+            Console.WriteLine("\t4. Search by phone number");
+            Console.WriteLine("\t5. Rerecord phone number of some user");
+            Console.WriteLine("\t0. Exit");
+
+            ConsoleKeyInfo ck = Console.ReadKey();
+
+            switch (ck.Key)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    ShowAllNumbers();
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    CreatePhoneNumber();
+                    break;
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    SearchByName();
+                    break;
+                case ConsoleKey.D4:
+                case ConsoleKey.NumPad4:
+                    SearchByNumber();
+                    break;
+                case ConsoleKey.D5:
+                case ConsoleKey.NumPad5:
+                    RerecordNumber();
+                    break;
+                case ConsoleKey.D0:
+                case ConsoleKey.NumPad0:
+                    Exit();
+                    break;
+            }
+        }
+
+        private static void SearchByName()
+        {
+            Console.Clear();
+            Console.WriteLine("Search condition - if first/last name contains search term you will see it");
+
+            Console.WriteLine("Enter Search Term...");
+            var searchTerm = Console.ReadLine();
+
+            bool found = false;
+            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
+            {
+                if (firstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                    lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Not such users");
+            }
+
+            Wait();
+        }
+
+        private static void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        static void CreatePhoneNumber()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter First Name...");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name...");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number...");
+            var phoneNumber = Console.ReadLine();
+
+            File.AppendAllLines(Filename, new[] { $"{firstName},{lastName},{phoneNumber}" });
+        }
+
+        static void ShowAllNumbers()
+        {
+            Console.Clear();
+            // show header
+            Console.WriteLine($"{"First Name",-15}{"Last Name",-15}{"Phone Number",-15}");
+
+            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
+            {
+                Console.Write($"{firstName,-15}");
+                Console.Write($"{lastName,-15}");
+                Console.Write($"{phoneNumber,-15}");
+                Console.WriteLine();
+            }
+
+            Wait();
+        }
+
+        private static void Wait()
+        {
+            Console.WriteLine("To back to menu type Enter...");
+            Console.ReadLine();
+        }
+
+        static (string, string, string)[] ReadPhoneBook()
+        {
+            string[] lines = File.ReadAllLines(Filename);
+
+            var phoneBook = new (string, string, string)[lines.Length - 1];
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] splitted = lines[i].Split(',');
+                phoneBook[i - 1] = (splitted[0], splitted[1], splitted[2]);
+            }
+
+            return phoneBook;
+        }
+
+        static string[] ConvertToText((string, string, string)[] data)
+        {
+            var content = new string[data.Length + 1];
+            content[0] = "FirstName,LastName,PhoneNumber";
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                content[i + 1] = $"{data[i].Item1},{data[i].Item2},{data[i].Item3}";
+            }
+
+            return content;
+        }
+        private static void SearchByNumber()
+        {
+            Console.Clear();
+            Console.WriteLine("Search condition - if phone number contains search term you will see it");
+
+            Console.WriteLine("Enter Search Term...");
+            var searchTerm = Console.ReadLine();
+
+            bool found = false;
+            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
+            {
+                if (phoneNumber.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Not such users");
+            }
+
+            Wait();
+        }
+        private static void RerecordNumber()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Write the first name and last name of the user to change the phone number\n");
+            Console.WriteLine("First name:");
+            string lookFirstName = Console.ReadLine();
+            Console.WriteLine("Second name:");
+            string lookLastName = Console.ReadLine();
+
+            bool found = false;
+            (string firstName, string lastName, string phoneNumber)[] phoneBook = ReadPhoneBook();
+
+            for (int i = 0; i < phoneBook.Length; ++i)
+            {
+
+                if (String.Equals(lookFirstName, phoneBook[i].firstName, StringComparison.OrdinalIgnoreCase) &&
+                    String.Equals(lookLastName, phoneBook[i].lastName, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("\nWrite new phone number:");
+                    phoneBook[i].phoneNumber = Console.ReadLine();
+
+                    string[] newPhoneBook = new string[phoneBook.Length + 1];
+                    newPhoneBook[0] = Head;
+
+                    for (int j = 0; j < phoneBook.Length; j++)
+                    {
+                        newPhoneBook[j + 1] = String.Join(',', phoneBook[j].firstName, phoneBook[j].lastName, phoneBook[j].phoneNumber);
+                    }
+
+                    File.WriteAllLines(Filename, newPhoneBook);
+
+                    Console.WriteLine("Update successfuly done");
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("This user doesn`t exist");
+            }
+
+            Wait();
+        }
+
     }
 }
