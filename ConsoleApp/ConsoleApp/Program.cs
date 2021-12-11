1ebac252ba2a4b0de9dfa17bf87fba0da321010f
Ivan Chicha <94204093+VanSSHH@users.noreply.github.com>
2021-12-11 17:25:28 +0200

    qwerty

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,59 +2,61 @@
 
 namespace ConsoleApp
 {
-    class Program
+    class Programm
     {
         static void Main()
         {
-            int[] arr = { 1, 3, 2, 6, 4, 5, 8, 0, 9, 7 };
-            BubleSort(arr);
-            InsertionSort(arr);
-        }
-        static void PrintArray(int[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine(arr[i]);
-            }
-        }
-        static void BubleSort(int[] arr)
-        {
-            int temp;
-            for (int i = 0; i < arr.Length - 1; ++i)
-            {
-                for (int j = 0; j < arr.Length; ++i)
-                {
-                    if (arr[j] > arr[j + 1])
-                    {
-                        temp = arr[j + 1];
-                        arr[j + 1] = arr[j];
-                        arr[j] = temp;
-                        
-                    }
-                }
-            }
-            
-        }
-        static void InsertionSort(int[] arr)
-        {
-            int MinIndex = 0;
-            int MinValFound = 0;
+            Console.WriteLine("Write your first line:");
+            string fstr = Console.ReadLine();
+            Console.WriteLine("Write your second line :");
+            string sstr = Console.ReadLine();
+            Compare(fstr, sstr);
+            Console.WriteLine(" ");
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                MinIndex = i;
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[j]<arr[MinIndex])
-                    {
-                        MinIndex = j;
-                    }
-                }
-                MinValFound = arr[MinIndex];
-                arr[MinIndex] = arr[i];
-                arr[i] = MinValFound;
-            }   
-        }
+            Console.WriteLine("Write another one line:");
+            string symstr = Console.ReadLine();
+            Analyze(symstr);
+            Console.WriteLine(" ");
 
+            Console.WriteLine("Write another one line to sort symbols in alphabetical order:");
+            string alfstr = Console.ReadLine();
+            Sort(alfstr);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Write another one line to delite duplicated symbols in it:");
+            string dupstr = Console.ReadLine();
+            Duplicate(dupstr);
+            Console.WriteLine(" ");
+
+        }
+        static void Compare(string fstr, string sstr)
+        {
+            bool cb;
+            bool result = (fstr == sstr) ? cb = true : cb = false;
+            Console.WriteLine($"Is this lines are equel: {cb}");
+
+        }
+        static void Analyze(string symstr)
+        {
+            int i;
+            int symstrLength = symstr.Length;
+            for (i = 0; i < symstrLength; ++i)
+            {
+                int symstr1 = symstr.Length - 1;
+                symstr = symstr.Remove(symstr1);
+            }
+            Console.WriteLine($"There is {i} symbols");
+        }
+        static void Sort(string alfstr)
+        {
+            char[] alfstrX = alfstr.ToCharArray();
+            Array.Sort(alfstrX);
+            Console.WriteLine(alfstrX);
+        }
+        static void Duplicate(string dupstr)
+        {
+            dupstr = new string(dupstr.Distinct().ToArray());
+            Console.WriteLine(dupstr);
+        }
     }
 }
