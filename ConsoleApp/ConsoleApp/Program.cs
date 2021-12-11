f951242cae2e1597d0873ab78429654c2c7d3f11
Mour-Yam <70475271+Mour-Yam@users.noreply.github.com>
2021-12-11 20:27:21 +0200

    i.safontev/homework/09-oop

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -3,208 +3,52 @@ using System.Text;
 using System.IO;
 namespace ConsoleApp
 {
-    //i.safontev/homework/08-exceptions
+    //i.safontev/homework/09-oop
+    class BookLibrary
+    {
+        private Client[] Clients { get; set; }
+        private Book[] Books { get; set; }
+        private Librarians[] Librarian { get; set; }
+    }
+    class Librarians
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private int Age { get; set; }
+        private int WorkExperience { get; set; }
+
+    }
+    class Client
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string ClientID { get; set; }
+        private Book[] BorrowedBooks { get; set; }
+
+    }
+    class Book
+    {
+        private string Name { get; set; }
+        public string BookID { get; set; }
+        public Author WrittenBy { get; set; }
+        private string Year { get; set; }
+        private string Genre { get; set; }
+        private bool IsTaken { get; set; }
+        private Client ByWho { get; set; }
+
+    }
+    class Author
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+
+    }
+
     class Program
     {
-        const string Filename = @"D:\MyFiles\C# Course\Main\ConsoleApp\ConsoleApp\phonebook.csv";
-
         static void Main()
         {
-            while (true)
-            {
-                Menu();
-            }
+
         }
-
-        static void Menu()
-        {
-            Console.Clear();
-            Console.WriteLine("Welcome to Phone Book Application!\n");
-            Console.WriteLine("\tMenu");
-            Console.WriteLine("\t1. Show all phone book");
-            Console.WriteLine("\t2. Create phone record");
-            Console.WriteLine("\t3. Search by name");
-            Console.WriteLine("\t4. Search by phone number");
-            Console.WriteLine("\t5. Refresh phone number by name/ surname");
-            Console.WriteLine("\t0. Exit");
-
-            ConsoleKeyInfo ck = Console.ReadKey();
-
-            switch (ck.Key)
-            {
-                case ConsoleKey.D1:
-                case ConsoleKey.NumPad1:
-                    ShowAllNumbers();
-                    break;
-                case ConsoleKey.D2:
-                case ConsoleKey.NumPad2:
-                    CreatePhoneNumber();
-                    break;
-                case ConsoleKey.D3:
-                case ConsoleKey.NumPad3:
-                    SearchByName();
-                    break;
-                case ConsoleKey.D4:
-                case ConsoleKey.NumPad4:
-                    SearchByPhoneNumber();
-                    break;
-                case ConsoleKey.D5:
-                case ConsoleKey.NumPad5:
-                    RefreshNumber();
-                    break;
-                case ConsoleKey.D0:
-                case ConsoleKey.NumPad0:
-                    Exit();
-                    break;
-            }
-        }
-
-        private static void SearchByName()
-        {
-            Console.Clear();
-            Console.WriteLine("Search condition - if first/last name contains search term you will see it");
-            Console.WriteLine("Enter Search Term...");
-            var searchTerm = Console.ReadLine();
-
-            bool found = false;
-            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
-            {
-                if (firstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
-                    lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
-                    found = true;
-                }
-            }
-
-            if (!found)
-            {
-                Console.WriteLine("Not such users");
-            }
-
-            Wait();
-        }
-
-        private static void Exit()
-        {
-            Environment.Exit(0);
-        }
-
-        private static void CreatePhoneNumber()
-        {
-            Console.Clear();
-            Console.WriteLine("Enter First Name...");
-            var firstName = Console.ReadLine();
-
-            Console.WriteLine("Enter Last Name...");
-            var lastName = Console.ReadLine();
-
-            Console.WriteLine("Enter Phone Number...");
-            var phoneNumber = Console.ReadLine();
-
-            File.AppendAllLines(Filename, new[] { $"{firstName},{lastName},{phoneNumber}" });
-        }
-
-        private static void ShowAllNumbers()
-        {
-            try
-            {
-                Console.Clear();
-                Console.WriteLine($"{"First Name",-15}{"Last Name",-15}{"Phone Number",-15}");
-
-                foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
-                {
-                    Console.Write($"{firstName,-15}");
-                    Console.Write($"{lastName,-15}");
-                    Console.Write($"{phoneNumber,-15}");
-                    Console.WriteLine();
-                }
-            }
-            catch (System.NullReferenceException)
-            {
-                Console.WriteLine("Exception: you passed the null object reference!");
-            }
-            Wait();
-        }
-
-        private static void Wait()
-        {
-            Console.WriteLine("To back to menu type Enter...");
-            Console.ReadLine();
-        }
-
-        static (string, string, string)[] ReadPhoneBook()
-        {
-            try
-            {
-                string[] lines = File.ReadAllLines(Filename);
-                var phoneBook = new (string, string, string)[lines.Length - 1];
-                for (int i = 1; i < lines.Length; i++)
-                {
-                    string[] splitted = lines[i].Split(',');
-                    phoneBook[i - 1] = (splitted[0], splitted[1], splitted[2]);
-                }
-
-                return phoneBook;
-            }
-            catch(System.IO.FileNotFoundException)
-            {
-                Console.WriteLine("Exception: File not found!");
-                return null;
-            }
-        }
-
-        private static void SearchByPhoneNumber() 
-        {
-            Console.Clear();
-            Console.WriteLine("Search condition - if phone numbert contains search term you will see it");
-            Console.WriteLine("Enter Search Term...");
-            var searchTerm = Console.ReadLine();
-
-            bool found = false;
-            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
-            {
-                if (phoneNumber.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
-                    found = true;
-                }
-            }
-
-            if (!found)
-            {
-                Console.WriteLine("Not such users");
-            }
-
-            Wait();
-        }
-        
-        private static void RefreshNumber()
-        {
-            Console.Clear();
-            Console.WriteLine("Search condition - if first/last name contains search term you will see it");
-            Console.WriteLine("Enter Search Term...");
-            var searchTerm = Console.ReadLine();
-
-            bool found = false;
-            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
-            {
-                if (firstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
-                    lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
-                    found = true;
-                    Console.WriteLine("Enter the new number: ");
-                    string newNumber = Console.ReadLine();
-                    File.WriteAllText(Filename, File.ReadAllText(Filename).Replace(phoneNumber, newNumber));
-                }
-            }
-            if (!found)
-            {
-                Console.WriteLine("Not such users");
-            }
-            Wait();
-        }
-
     }
 }
