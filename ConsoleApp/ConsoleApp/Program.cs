51c569571e52914493cdda1cb7814d49ce8fdcb7
Legion59 <legion.59mk@gmail.com>
2021-12-13 01:35:30 +0200

    Download

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,66 +2,56 @@
 
 namespace ConsoleApp
 {
-    public class Comment
+    class School
     {
-        public User Author { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public string Content { get; set; }
+        string Name { get; set; }
+        Teacher[] teachers { get; set; }
+        Pupil[] pupils { get; set; }
+        Schedule schedule { get; set; }
+    }
+
+    class Schedule
+    {
+        DateTime DayOfWeek { get; set; }
+        Lesson[] Lessons { get; set; }
+    }
+
+    class Lesson
+    {
+        string NameLesson { get; set; }
+        TimeOnly TimeStart { get; set; }
+        TimeOnly TimeEnd { get; set; }
+    }
+
+    class Class
+    {
+        Teacher[] Teachers { get; set; }
+        Pupil[] Pupils { get; set; }
+        string[] Schedule { get; set; }
+    }
+    class Teacher
+    {
+        string FirstName { get; init; }
+        string LastName { get; init; }
+        string FullName => $"{FirstName} {LastName}";
+        string PhoneNumber { get; set; }
+        string Email { get; set; }
 
     }
 
-    public class User
+    class Pupil
     {
-        public string UserName { get; set; }
-        private string Password { get; set; }
-        public long Id { get; init; }
-        public string Email { get; set; }
-        public byte[] Avatar { get; set; }
-        public User[] Followers { get; set; }
-    }
-
-    public class Post
-    {
-        public User User { get; set; }
-        public long Id { get; init; }
-        public DateTime CreatedAt { get; set; }
-        public byte[] Content { get; set; }
-        public string Description { get; set; }
-        public Comment[] Comment { get; set; }
-    }
-
-    public class Feed
-    {
-        public Post[] Posts { get; set; }
-        public User Owner { get; set; }
-    }
-
-    public class Updatable
-    {
-        private int _fieldA;
-        private int _fieldB;
-        public int FieldA
-        {
-            get
-            {
-                return _fieldA;
-            }
-            set
-            {
-                ++TimeUpdated;
-                _fieldA = value;
-            }
-
-        }
-        public string FieldB { get; set; }
-        public int TimeUpdated { get; private set; }
+        string FirstName { get; init; }
+        string LastName { get; init; }
+        string FullName => $"{FirstName} {LastName}";
+        string PhoneNumber { get; set; }
     }
 
     public class Program
     {
         static void Main()
         {
-            
+           
         }           
     }
 }
