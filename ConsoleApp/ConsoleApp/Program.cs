8f01aa1bbe5761c43ee907c41c9ccc496e128e0d
d.misik <misik.dmitriy@gmail.com>
2021-12-13 20:36:16 +0200

    open/closed

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,6 +2,8 @@
 
 namespace ConsoleApp
 {
+    #region Single Responsibility
+
     class EmailService
     {
         public void SendEmail(string username) => Console.WriteLine($"Email sent to {username}");
@@ -24,6 +26,77 @@ namespace ConsoleApp
             return false;
         }
     }
+
+    #endregion
+
+    #region Opend/closed
+
+    enum AnimalType
+    {
+        Cat,
+        Dog,
+        Fish
+    }
+
+    class WrongAnimal
+    {
+        public AnimalType AnimalType { get; set; }
+
+        public void MakeNoise()
+        {
+            Console.WriteLine($"{AnimalType} says {Noise()}");
+        }
+
+        private string Noise() => AnimalType switch
+        {
+            AnimalType.Cat => "meow",
+            AnimalType.Dog => "ruff",
+            AnimalType.Fish => "bulp",
+            _ => throw new ArgumentOutOfRangeException(nameof(AnimalType)),
+        };
+
+        public void Swim()
+        {
+            Console.WriteLine($"{AnimalType} swim like a {SwimLike()}");
+        }
+
+        private string SwimLike() => AnimalType switch
+        {
+            AnimalType.Cat => throw new NotImplementedException("Cat cannot swim"),
+            AnimalType.Dog => throw new NotImplementedException("Dog cannot swim"),
+            AnimalType.Fish => "fish",
+            _ => throw new ArgumentOutOfRangeException(nameof(AnimalType)),
+        };
+    }
+
+    abstract class RightAnimal
+    {
+        protected abstract string AnimalType { get; }
+
+        public void MakeNoise()
+        {
+            Console.WriteLine($"{AnimalType} says {Noise()}");
+        }
+
+        abstract protected string Noise();
+    }
+
+    class Cat : RightAnimal
+    {
+        protected override string AnimalType => "Cat";
+        protected override string Noise() => "meow";
+    }
+
+    class Fish : RightAnimal
+    {
+        protected override string AnimalType => "Fish";
+        protected override string Noise() => "bulp";
+
+
+        public void Swim() => Console.WriteLine($"Fish swim like a fish");
+    }
+
+    #endregion
 
     class Program
     {
