f2cbf36aa46303b2bf7a7b4cb4db69a4cfe80fe9
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2021-12-15 16:57:34 +0200

    Interface Product

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -3,56 +3,47 @@ using System.IO;
 
 namespace ConsoleApp
 {
-    class Auto
+    interface IPrice
     {
-        public int AmountOfWheels { get; set; }
-        public string TypeOfEngine { get; set; }
-        public string TypeOfAuto { get; set; }
-        public string Color { get; set; }
+        double ProductPrice(double i1);
+        double Discount(double i2);
+    }
+        
+    interface IQuantity
+    {
+        string AmountOfProduct (string i1);
 
+        bool Availablilty();
     }
 
-    class TypeOfTheTransport
+    interface IID
     {
-        public string PassengerCar { get; set; }
-        public string Truck { get; set; }
-        public string Minivan { get; set; }
-        public string Motorcycle { get; set; }
-        public string ATV { get; set; }
-        public string MissleComplex { get; set; }
-
+        int Gtin13(int i1);
+        int Mpn(int i1);
+        int Scu(int i1);
+    }
+    interface IName
+    {
+        string NameOfTheProduct(string i1);
+        string Brand (string i1);
+       
     }
 
-    class TypeOfTheProblam
+    class Product : IPrice, IQuantity, IID, IName
     {
-        public string Scratch { get; set; }
-        public string EngineProblam { get; set; }
-        public string ProblamWithTheChassis { get; set; }
-        public string Painting { get; set; }
-        public string Crash { get; set; }
-        public string Electronics { get; set; }
+        public double ProductPrice(double price) => price;
+        public double Discount(double discount) => discount;
+        public string AmountOfProduct (string product) => product;
+        public bool Availablilty() => true;
+        public int Gtin13(int i1) => throw new NotImplementedException();
+        public int Mpn(int i1) => throw new NotImplementedException();
+        public int Scu(int i1) => throw new NotImplementedException();
+        public string NameOfTheProduct(string i1) => throw new NotImplementedException();
+        public string Brand(string i1) => throw new NotImplementedException();
+
+        string discription();
 
     }
-
-    class Client
-    {
-        private string FirstName { get; set; }
-        private string LastName { get; set; }
-        private object DocumentCopy { get; set; }
-        private string Sex { get; set; }
-
-    }
-
-    class Master
-    {
-        private string FirstName { get; set; }
-        private string LastName { get; set; }
-        private int HowOld { get; set; }
-        public string Qualification { get; set; }
-        public string Proffession { get; set; }
-        public string Experience { get; set; }
-    }
-
     class Program
     {        
         static void Main()
