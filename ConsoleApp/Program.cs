8179b8a5e05bf974b858ffdf9489a1f77ede59b1
a.tkachenko <your_email@whatever.com>
2021-12-15 12:51:58 +0200

    update admin

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -34,7 +34,7 @@
         }
         interface IProduct
         {
-            Product AddProduct(int ID, string name, string Category, string Description, int Price);
+            void AddProduct(int ID, string name, string Category, string Description, int Price);
             public void EditProduct(ref Product product, int id, string name, string category, string description, int price);
             bool DeleteProduct(ref Product product, int ID);
         }
@@ -46,14 +46,19 @@
         {
             int ID;
             string Name;
+            private Product [] _product;
             public Admin(int id, string name)
             {
                 this.ID = id;
                 this.Name = name;
             }
-            public Product AddProduct(int id, string name, string category, string description, int price)
+            public void InitProductDateBase(Product [] prduct)
             {
-                return new Product(ID, name, category, description, price);
+                _product = prduct;
+            }
+            public void AddProduct(int id, string name, string category, string description, int price)
+            {
+                _product[id] = new Product(id, name, category, description, price);
             }
 
             public void EditProduct(ref Product product, int id, string name, string category, string description, int price)
@@ -92,16 +97,34 @@
         }
         class InternetShop
         {
-            public Product[] _products { get; set; }
-            public Admin _admin { get; set; }
+            private const int _maximumNumberOfProducts = 3;
+            private Product[] _products { get; set; }
+            public Admin Admin { get; set; }
             public Buyer[] buyers { get; set; }
+            public InternetShop()
+            {
+                _products = new Product[_maximumNumberOfProducts];
+            }
+            public Product[] EnableDataBaseForAdmin(Admin admin)
+            {
+                if (this.Admin == admin)
+                {
+                    return _products;
+                }
+               else
+                {
+                    throw (new Exception("You do not have permission!"));
+                }
+            }
         }
         static void Main(string[] args)
         {
             Console.WriteLine("\r\n a.tkachenko/homework/13-InternetShop \r\n");
 
             InternetShop CarShop = new InternetShop();
-            CarShop._products = new Product[3];
+            Admin admin = new Admin(0, "admin");
+            CarShop.Admin = admin;
+            admin.InitProductDateBase(CarShop.EnableDataBaseForAdmin(CarShop.Admin));
             CarShop._products[0] = new Product(0, "Ford", "Car", "good car", 10000);
             CarShop._products[1] = new Product(0, "Opel", "Car", "good car", 12000);
             CarShop._products[2] = new Product(0, "Renault", "Car", "good car", 15000);
