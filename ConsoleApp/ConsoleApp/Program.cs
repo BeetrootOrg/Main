be5c398600fb7b4a4c330d8974aaeafbcf809fbc
d.misik <misik.dmitriy@gmail.com>
2021-12-15 19:19:10 +0200

    struct

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,27 +2,43 @@
 
 namespace ConsoleApp
 {
+    struct Complex : IEquatable<Complex>
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public override bool Equals(object obj) => obj is Complex complex && Equals(complex);
+
+        public bool Equals(Complex other) => Real == other.Real &&
+                   Imaginary == other.Imaginary;
+
+        public override int GetHashCode() => HashCode.Combine(Real, Imaginary);
+    }
+
     class Program
     {
         static void Main()
         {
-            Random rand = new((int)DateTime.Now.Ticks);
-
-            Console.WriteLine("Start the game");
-            while (true)
+            Complex complex1 = new()
             {
-                var name = Console.ReadLine();
-                var random = rand.Next(0, 6);
-                if (random == 0)
-                {
-                    Console.WriteLine($"{name}, you killed!");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"{name} continues game...");
-                }
-            }
+                Imaginary = 1,
+                Real = 2
+            };
+
+            Complex complex2 = new()
+            {
+                Imaginary = 1,
+                Real = 2
+            };
+
+            Complex complex3 = new()
+            {
+                Imaginary = 1.5,
+                Real = 2
+            };
+
+            Console.WriteLine(complex1.Equals(complex2));
+            Console.WriteLine(complex2.Equals(complex3));
         }
     }
 }
