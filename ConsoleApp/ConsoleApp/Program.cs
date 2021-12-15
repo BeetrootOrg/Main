1c54f02da3def95cff5f350c741acafd647eef3d
d.misik <misik.dmitriy@gmail.com>
2021-12-15 19:30:56 +0200

    operator+

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,12 +7,23 @@ namespace ConsoleApp
         public double Real { get; set; }
         public double Imaginary { get; set; }
 
+        public Complex(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
         public override bool Equals(object obj) => obj is Complex complex && Equals(complex);
 
         public bool Equals(Complex other) => Real == other.Real &&
                    Imaginary == other.Imaginary;
 
         public override int GetHashCode() => HashCode.Combine(Real, Imaginary);
+
+        public override string ToString() => $"{Real}+{Imaginary}i";
+
+        public static Complex operator +(Complex complex1, Complex complex2) =>
+            new(complex1.Real + complex2.Real, complex1.Imaginary + complex2.Imaginary);
     }
 
     class Program
@@ -34,11 +45,16 @@ namespace ConsoleApp
             Complex complex3 = new()
             {
                 Imaginary = 1.5,
-                Real = 2
+                Real = 3
             };
 
             Console.WriteLine(complex1.Equals(complex2));
             Console.WriteLine(complex2.Equals(complex3));
+
+            Console.WriteLine(complex1);
+
+            var sum = complex1 + complex3;
+            Console.WriteLine(sum);
         }
     }
 }
