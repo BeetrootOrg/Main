219e45725ec068e9f2ac3465950d0c04135baa66
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2021-12-15 20:02:19 +0200

    some

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,39 +1,116 @@
-﻿namespace ConsoleApp
+﻿using System;
+
+namespace ConsoleApp
 {
-    interface IAddTwoNumbers
+    struct Complex : IEquatable<Complex>
     {
-        int Add(int i1, int i2);
-    }
+        public double Real { get; init; }
+        public double Imaginary { get; init; }
 
-    interface IMultiplyTwoNumbers
-    {
-        int Mul(int i1, int i2);
-    }
+        public Complex(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
 
-    public class Calculator : IAddTwoNumbers, IMultiplyTwoNumbers
-    {
-        public int Add(int i1, int i2) => i1 + i2;
-        public int Mul(int i1, int i2) => i1 * i2;
-    }
+        public Complex(Complex another)
+        {
+            Real = another.Real;
+            Imaginary = another.Imaginary;
+        }
 
-    public class WrongAddCalculator : IAddTwoNumbers
-    {
-        public int Add(int i1, int i2) => i1 - i2;
+        public override bool Equals(object obj) => obj is Complex complex && Equals(complex);
+
+        public bool Equals(Complex other) => Real == other.Real &&
+                   Imaginary == other.Imaginary;
+
+        public override int GetHashCode() => HashCode.Combine(Real, Imaginary);
+
+        public override string ToString() => $"{Real}+{Imaginary}i";
+
+        public static Complex operator +(Complex complex1, Complex complex2) =>
+            new(complex1.Real + complex2.Real, complex1.Imaginary + complex2.Imaginary);
+
+        public static Complex operator *(Complex complex1, Complex complex2) =>
+            new(complex1.Real * complex2.Real - complex1.Imaginary * complex2.Imaginary,
+                complex1.Real * complex2.Imaginary + complex1.Imaginary * complex2.Real);
+
+        public static bool operator ==(Complex complex1, Complex complex2) => complex1.Equals(complex2);
+        public static bool operator !=(Complex complex1, Complex complex2) => !complex1.Equals(complex2);
+
+        public static explicit operator double(Complex complex) =>
+            Math.Sqrt(Math.Pow(complex.Real, 2) + Math.Pow(complex.Imaginary, 2));
+
+        public double this[int i]
+        {
+            get
+            {
+                return i switch
+                {
+                    0 => Real,
+                    1 => Imaginary,
+                    _ => throw new ArgumentOutOfRangeException(nameof(i))
+                };
+            }
+            /* set 
+            { 
+                switch(i)
+                {
+                    case 0:
+                        Real = value;
+                        break;
+                    case 1:
+                        Imaginary = value;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(i));
+                }
+            } */
+        }
     }
 
     class Program
     {
         static void Main()
         {
-            Calculator calculator = new Calculator();
-            WrongAddCalculator wrongCalculator = new WrongAddCalculator();
-            System.Console.WriteLine(AddTwoNumbers(calculator, 1, 2));
-            System.Console.WriteLine(AddTwoNumbers(wrongCalculator, 1, 2));
+            Complex complex1 = new()
+            {
+                Imaginary = 1,
+                Real = 2
+            };
 
-            System.Console.WriteLine(MulTwoNumbers(calculator, 1, 2));
+            Complex complex2 = new()
+            {
+                Imaginary = 1,
+                Real = 2
+            };
+
+            Complex complex3 = new()
+            {
+                Imaginary = 1.5,
+                Real = 3
+            };
+
+            Console.WriteLine(complex1.Equals(complex2));
+            Console.WriteLine(complex2.Equals(complex3));
+
+            Console.WriteLine(complex1);
+
+            var sum = complex1 + complex3;
+            Console.WriteLine(sum);
+
+            Console.WriteLine($"{complex1}*{complex3}={complex1 * complex3}");
+            Console.WriteLine(complex1 == complex2);
+
+            double d = (double)complex1;
+            Console.WriteLine(d);
+
+            // complex[0] -> real
+            // complex[1] -> imaginary
+            Console.WriteLine(complex1[0]);
+            Console.WriteLine(complex1[1]);
+
+            Console.WriteLine(complex1);
         }
-
-        static int AddTwoNumbers(IAddTwoNumbers addTwoNumbers, int i1, int i2) => addTwoNumbers.Add(i1, i2);
-        static int MulTwoNumbers(IMultiplyTwoNumbers addTwoNumbers, int i1, int i2) => addTwoNumbers.Mul(i1, i2);
     }
 }
