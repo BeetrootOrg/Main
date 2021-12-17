3f06421d9dcc03fd28a9e25f5a4ab95152e15e63
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2021-12-17 22:17:44 +0200

    price update

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -3,55 +3,102 @@ using System.IO;
 
 namespace ConsoleApp
 {
+    //const string Filename = @"discription.csv";
+
     interface IPrice
     {
-        double ProductPrice(double i1);
-        double Discount(double i2);
+        double ProductPrice(double oldPrice, double discount);
+                
     }
-        
-    interface IQuantity
+    
+    interface IAmountOfProduct
     {
-        string AmountOfProduct (string i1);
-
-        bool Availablilty();
+        int AmountOfProduct(int amountOfProdut);
+    }
+    interface IStockStatus
+    {
+        string StockStatus(string Stock);
     }
 
     interface IID
     {
-        int Gtin13(int i1);
-        int Mpn(int i1);
-        int Scu(int i1);
+        int Gtin13(int Gtin13);
+        int Mpn(int Mpn);
+        int Scu(int Scu);
     }
     interface IName
     {
-        string NameOfTheProduct(string i1);
-        string Brand (string i1);
+        string NameOfTheProduct(string NameOfTheProduct);
+        string Brand (string BrandName);
        
     }
 
-    class Product : IPrice, IQuantity, IID, IName
+    class Product : IPrice
     {
-        public double ProductPrice(double price) => price;
-        public double Discount(double discount) => discount;
-        public string AmountOfProduct (string product) => product;
-        public bool Availablilty() => true;
-        public int Gtin13(int i1) => throw new NotImplementedException();
-        public int Mpn(int i1) => throw new NotImplementedException();
-        public int Scu(int i1) => throw new NotImplementedException();
-        public string NameOfTheProduct(string i1) => throw new NotImplementedException();
-        public string Brand(string i1) => throw new NotImplementedException();
-
-        string discription();
-
+        double discountPrice;
+       public double ProductPrice(double oldPrice, double discount)
+        //=> oldPrice * discount;
+        {
+            if (discount > 0 && discount <=99)
+            {
+                discountPrice = oldPrice * (discount/100);
+            }
+            else
+            {
+                discountPrice = oldPrice;
+            }
+            return discountPrice;
+        }
+             
     }
+
+    class Availability : IAmountOfProduct
+    {
+
+        public int AmountOfProduct(int amountOfProdut)
+        {
+            string stockStatus;
+
+            
+            return amountOfProdut;
+        }
+    }
+
+    class StockStatus : Availability, IStockStatus
+    {
+        string IStockStatus.StockStatus(string Stock)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //class Specs : IName, IID, IPrice
+    //{
+    //    public string Brand(string BrandName)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public string NameOfTheProduct(string NameOfTheProduct)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+
     class Program
     {        
         static void Main()
         {
-            
+            Product product = new Product();
+            Availability availability = new Availability(); 
+            Console.WriteLine(Price(product,10,70));
+            Console.WriteLine(Availability(availability,10));
+
             
         }
 
-        
+        static double Price(IPrice price, double oldPrice, double discount) => price.ProductPrice(oldPrice, discount);
+        static double Availability(IAmountOfProduct availability, int amountOfProdut) => availability.AmountOfProduct(amountOfProdut);
     }
 }
