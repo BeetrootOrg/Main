dcb82837b6a5bbbc66169a9fbbc4fb20ce227d2d
oleksandrmartynenko <81743146+oleksandrmartynenko@users.noreply.github.com>
2021-12-18 16:51:00 +0200

    dd

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,21 +1,19 @@
 ﻿using System;
+
 namespace ConsoleApp
 {
-
     class Animal
     {
         public int NumOfPaws { get; set; }
         public int Length { get; set; }
-        public virtual bool HasTail { get; set; }
+        public virtual bool HasTale { get; set; }
         public string Color { get; set; }
 
-        public virtual string MakeNoise() => "Unknown animal says ???";
-        public void Eat() => Console.WriteLine("Unknown animal eats ???");
+        public virtual string MakeNoise() => "Unknown Animal says ???";
+        public void Eat() => Console.WriteLine("Unknwon Animal eats ???");
+        public virtual string GetAnimalType() => "?";
 
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
+        public override bool Equals(object obj) => Equals(obj as Animal);
 
         protected virtual bool Equals(Animal animal)
         {
@@ -23,32 +21,50 @@ namespace ConsoleApp
             {
                 return false;
             }
+
             if (ReferenceEquals(this, animal))
             {
                 return true;
             }
+
             if (this.GetType() != animal.GetType())
             {
                 return false;
             }
 
-            return NumOfPaws == animal.NumOfPaws && Length == animal.Length && HasTail == animal.HasTail && Color == animal.Color;
+            return NumOfPaws == animal.NumOfPaws &&
+                Length == animal.Length &&
+                HasTale == animal.HasTale &&
+                Color == animal.Color;
         }
 
+        public override int GetHashCode() => NumOfPaws.GetHashCode() ^
+            Length.GetHashCode() ^
+            HasTale.GetHashCode() ^
+            Color.GetHashCode();
 
-
-        public override int GetHashCode() => NumOfPaws.GetHashCode() ^ Length.GetHashCode() ^ HasTail.GetHashCode() ^ Color.GetHashCode();
-
-        public override string ToString() => $"{nameof(NumOfPaws)}={NumOfPaws}";
+        public override string ToString() => $"{nameof(NumOfPaws)} = {NumOfPaws}; " +
+            $"{nameof(Length)} = {Length}; " +
+            $"{nameof(HasTale)} = {HasTale}; " +
+            $"{nameof(Color)} = {Color}";
     }
 
     class Cat : Animal
     {
         public string Breed { get; set; }
+        public override bool HasTale { get => true; set => throw new NotImplementedException("Cannot change tale"); }
 
         public override string MakeNoise() => "Cat says 'meow'";
-        public new void Eat() => Console.WriteLine("cat eats fish");
+        public new void Eat() => Console.WriteLine("Cat eats fish");
+
+        public override string GetAnimalType() => $"this is a cat {Breed}";
     }
+
+    class Dog : Animal
+    {
+        public override string GetAnimalType() => "this is a dog";
+    }
+
     class Program
     {
         static void Main()
@@ -56,28 +72,83 @@ namespace ConsoleApp
             var animal = new Animal
             {
                 Color = "red",
-                HasTail = true,
+                HasTale = true,
                 NumOfPaws = 5,
                 Length = -1
             };
+
             var cat = new Cat
             {
                 Color = "black",
-                HasTail = true,
-                NumOfPaws = 4,
-                Length = 30
+                Length = 30,
+                NumOfPaws = 4
             };
 
+            Console.WriteLine("ANIMAL");
+            ShowAnimal(animal);
+
+            Console.WriteLine("CAT");
+            ShowAnimal(cat);
+
+            Console.WriteLine(cat.HasTale);
+
+            Console.WriteLine(animal);
             Console.WriteLine(cat);
 
-            static void ShowAnimal(Animal animal)
+            var animal2 = new Animal
             {
-                Console.WriteLine(animal.MakeNoise());
-                animal.Eat();
+                Color = "red",
+                HasTale = true,
+                NumOfPaws = 5,
+                Length = -1
+            };
 
-            }
+            Console.WriteLine(animal == animal2);
+            Console.WriteLine(animal.Equals(animal2));
+            Console.WriteLine(animal.Equals(new object()));
+            Console.WriteLine(animal.GetHashCode());
+            Console.WriteLine(animal2.GetHashCode());
 
+            Console.WriteLine(GetAnimalType1(cat));
+            Console.WriteLine(GetAnimalType1(new Dog()));
+
+            Console.WriteLine(GetAnimalType2(cat));
+            Console.WriteLine(GetAnimalType2(new Dog()));
+
+            Console.WriteLine(GetAnimalType3(animal));
+            Console.WriteLine(GetAnimalType3(cat));
+            Console.WriteLine(GetAnimalType3(new Dog()));
         }
 
+        static void ShowAnimal(Animal animal)
+        {
+            Console.WriteLine(animal.MakeNoise());
+            animal.Eat();
+        }
+
+        // worse approach
+        static string GetAnimalType1(Animal animal)
+        {
+            if (animal is Cat cat)
+            {
+                return $"this is a cat {cat.Breed}";
+            }
+
+            if (animal is Dog)
+            {
+                return $"this is a dog";
+            }
+
+            return "?";
+        }
+
+        // better approach - dynamic polimorphism
+        static string GetAnimalType2(Animal animal) => animal.GetAnimalType();
+
+        // better approach - static polimorphism
+        // overload
+        static string GetAnimalType3(Animal animal) => "?";
+        static string GetAnimalType3(Cat cat) => $"this is a cat {cat.Breed}";
+        static string GetAnimalType3(Dog dog) => $"this is a dog";
     }
 }
