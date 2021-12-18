bf642daa98f320061541913be4df430662674cfd
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2021-12-18 19:56:56 +0200

    autoService

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,68 +2,6 @@
 
 namespace ConsoleApp
 {
-    enum Subject
-    {
-        Math,
-        Chemistry,
-        Biology,
-        Law,
-        History,
-        Literature
-    }
-
-    class School
-    {
-        public string Number { get; init; }
-        public string Adress { get; init; }
-        public int QuantityOfRooms { get; set; }
-    
-    }
-
-    class Teacher
-    {
-        public string FirstName { get; init; }
-        public string LastName { get; init;  }
-        private DateTime DateOfBirth{ get; init; }
-        public DateTime Experience { get; set; }
-        private string  HomeAdress{ get; set; }
-        private string PhoneNumber { get; set; }
-        public Subject Subject { get; set; }
-
-    }
-
-    class Schedule
-    {
-        public string Classes { get; set; }
-        public Subject Subjects { get; set; }
-        public int Rooms { get; set; }
-        public string Teachers {get;set;}
-
-  
-    }
-
-    class Class
-    {   
-        public int String{ get; init; }
-        public int Grade{ get; set; }
-        public string ClassTeacher { get; set; }
-        private int ClassRoom { get; set; }
-        
-    }
-
-    class Student
-    {
-        public string FirstName { get; init; }
-        public string LastName { get; init; }
-        public DateTime DateOfBirth { get; init; }
-        public string HomeAdress { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Class { get; set; }
-
-     
-    }
-
-
 
     class Program
     {
