47aa48d3cef77069cd8f8885cecb2e3500d241d9
Mour-Yam <70475271+Mour-Yam@users.noreply.github.com>
2021-12-18 12:01:46 +0200

    i.safontev/classwork/15-generics

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,123 +2,220 @@
 
 namespace ConsoleApp
 {
-    //i.safontev/classwork/14-records
-    #region Record
+    //i.safontev/classwork/15-generics
+    #region Interfaces
 
-    record struct Complex(double Real, double Imaginary) : IEquatable<Complex>
+    public interface IList
     {
-        public static Complex operator +(Complex complex1, Complex complex2) =>
-            new(complex1.Real + complex2.Real, complex1.Imaginary + complex2.Imaginary);
+        int Count { get; }
+        void Clear();
+        void Remove(int index);
+    }
 
-        public static Complex operator *(Complex complex1, Complex complex2) =>
-            new(complex1.Real * complex2.Real - complex1.Imaginary * complex2.Imaginary,
-                complex1.Real * complex2.Imaginary + complex1.Imaginary * complex2.Real);
 
-        public static explicit operator double(Complex complex) =>
-            Math.Sqrt(Math.Pow(complex.Real, 2) + Math.Pow(complex.Imaginary, 2));
+    public interface IReadOnlyList<out T> : IList
+    {
+        T[] GetAll();
+        T this[int index] { get; }
+    }
 
-        public double this[int i]
+    public interface IWriteOnlyList<in T> : IList
+    {
+        void Add(T item);
+        void Insert(T item, int index);
+        T this[int index] { set; }
+    }
+
+    #endregion
+
+    #region LinkedList
+
+    public class LinkedList<T> : IReadOnlyList<T>, IWriteOnlyList<T>
+    {
+        private class ListItem
         {
-            get
+            public T Value { get; set; }
+            public ListItem Next { get; set; }
+        }
+
+        private ListItem _head;
+        public int Count { get; private set; }
+
+        public void Add(T item) => Insert(item, Count);
+
+        public T this[int index]
+        {
+            get => GetByIndex(index).Value;
+            set
             {
-                return i switch
+                var item = GetByIndex(index);
+                item.Value = value;
+            }
+        }
+
+        public T[] GetAll()
+        {
+            var array = new T[Count];
+            var item = _head;
+
+            for (int i = 0; i < Count; ++i)
+            {
+                array[i] = item.Value;
+                item = item.Next;
+            }
+
+            return array;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            Count = 0;
+        }
+
+        public void Remove(int index)
+        {
+            if (index == Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            if (index == 0)
+            {
+                _head = _head.Next;
+            }
+            else
+            {
+                var prev = GetByIndex(index - 1);
+                prev.Next = prev.Next.Next;
+            }
+
+            --Count;
+        }
+
+        public void Insert(T item, int index)
+        {
+            if (index == 0)
+            {
+                var listItem = new ListItem
                 {
-                    0 => Real,
-                    1 => Imaginary,
-                    _ => throw new ArgumentOutOfRangeException(nameof(i))
+                    Value = item,
+                    Next = _head
                 };
+
+                _head = listItem;
             }
-        }
-    }
-
-    #endregion
-
-    #region Readonly Struct
-
-    readonly struct ReadonlyStruct
-    {
-        public int MyProperty { get; }
-
-        // next line = compilation error
-        // public int MyProperty1 { get; set; }
-    }
-
-    #endregion
-
-    #region Sealed class
-
-    public sealed class A
-    {
-        public void MethodA()
-        {
-            Console.WriteLine("A");
-        }
-    }
-
-    // compilation error - cannot derive
-    //public class B : A
-    //{
-    //}
-
-    #endregion
-
-    #region IComparable
-
-    struct Number : IComparable<Number>, IFormattable
-    {
-        public double Num { get; set; }
-        public int CompareTo(Number other)
-        {
-            if (Num - other.Num < 0) return -1;
-            if (Num - other.Num > 0) return 1;
-            return 0;
-        }
-
-        public string ToString(string format, IFormatProvider formatProvider)
-        {
-            if (format == "F")
+            else
             {
-                return $"{Num:F3}";
+                var prev = GetByIndex(index - 1);
+
+                var listItem = new ListItem
+                {
+                    Value = item,
+                    Next = prev.Next
+                };
+
+                prev.Next = listItem;
             }
 
-            return Num.ToString();
+            ++Count;
+        }
+
+        private ListItem GetByIndex(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            var item = _head;
+            for (int i = 0; i < index; i++)
+            {
+                item = item.Next;
+            }
+
+            return item;
         }
     }
 
     #endregion
-
 
     class Program
     {
         static void Main()
         {
-            Complex complex1 = new(1, 2);
-            Complex complex2 = new(1, 2);
-            Complex complex3 = new(1.5, 3);
+            var int1 = 5;
+            var int2 = 7;
 
-            Console.WriteLine(complex1.Equals(complex2));
-            Console.WriteLine(complex2.Equals(complex3));
+            Swap(ref int1, ref int2);
 
-            Console.WriteLine(complex1);
+            var str1 = "string";
+            var str2 = "hello";
+            Swap(ref str1, ref str2);
 
-            var sum = complex1 + complex3;
-            Console.WriteLine(sum);
+            // ShowArray(new[] { 1, 2, 3 });
+            // ShowArray(new[] { "hello", "Dima" });
 
-            Console.WriteLine($"{complex1}*{complex3}={complex1 * complex3}");
-            Console.WriteLine(complex1 == complex2);
+            var list = new LinkedList<string>();
+            list.Add("el1");
+            list.Add("el2");
+            list.Add("el3");
+            ShowArray(list.GetAll());
 
-            double d = (double)complex1;
-            Console.WriteLine(d);
+            list[0] = "el4";
 
-            // complex[0] -> real
-            // complex[1] -> imaginary
-            Console.WriteLine(complex1[0]);
-            Console.WriteLine(complex1[1]);
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(list[2]);
 
-            Console.WriteLine(complex1);
+            ShowArray(list.GetAll());
 
-            var num = new Number { Num = 5.1234 };
-            Console.WriteLine($"{num:F}");
+            // ShowRandomListElement(list);
+            // SetRandomListElement(list, "element");
+
+            list[0] = "el0";
+            list[1] = "el1";
+            list[2] = "el2";
+
+            list.Remove(0);
+            list.Add("el3");
+            list.Remove(2);
+            list.Add("el4");
+            list.Remove(1);
+
+            Console.WriteLine("AFTER REMOVE");
+            ShowArray(list.GetAll());
+
+            list.Insert("insert0", 0);
+            list.Insert("insert1", 3);
+            list.Insert("insert2", 2);
+
+            list.Clear();
+        }
+
+        static void Swap<T>(ref T val1, ref T val2)
+        {
+            (val1, val2) = (val2, val1);
+        }
+
+        static void ShowArray<TElement>(TElement[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void ShowRandomListElement<T>(IReadOnlyList<T> list)
+        {
+            var index = new Random((int)DateTime.Now.Ticks).Next(0, list.Count);
+            Console.WriteLine(list[index]);
+        }
+
+        static void SetRandomListElement<T>(IWriteOnlyList<T> list, T element)
+        {
+            var index = new Random((int)DateTime.Now.Ticks).Next(0, list.Count);
+            list[index] = element;
         }
     }
 }
