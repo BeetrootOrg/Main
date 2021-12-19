57d9e5b908ff53b2d65982cbf6e956ae2fe213e6
a.tkachenko <some_email@whatever.com>
2021-12-19 19:33:43 +0200

    refactor

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,16 +10,17 @@
         {
             int Count { get; }
             void Clear();
-            void Remove(int index);
         }
         public interface IReadOnlyList<out T> : IList
         {
             T[] GetAll();
+            public T Pop();
+            public T Peek();
             T this[int index] { get; }
         }
         public interface IWriteOnlyList<in T> : IList
         {
-            void Add(T item);
+            public void Push(T item);
             void Insert(T item, int index);
             T this[int index] { set; }
         }
@@ -30,17 +31,13 @@
                 public T Value { get; set; }
                 public ListItem Next { get; set; }
             }
-
             private ListItem _head;
             public int Count { get; private set; }
-
-            public void Add(T item) => Insert(item, Count);
-            public void Push(T item) //  - adds obj at the top of stack
+            public void Push(T item)
             {
                 Console.WriteLine("Push: {0}", item);
                 Insert(item, Count);
             }
-
             public T this[int index]
             {
                 get => GetByIndex(index).Value;
@@ -50,7 +47,6 @@
                     item.Value = value;
                 }
             }
-
             public T[] GetAll()
             {
                 var array = new T[Count];
@@ -64,13 +60,11 @@
 
                 return array;
             }
-
             public void Clear()
             {
                 _head = null;
                 Count = 0;
             }
-
             private T ShowAndRemove(int index)
             {
                 T retValue;
@@ -117,25 +111,6 @@
                 }
                 return retValue;
             }
-            public void Remove(int index)
-            {
-                if (index == Count)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(index));
-                }
-
-                if (index == 0)
-                {
-                    _head = _head.Next;
-                }
-                else
-                {
-                    var prev = GetByIndex(index - 1);
-                    prev.Next = prev.Next.Next;
-                }
-
-                --Count;
-            }
             public T Pop()
             {
                 try
@@ -144,7 +119,6 @@
                 }
                 catch (Exception ex)
                 {
-                    // Console.WriteLine(" - " + ex.Message);
                     return default(T);
                 }
             }
@@ -160,10 +134,20 @@
                     return default(T);
                 }
             }
-            //void CopyTo(/*arr*/) // - copies stack to array
-            //{
-
-            //}
+            public void CopyTo(T [] array)
+            {
+                if(Count <= 0)
+                {
+                    array = null;
+                    return;
+                }
+                var item = _head;
+                for (int i = 0; i < Count; ++i)
+                {
+                    array[i] = item.Value;
+                    item = item.Next;
+                }
+            }
             public void Insert(T item, int index)
             {
                 if (index == 0)
@@ -191,7 +175,6 @@
 
                 ++Count;
             }
-
             private ListItem GetByIndex(int index)
             {
                 if (index < 0 || index >= Count)
@@ -208,7 +191,6 @@
                 return item;
             }
         }
-
         #endregion
         static void Main(string[] args)
         {
@@ -216,45 +198,50 @@
 
             try
             {
-                var list = new LinkedList<string>();
-                list.Push("el1");
-                ShowArray(list.GetAll());
-                list.Push("el2");
-                ShowArray(list.GetAll());
-                list.Push("el3");
-                ShowArray(list.GetAll());
+                var stack = new LinkedList<string>();
+                stack.Push("el1");
+                ShowArray(stack.GetAll());
+                stack.Push("el2");
+                ShowArray(stack.GetAll());
+                stack.Push("el3");
+                ShowArray(stack.GetAll());
 
-                Console.WriteLine("Stack Count: {0}\r\n", list.Count);
+                Console.WriteLine("Stack Count: {0}\r\n", stack.Count);
 
-                list.Clear();
-                Console.WriteLine("Stack Count: {0}\r\n", list.Count);
-                ShowArray(list.GetAll());
+                stack.Clear();
+                Console.WriteLine("Stack Count: {0}\r\n", stack.Count);
+                ShowArray(stack.GetAll());
 
-                list.Push("el4");
-                ShowArray(list.GetAll());
-                list.Push("el5");
-                ShowArray(list.GetAll());
-                list.Push("el6");
-                ShowArray(list.GetAll());
-                list.Push("el7");
-                ShowArray(list.GetAll());
+                stack.Push("el4");
+                ShowArray(stack.GetAll());
+                stack.Push("el5");
+                ShowArray(stack.GetAll());
+                stack.Push("el6");
+                ShowArray(stack.GetAll());
+                stack.Push("el7");
+                ShowArray(stack.GetAll());
 
-                Console.WriteLine("Pop: {0}", list.Pop());
-                ShowArray(list.GetAll());
-                Console.WriteLine("Pop: {0}", list.Pop());
-                ShowArray(list.GetAll());
+                string [] StackCopy = new string[stack.Count];
+                stack.CopyTo(StackCopy);
+                Console.Write("Show Copied ");
+                ShowArray(StackCopy);
 
-                Console.WriteLine("Peek: {0}", list.Peek());
-                ShowArray(list.GetAll());
+                Console.WriteLine("Pop: {0}", stack.Pop());
+                ShowArray(stack.GetAll());
+                Console.WriteLine("Pop: {0}", stack.Pop());
+                ShowArray(stack.GetAll());
 
-                Console.WriteLine("Pop: {0}", list.Pop());
-                ShowArray(list.GetAll());
-                Console.WriteLine("Pop: {0}", list.Pop());
-                ShowArray(list.GetAll());
+                Console.WriteLine("Peek: {0}", stack.Peek());
+                ShowArray(stack.GetAll());
 
-                list.Clear();
-                Console.WriteLine("Stack Count: {0}\r\n", list.Count);
-                ShowArray(list.GetAll());
+                Console.WriteLine("Pop: {0}", stack.Pop());
+                ShowArray(stack.GetAll());
+                Console.WriteLine("Pop: {0}", stack.Pop());
+                ShowArray(stack.GetAll());
+
+                stack.Clear();
+                Console.WriteLine("Stack Count: {0}\r\n", stack.Count);
+                ShowArray(stack.GetAll());
 
             }
             catch (Exception ex)
