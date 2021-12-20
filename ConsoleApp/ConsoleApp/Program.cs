d3f7e220d85a07acb7abbe92628ca1db58d27a4e
d.misik <misik.dmitriy@gmail.com>
2021-12-20 20:26:10 +0200

    timer

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,183 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace ConsoleApp
 {
-    delegate bool Dima(int number);
-
-    #region Changes
-    public class ChangesObservable<T>
-    {
-        public delegate void ChangeEventHandler(object sender, ChangeEvetArgs args);
-
-        public record ChangeEvetArgs(T PreviousValue, T NewValue);
-
-        public event ChangeEventHandler ChangeEvent;
-
-        private T _value;
-
-        public string Name { get; init; }
-
-        public T Value
-        {
-            get
-            {
-                return _value;
-            }
-            set
-            {
-                var oldValue = _value;
-                _value = value;
-                RaiseChangedEvent(oldValue, value);
-            }
-        }
-
-        protected virtual void RaiseChangedEvent(T old, T @new) => ChangeEvent?.Invoke(this, new ChangeEvetArgs(old, @new));
-
-    }
-    #endregion
-
-    #region Where Enumerable
-
-    public class WhereEnumerable<T> : IEnumerable<T>
-    {
-        private readonly IEnumerable<T> _collection;
-        private readonly Func<T, bool> _predicate;
-
-        public WhereEnumerable(IEnumerable<T> collection, Func<T, bool> predicate)
-        {
-            _collection = collection;
-            _predicate = predicate;
-        }
-
-        public IEnumerator<T> GetEnumerator() => new WhereEnumerator(_collection.GetEnumerator(), _predicate);
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
-
-        private class WhereEnumerator : IEnumerator<T>
-        {
-            private readonly IEnumerator<T> _enumerator;
-            private readonly Func<T, bool> _predicate;
-
-            public WhereEnumerator(IEnumerator<T> enumerator, Func<T, bool> predicate)
-            {
-                _enumerator = enumerator;
-                _predicate = predicate;
-            }
-
-            public T Current { get; private set; }
-
-            object System.Collections.IEnumerator.Current => Current;
-
-            public void Dispose()
-            {
-                _enumerator.Dispose();
-            }
-
-            public bool MoveNext()
-            {
-                while (_enumerator.MoveNext())
-                {
-                    if (_predicate(_enumerator.Current))
-                    {
-                        Current = _enumerator.Current;
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-
-            public void Reset()
-            {
-                _enumerator.Reset();
-            }
-        }
-    }
-
-    #endregion
-
     class Program
     {
         static void Main()
         {
-            var array = Enumerable.Range(0, 10);
-            ShowAll(FilterValues(array, (int item) => item > 5));
-            ShowAll(FilterValues(array, OddOnly));
-
-            // 1st option - decalre delegate
-            Dima evenOnly = (int item) => item % 2 == 0;
-            ShowAll(FilterValues(array, evenOnly));
-
-            // 2nd option - declare anonymous method
-            static bool OddOnly(int item) => item % 2 == 1;
-
-            var number = 0;
-            Func<int, int> calculate = (i) =>
-            {
-                number += i;
-                return number;
-            };
-
-            Func<int, int> calculate2 = (i) =>
-            {
-                var number = 0;
-                number += i;
-                return number;
-            };
-
-            calculate(5);
-            calculate(2);
-
-            Console.WriteLine("CLOSURE NUMBER");
-            Console.WriteLine(number);
-
-            var num1 = calculate2(5);
-            var num2 = calculate2(2);
-
-            Console.WriteLine("NON CLOSURE NUMBER");
-            Console.WriteLine(number);
-            Console.WriteLine(num1);
-            Console.WriteLine(num2);
-
-            var observable1 = new ChangesObservable<int> { Name = "First" };
-            var observable2 = new ChangesObservable<int> { Name = "Second" };
-
-            ChangesObservable<int>.ChangeEventHandler handler = (sender, args) =>
-            {
-                var changesObservable = sender as ChangesObservable<int>;
-                Console.WriteLine($"Value changed inside {changesObservable.Name} from {args.PreviousValue} to {args.NewValue}");
-            };
-            
-            observable1.ChangeEvent += handler;
-            observable2.ChangeEvent += handler;
-
-            observable1.Value = 1;
-            observable2.Value = 2;
-
-            ShowAll(new WhereEnumerable<int>(new[] { 1, 2, 3, 4, 0, 8, 11, 1998 }, (item) => item % 2 == 1));
-            ShowAll(new WhereEnumerable<int>(new[] { 1, 2, 3, 4, 0, 8, 11, 1998 }, (item) => item > 5));
-            ShowAll(new WhereEnumerable<int>(new[] { 1, 2, 3, 4, 0, 8, 11, 1998 }, (item) => item % 2 == 0));
-            ShowAll(new WhereEnumerable<int>(new[] { 1, 2, 3, 4, 0, 8, 11, 1998 }, (item) => item % 3 == 0));
+            ShowMessageEvery5Seconds("message");
+            Thread.Sleep(5000);
         }
 
-        public static IEnumerable<int> FilterValues(IEnumerable<int> collection, Dima predicate)
+        private static void ShowMessageEvery5Seconds(string message)
         {
-            foreach (var item in collection)
-            {
-                if (predicate(item))
-                {
-                    yield return item;
-                }
-            }
-        }
+            var timer = new Timer(Callback, null, 0, 1000);
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            Console.WriteLine("FINISHED");
+            timer.Dispose();
 
-        public static void ShowAll<T>(IEnumerable<T> collection)
-        {
-            foreach (var item in collection)
+            void Callback(object state)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(message);
             }
         }
     }
