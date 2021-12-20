4ce9943346057143c4fd5f026d659e9059e9e147
d.misik <misik.dmitriy@gmail.com>
2021-12-20 19:45:43 +0200

    observable

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,6 +6,36 @@ namespace ConsoleApp
 {
     delegate bool Dima(int number);
 
+
+
+    public class ChangesObservable<T>
+    {
+        public delegate void ChangeEventHandler(object sender, ChangeEvetArgs args);
+
+        public record ChangeEvetArgs(T PreviousValue, T NewValue);
+
+        public event ChangeEventHandler ChangeEvent;
+
+        private T _value;
+
+        public T Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                var oldValue = _value;
+                _value = value;
+                RaiseChangedEvent(oldValue, value);
+            }
+        }
+
+        protected virtual void RaiseChangedEvent(T old, T @new) => ChangeEvent?.Invoke(this, new ChangeEvetArgs(old, @new));
+
+    }
+
     class Program
     {
         static void Main()
@@ -35,11 +65,34 @@ namespace ConsoleApp
                 return number;
             };
 
-            calculate2(5);
-            calculate2(2);
+            calculate(5);
+            calculate(2);
 
-            Console.WriteLine("NUMBER");
+            Console.WriteLine("CLOSURE NUMBER");
             Console.WriteLine(number);
+
+            var num1 = calculate2(5);
+            var num2 = calculate2(2);
+
+            Console.WriteLine("NON CLOSURE NUMBER");
+            Console.WriteLine(number);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+
+            var observable = new ChangesObservable<int>();
+            observable.ChangeEvent += (sender, args) =>
+            {
+                Console.WriteLine($"Value changed from {args.PreviousValue} to {args.NewValue}");
+            };
+
+            observable.ChangeEvent += (sender, args) =>
+            {
+                Console.WriteLine($"ANOTHER HANDLER: Value changed from {args.PreviousValue} to {args.NewValue}");
+            };
+
+            observable.Value = 1;
+            observable.Value = 2;
+            observable.Value = 3;
         }
 
         public static IEnumerable<int> FilterValues(IEnumerable<int> collection, Dima predicate)
