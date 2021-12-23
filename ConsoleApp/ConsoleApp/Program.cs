e91fba5241f7ffdf34e6ee08e074f3b3e3013bdd
d.misik <misik.dmitriy@gmail.com>
2021-12-23 09:24:59 +0200

    slack integration

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,28 +1,55 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main()
+        static async Task Main()
         {
-            Random rand = new((int)DateTime.Now.Ticks);
-
-            Console.WriteLine("Start the game");
-            while (true)
+            const string slackBotToken = "SLACK_BOT_TOKEN";
+            using var httpClient = new HttpClient
             {
-                var name = Console.ReadLine();
-                var random = rand.Next(0, 6);
-                if (random == 0)
+                BaseAddress = new Uri("https://slack.com"),
+            };
+
+            var token = Environment.GetEnvironmentVariable(slackBotToken);
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new DefaultContractResolver
                 {
-                    Console.WriteLine($"{name}, you killed!");
-                    break;
+                    NamingStrategy = new SnakeCaseNamingStrategy()
                 }
-                else
-                {
-                    Console.WriteLine($"{name} continues game...");
-                }
+            };
+
+            var slackClient = new SlackApiClient(httpClient, settings);
+            var result = await slackClient.GetMembers();
+
+            if (!result.Ok)
+            {
+                throw new Exception("Slack API returned not OK");
             }
+
+            var students = result.Members
+                .Where(member => !member.IsBot && !member.IsAdmin && !member.IsOwner && !member.IsAppUser && !member.IsPrimaryOwner)
+                .ToArray();
+
+            if (!students.Any())
+            {
+                throw new Exception("No students in workspace");
+            }
+
+            var random = new Random((int)DateTime.Now.Ticks);
+            var user = students[random.Next(0, students.Length)];
+
+            Console.WriteLine($"{user.Name} ({user.RealName}), you've killed!");
         }
     }
 }
