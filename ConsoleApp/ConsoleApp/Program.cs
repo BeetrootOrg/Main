80f1240b09726e5ac03040d754b879e89a62e4b2
Ivan Chicha <94204093+VanSSHH@users.noreply.github.com>
2021-12-25 17:04:16 +0200

    sw

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,301 +1,63 @@
 ﻿using System;
-using System.IO;
-
 namespace ConsoleApp
 {
-    class Program
+    enum Genre
     {
-        const string Filename = @"phonebook123.csv";
-        const string Header = "FirstName,LastName,PhoneNumber";
-        const int MaxStringLength = 14;
-
-        static void Main()
-        {
-            while (true)
-            {
-                try
-                {
-                    Menu();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    Console.ReadLine();
-                }
-            }
-        }
-
-        static void Menu()
-        {
-            Console.Clear();
-            Console.WriteLine("Welcome to Phone Book Application!\n");
-            Console.WriteLine("\tMenu");
-            Console.WriteLine("\t1. Show all phone book");
-            Console.WriteLine("\t2. Create phone record");
-            Console.WriteLine("\t3. Search by name");
-            Console.WriteLine("\t4. Search by phone number");
-            Console.WriteLine("\t5. Rerecord phone number of some user");
-            Console.WriteLine("\t0. Exit");
-
-            ConsoleKeyInfo ck = Console.ReadKey();
-
-            switch (ck.Key)
-            {
-                case ConsoleKey.D1:
-                case ConsoleKey.NumPad1:
-                    ShowAllNumbers();
-                    break;
-                case ConsoleKey.D2:
-                case ConsoleKey.NumPad2:
-                    CreatePhoneNumber();
-                    break;
-                case ConsoleKey.D3:
-                case ConsoleKey.NumPad3:
-                    SearchByName();
-                    break;
-                case ConsoleKey.D4:
-                case ConsoleKey.NumPad4:
-                    SearchByNumber();
-                    break;
-                case ConsoleKey.D5:
-                case ConsoleKey.NumPad5:
-                    RerecordNumber();
-                    break;
-                case ConsoleKey.D0:
-                case ConsoleKey.NumPad0:
-                    Exit();
-                    break;
-
-            }
-        }
-
-        private static void SearchByName()
-        {
-            try
-            {
-                Console.Clear();
-                Console.WriteLine("Search condition - if first/last name contains search term you will see it");
-
-                Console.WriteLine("Enter Search Term...");
-                var searchTerm = Console.ReadLine();
-
-                bool found = false;
-                var phoneBook = ReadPhoneBook();
-
-                if (phoneBook == null)
-                {
-                    Console.WriteLine("Error occured during file read.");
-                    return;
-                }
-
-                foreach (var (firstName, lastName, phoneNumber) in phoneBook)
-                {
-                    if (firstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
-                        lastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
-                    {
-                        Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
-                        found = true;
-                    }
-                }
-
-                if (!found)
-                {
-                    Console.WriteLine("No such users");
-                }
-            }
-            finally
-            {
-                Wait();
-            }
-        }
-
-        private static void Exit()
-        {
-            Environment.Exit(0);
-        }
-
-        static void CreatePhoneNumber()
-        {
-            Console.Clear();
-            Console.WriteLine("Enter First Name...");
-            var firstName = Console.ReadLine();
-
-            if (firstName.Length > MaxStringLength)
-            {
-                throw new ArgumentException($"Max length of first name is {MaxStringLength}", nameof(firstName));
-            }
-
-            Console.WriteLine("Enter Last Name...");
-            var lastName = Console.ReadLine();
-
-            if (lastName.Length > MaxStringLength)
-            {
-                throw new ArgumentException($"Max length of last name is {MaxStringLength}", nameof(lastName));
-            }
-
-            Console.WriteLine("Enter Phone Number...");
-            var phoneNumber = Console.ReadLine();
-
-            if (phoneNumber.Length > MaxStringLength)
-            {
-                throw new ArgumentException($"Max length of phone number is {MaxStringLength}", nameof(phoneNumber));
-            }
-
-            File.AppendAllLines(Filename, new[] { $"{firstName},{lastName},{phoneNumber}" });
-        }
-
-        static void ShowAllNumbers()
-        {
-            Console.Clear();
-
-            var phoneBook = ReadPhoneBook();
-            if (phoneBook == null)
-            {
-                Console.WriteLine("Error occured during file read, press Enter...");
-                Console.ReadLine();
-                return;
-            }
-
-            Console.WriteLine($"{"First Name",-15}{"Last Name",-15}{"Phone Number",-15}");
-
-            foreach (var (firstName, lastName, phoneNumber) in phoneBook)
-            {
-                Console.Write($"{firstName,-15}");
-                Console.Write($"{lastName,-15}");
-                Console.Write($"{phoneNumber,-15}");
-                Console.WriteLine();
-            }
-
-            Wait();
-        }
-
-        private static void Wait()
-        {
-            Console.WriteLine("To back to menu type Enter...");
-            Console.ReadLine();
-        }
-
-
-        static (string, string, string)[] ReadPhoneBook()
-        {
-            try
-            {
-                string[] lines = File.ReadAllLines(Filename);
-
-                var phoneBook = new (string, string, string)[lines.Length - 1];
-                for (int i = 1; i < lines.Length; i++)
-                {
-                    string[] splitted = lines[i].Split(',');
-                    phoneBook[i - 1] = (splitted[0], splitted[1], splitted[2]);
-                }
-
-                return phoneBook;
-            }
-            catch (DirectoryNotFoundException dnfe)
-            {
-                throw;
-            }
-            catch (IOException)
-            {
-                try
-                {
-                    File.WriteAllText(Filename, $"{Header}\n");
-                }
-                catch
-                {
-                    // ignore
-                }
-
-                return new (string, string, string)[0];
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
-        static string[] ConvertToText((string, string, string)[] data)
-        {
-            var content = new string[data.Length + 1];
-            content[0] = "FirstName,LastName,PhoneNumber";
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                content[i + 1] = $"{data[i].Item1},{data[i].Item2},{data[i].Item3}";
-            }
-
-            return content;
-        }
-        private static void SearchByNumber()
-        {
-            Console.Clear();
-            Console.WriteLine("Search condition - if phone number contains search term you will see it");
-
-            Console.WriteLine("Enter Search Term...");
-            var searchTerm = Console.ReadLine();
-
-            bool found = false;
-            foreach (var (firstName, lastName, phoneNumber) in ReadPhoneBook())
-            {
-                if (phoneNumber.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine($"Found user {firstName} {lastName} with phone {phoneNumber}");
-                    found = true;
-                }
-            }
-
-            if (!found)
-            {
-                Console.WriteLine("Not such users");
-            }
-
-            Wait();
-        }
-        private static void RerecordNumber()
-        {
-            Console.Clear();
-
-            Console.WriteLine("Write the first name and last name of the user to change the phone number\n");
-            Console.WriteLine("First name:");
-            string lookFirstName = Console.ReadLine();
-            Console.WriteLine("Second name:");
-            string lookLastName = Console.ReadLine();
-
-            bool found = false;
-            (string firstName, string lastName, string phoneNumber)[] phoneBook = ReadPhoneBook();
-
-            for (int i = 0; i < phoneBook.Length; ++i)
-            {
-
-                if (String.Equals(lookFirstName, phoneBook[i].firstName, StringComparison.OrdinalIgnoreCase) &&
-                    String.Equals(lookLastName, phoneBook[i].lastName, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine("\nWrite new phone number:");
-                    phoneBook[i].phoneNumber = Console.ReadLine();
-
-                    string[] newPhoneBook = new string[phoneBook.Length + 1];
-                    newPhoneBook[0] = Header;
-
-                    for (int j = 0; j < phoneBook.Length; j++)
-                    {
-                        newPhoneBook[j + 1] = String.Join(',', phoneBook[j].firstName, phoneBook[j].lastName, phoneBook[j].phoneNumber);
-                    }
-
-                    File.WriteAllLines(Filename, newPhoneBook);
-
-                    Console.WriteLine("Update successfuly done");
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found)
-            {
-                Console.WriteLine("This user doesn`t exist");
-            }
-
-            Wait();
-        }
-
+        Detective,
+        Fantasy,
+        Horror,
+        Other,
     }
-} 
+
+    class Library
+    {
+        public int BookShelf { get; set; }
+        public int Visitor { get; set; }
+        public Staff Staff { get; init; }
+    }
+    class Visitor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; init; }
+        public Genre FavGeneres { get; set; }
+    }
+    class Staff
+    {
+        public Librarian Librarian { get; set; }
+        public SecurityGuard SecurityGuard { get; set; }
+        public Cleaner Cleaner { get; init; }
+    }
+    class Cleaner
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; init; }
+        public int Payment{ get; set; }
+    }
+    class SecurityGuard
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; init; }
+        public int Payment { get; set; }
+    }
+    class Librarian
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; init; }
+        public int Payment { get; set; }
+    }
+    class BookShelf
+    {
+        public Book Book { get; set; }
+        public string GenrePointers { get; set; }
+    }
+    class Book
+    {
+        public Genre Genere { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+    }
+}
