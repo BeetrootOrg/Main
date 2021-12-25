2bb84c61329d7e33a7ec1e13aaa520677aa441ed
Mour-Yam <70475271+Mour-Yam@users.noreply.github.com>
2021-12-25 21:45:55 +0200

    i.safontev/homework/12-abstractions

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,13 +6,29 @@ namespace ConsoleApp
     //i.safontev/homework/12-abstractions
     #region Internet Shop
 
-    public class InternetShop
-    {
+    public class InternetShop : INewProduct, IAddBuyer
+    { 
         public Product[] Products { get; set; }
         public Buyer[] Buyers { get; set; }
+        public void RegistrateNewProduct(Product newProduct)
+        {
+            Product[] newProducts = new Product[Products.Length + 1];
+            Array.Copy(Products, 0, newProducts, 0, Products.Length);
+            newProducts[newProducts.Length - 1] = newProduct;
+            Products = newProducts;
+        }
+
+        public void AddNewBuyer(Buyer newBuyer)
+        {
+            Buyer[] newBuyers = new Buyer[Buyers.Length + 1];
+            Array.Copy(Buyers, 0, newBuyers, 0, Buyers.Length);
+            newBuyers[newBuyers.Length] = newBuyer;
+            Buyers = newBuyers;
+        }
+
     }
 
-    public class Product : IAddNProduct, INewProduct, ISellProduct
+    public class Product : IAddNProduct, ISellProduct
     {
         public string ID { get; set; }
         public string Name { get; set; }
@@ -20,38 +36,21 @@ namespace ConsoleApp
         public int CommonCount { get; set; }
 
         public void AddNProduct(int n) { CommonCount += n; }
-        public Product RegistrateNewProduct(string ID, string Name, double Price, int Count)
-        {
-            Product newProduct = new Product
-            {
-                ID = ID,
-                Name = Name,
-                Price = Price,
-                CommonCount = Count,
-            };
-            return newProduct;
-        }
         public void SellProduct(int n) { CommonCount -= n; }
+
+        public override string ToString()
+        {
+            return $" ID= {ID}\n Name= {Name}\n Price= {Price}\n Common Count= {CommonCount}\n";
+        }
     }
 
-    public class Buyer : IAddBuyer
+    public class Buyer
     {
         private string ID { get; set; }
         private string FirstName { get; set; }
         private string LastName { get; set; }
         private string PhoneNumber { get; set; }
 
-        public Buyer AddNewBuyer(string ID, string FirstName, string LastName, string PhoneNumber)
-        {
-            Buyer newBuyer = new Buyer
-            {
-                ID = ID,
-                FirstName = FirstName,
-                LastName = LastName,
-                PhoneNumber = PhoneNumber,
-            };
-            return newBuyer;
-        }
     }
 
     public class Receipts
@@ -67,12 +66,12 @@ namespace ConsoleApp
 
     public interface IAddBuyer
     {
-        Buyer AddNewBuyer(string ID, string FirstName, string LastName, string PhoneNumber);
+        void AddNewBuyer(Buyer newBuyer);
     }
 
     public interface INewProduct
     {
-        Product RegistrateNewProduct(string ID, string Name, double Price, int Count);
+        void RegistrateNewProduct(Product newProduct);
     }
 
     public interface IAddNProduct
@@ -90,7 +89,7 @@ namespace ConsoleApp
     {
         static void Main()
         {
-
+            
         }
     }
 }
