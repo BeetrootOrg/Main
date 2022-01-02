0319468050c59cb749f1edca3df7e60ee6177d9a
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2022-01-02 22:30:16 +0200

    all generics

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,84 +2,162 @@
 
 namespace ConsoleApp
 {
+    /*Create generic Stack<T>(what it is) class with next methods:
 
-
-    class Engine
-    { 
-        bool isElectric { get; init; }
-        int Capacity { get; set; }
-
-    }
-    class Base
-    { 
-        int Length { get; set; }    
-        int Width { get; set; }
-        int Clearance { get; set; }
-    }
-    class Wheel
+Push(obj) - adds obj at the top of stack
+Pop() - returns top element of stack & removes it
+Clear() - clear stack
+Count - property return number of elements
+Peek() - returns top element but doesn’t remove it
+CopyTo(arr) - copies stack to array
+    */
+    public class Stack<T>
     {
-        string Tire { get; set; }
-        string Disk { get; set; }
-    }
-
-    class Door
-    { 
-        bool Glass { get; set; }
-        bool Decors { get; set; }
-    }
-    class Roof
-    { 
-        bool isGlass { get; set; }
-        bool isMovable { get; set; }
-    }
-
-    class Vehicle
-    { 
-        Engine engines;
-        Base bases;
-        Wheel wheels;
-        Door doors;
-        Roof roof;
-    
-    }
-
-    class Worker
-    { 
-        bool isStaff { get; set; }
-        string FullName { get; set; }
-    }
-
-    class Moto : Vehicle
-    {
-        Engine engine;
-        Base bases;
-        Wheel[] wheels;
-
-    }
-
-    class Auto : Vehicle
-    {
-        Engine engine;
-        Base bases;
-        Wheel[] wheels;
-        Door[] doors;
-        Roof roof;
-
-    }
-
-    public class Autoservice
-    {
-        Vehicle[] vehicles { get; set; }
-        Worker[] workers { get; set; }
-    }
-    public class Program
-    {
-
-        static void Main()
+        private class StackItem
         {
-            {      
-                
+            public T Value { get; set; }
+            public StackItem Next { get; set; }
+
+        }
+
+        private StackItem _base;
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            Insert(item, 0);
+        }
+
+        public T this[int index]
+        {
+            get => GetByIndex(index).Value;
+            set
+            {
+                var item = GetByIndex(index);
+                item.Value = value;
             }
         }
+
+        public T[] GetAll()
+        {
+            var array = new T[Count];
+            var item = _base;
+
+            for (int i = 0; i < Count; ++i)
+            {
+                array[i] = item.Value;
+                item = item.Next;
+            }
+
+            return array;
+        }
+
+        public T[] CopyToArr(T[] arr)
+        {
+            arr = GetAll();
+            return arr;
+        }
+
+        public void Clear()
+        {
+            _base = null;
+            Count = 0;
+        }
+
+        public T Pop()
+        {
+            var tItem = _base;
+            if (Count > 0)
+            {
+                _base = _base.Next;
+                --Count;
+            }
+            else
+            throw new ArgumentException("there isn't elements do delete");
+            
+            return tItem.Value;
+            
+        }
+
+       public T  Peek()
+       {
+            return _base.Value;
+        
+       }
+
+        public void Insert(T item, int index)
+        {
+            if (index == 0)
+            {
+                var stackItem = new StackItem
+                {
+                    Value = item,
+                    Next = _base
+                };
+
+                _base = stackItem;
+            }
+            else
+            {
+                var prev = GetByIndex(index-1);
+
+                var stackItem = new StackItem
+                {
+                    Value = item,
+                    Next = prev.Next
+                };
+
+                prev.Next = stackItem;
+            }
+
+            ++Count;
+        }
+
+
+        private StackItem GetByIndex(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            var item = _base;
+            for (int i = 0; i < index; i++)
+            {
+                item = item.Next;
+            }
+
+            return item;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+
+            var stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(0);
+        
+
+
+
+        }
+
+
+        static void ShowArray<TElement>(TElement[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
     }
 }
