6b99f92187ec82257246abcc2005abba449db954
Oleg <oleg.karman@gmail.com>
2022-01-03 22:09:33 +0200

    o.karmanskyi/homework/17

--- course/Program.cs
+++ course/Program.cs
@@ -1,172 +1,148 @@
-﻿namespace Course
+﻿using System.Diagnostics;
+using static System.Console;
+
+namespace Course
 {
-    public abstract class Pool
+    public enum Direction
     {
-        public string Name { get; set; }
-        public Dictionary<string, int> Body { get; set; }
+        Up,
+        Down,
+        Right,
+        Left
     }
-
-    public class VotePool : Pool
+    
+    class Program
     {
-        public VotePool(string name, string[] options)
-        {
-            Name = name;
-            Body = new Dictionary<string, int>();
-            foreach (var option in options) Body.Add(option, 0);
-        }
-    }
+        private const int _mapWidth = 90;
+        private const int _mapHeight = 30;
 
-    class Course
-    {
-        public static List<Pool> _pools = new List<Pool>();
+        private const int _frameMilliseconds = 200;
+
+        private const ConsoleColor _borderColor = ConsoleColor.Gray;
+
+        private const ConsoleColor _foodColor = ConsoleColor.Red;
+
+        private const ConsoleColor _snakeColor = ConsoleColor.Green;
+
+        private static readonly Random _random = new Random();
+
         static void Main()
         {
+            SetWindowSize(_mapWidth, _mapHeight);
+            SetBufferSize(_mapWidth, _mapHeight);
+            CursorVisible = false;
 
             while (true)
             {
-                Menu();
+                StartGame();
+                ReadKey();
             }
         }
 
-        private static void Menu()
+        static void StartGame()
         {
-            Console.Clear();
-            Console.WriteLine("Welcome to Phone Book Application!\n");
-            Console.WriteLine("\tMenu");
-            Console.WriteLine("\t1. Create pool");
-            Console.WriteLine("\t2. Show pool results");
-            Console.WriteLine("\t3. Vote for something");
-            Console.WriteLine("\t0. Exit");
+            int score = 0;
 
-            ConsoleKeyInfo ck = Console.ReadKey();
+            Clear();
+            DrawBorder();
 
-            switch (ck.Key)
+            Snake snake = new Snake(10, 5, _snakeColor);
+
+            Pixel food = GenFood(snake);
+            food.Draw();
+
+            Direction currentMovement = Direction.Right;
+
+            var sw = new Stopwatch();
+
+            while (true)
             {
-                case ConsoleKey.D1:
-                case ConsoleKey.NumPad1:
-                    CreatePool();
-                    break;
-                case ConsoleKey.D2:
-                case ConsoleKey.NumPad2:
-                    ShowPools();
-                    break;
-                case ConsoleKey.D3:
-                case ConsoleKey.NumPad3:
-                    ShowPools(true);
-                    break;
-                case ConsoleKey.D0:
-                case ConsoleKey.NumPad0:
-                    Exit();
-                    break;
-            }
-        }
+                sw.Restart();
+                Direction oldMovement = currentMovement;
 
-        private static void CreatePool()
-        {
-            Console.Clear();
-            Console.WriteLine("Please, enter the name of the pool");
-            var poolName = Console.ReadLine();
-            Console.WriteLine("Please, enter coma-separated options for your pool");
-            var poolOptionsText = Console.ReadLine();
-            var poolOptions = poolOptionsText.Split(",");
-            if (!CheckPoolValidity(poolName, poolOptions))
-            {
-                throw new Exception("Pool data is invalid." +
-                    "The pool name must contain at least one symbol." +
-                    "The options must be unique and contains at least two.");
-            }
-            var pool = new VotePool(poolName, poolOptions);
-            _pools.Add(pool);
-            Console.WriteLine("\nYour pool was created. Thank you!");
-            Wait();
-        }
-
-        private static void ShowPools(bool isVoting = false)
-        {
-            Console.Clear();
-            var index = 1;
-            if (_pools.Count > 0) {
-                Console.WriteLine("Existing pools: \n");
-                foreach (var pool in _pools)
+                while (sw.ElapsedMilliseconds <= _frameMilliseconds)
                 {
-                    Console.WriteLine($"{index}. {pool.Name}");
-                    ++index;
+                    if (currentMovement == oldMovement)
+                        currentMovement = ReadMovement(currentMovement);
                 }
-                Console.WriteLine($"\n \nPlease, type the pool number which you would like to {(isVoting ? "participate" : "see")}");
-                CheckIndexValidity(_pools.Count, out int selectedPoolIndex);
-                if (isVoting) {
-                    VoteInPool(selectedPoolIndex - 1);
+
+                if (snake.Head.X == food.X && snake.Head.Y == food.Y)
+                {
+                    snake.Move(currentMovement, true);
+                    food = GenFood(snake);
+                    food.Draw();
+
+                    score++;
                 }
                 else
                 {
-                    ShowPool(selectedPoolIndex - 1);
+                    snake.Move(currentMovement);
                 }
+
+                if (snake.Head.X == _mapWidth - 1
+                    || snake.Head.X == 0
+                    || snake.Head.Y == _mapHeight - 1
+                    || snake.Head.Y == 0
+                    || snake.Body.Any(b => b.X == snake.Head.X && b.Y == snake.Head.Y))
+                    break;
             }
-            else
+
+            snake.Clear();
+            food.Clear();
+
+            SetCursorPosition(_mapWidth / 3, _mapHeight / 2);
+            WriteLine($"Game over, Score: {score}");
+        }
+
+        static void DrawBorder()
+        {
+            for (int i = 0; i < _mapWidth; i++)
             {
-                Console.WriteLine("No pools are created yet. Please, return and create some.");
-                Wait();
+                new Pixel(i, 0, _borderColor, '#').Draw();
+                new Pixel(i, _mapHeight - 1, _borderColor, '#').Draw();
             }
-        }
 
-        private static void ShowPool(int poolIndex)
-        {
-            Console.Clear();
-            Console.WriteLine($"{_pools[poolIndex].Name}\n");
-            foreach (var option in _pools[poolIndex].Body)
+            for (int i = 0; i < _mapHeight; i++)
             {
-                Console.WriteLine($"Option - {option.Key}. Votes - {option.Value}");
+                new Pixel(0, i, _borderColor, '#').Draw();
+                new Pixel(_mapWidth - 1, i, _borderColor, '#').Draw();
             }
-            Wait();
         }
 
-        private static void VoteInPool(int poolIndex)
+        static Pixel GenFood(Snake snake)
         {
-            Console.Clear();
-            Console.WriteLine($"{_pools[poolIndex].Name}\n");
-            var index = 1;
-            foreach (var option in _pools[poolIndex].Body)
+            Pixel food;
+
+            do
             {
-                Console.WriteLine($"{index}. {option.Key}");
-                ++index;
-            }
-            Console.WriteLine("\nPlease, type a number of specific option.");
-            CheckIndexValidity(_pools[poolIndex].Body.Count, out int selectedPoolIndex);
-            var selectedOptionName = _pools[poolIndex].Body.Keys.ToArray()[selectedPoolIndex - 1];
-            _pools[poolIndex].Body[selectedOptionName] = ++_pools[poolIndex].Body[selectedOptionName];
-            Console.WriteLine("Your vote has been counted. Thank you!");
-            Wait();
+                food = new Pixel(_random.Next(1, _mapWidth - 1), _random.Next(1, _mapHeight - 1), _foodColor, 'O');
+            } while (snake.Head.X == food.X && snake.Head.Y == food.Y ||
+                     snake.Body.Any(b => b.X == food.X && b.Y == food.Y));
+
+            return food;
         }
 
-        private static bool CheckPoolValidity(string name, string[] options)
+        static Direction ReadMovement(Direction currentDirection)
         {
-            if (name.Length == 0) return false;
-            if (options.Length < 2) return false;
-            string[] uniqueOptions = options.Distinct<string>().ToArray();
-            if (options.Length != uniqueOptions.Length) return false;
+            if (!KeyAvailable)
+                return currentDirection;
 
-            return true;
-        }
+            ConsoleKey key = ReadKey(true).Key;
 
-        private static void CheckIndexValidity(int maxCountToCompare, out int parsedIndex)
-        {
-            bool isIndexValid = int.TryParse(Console.ReadLine(), out int selectedPoolIndex);
-            if (!isIndexValid || selectedPoolIndex > maxCountToCompare)
+            currentDirection = key switch
             {
-                throw new Exception("The value is not a number or you've selected the non-existing option");
-            }
-            parsedIndex = selectedPoolIndex;
-        }
+                ConsoleKey.W when currentDirection != Direction.Down => Direction.Up,
+                ConsoleKey.A when currentDirection != Direction.Right => Direction.Left,
+                ConsoleKey.S when currentDirection != Direction.Up => Direction.Down,
+                ConsoleKey.D when currentDirection != Direction.Left => Direction.Right,
+                ConsoleKey.UpArrow when currentDirection != Direction.Down => Direction.Up,
+                ConsoleKey.LeftArrow when currentDirection != Direction.Right => Direction.Left,
+                ConsoleKey.DownArrow when currentDirection != Direction.Up => Direction.Down,
+                ConsoleKey.RightArrow when currentDirection != Direction.Left => Direction.Right,
+                _ => currentDirection
+            };
 
-        private static void Exit()
-        {
-            Environment.Exit(0);
-        }
-        
-        private static void Wait()
-        {
-            Console.WriteLine("\nTo back to menu type Enter...");
-            Console.ReadLine();
+            return currentDirection;
         }
     }
 }
