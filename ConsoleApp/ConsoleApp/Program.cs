ba3d2b1a3fd437bc7bad63b0886e7f48ef6b4e03
oleksandrmartynenko <81743146+oleksandrmartynenko@users.noreply.github.com>
2022-01-03 17:02:53 +0200

    1

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,5 +1,41 @@
 ﻿namespace ConsoleApp
 {
+    public class Shop
+    {
+        public Product[] ProductsList { get; set; }
+        public User[] UsersList { get; set; }
+    }
+
+    public class Product
+    {
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Weight { get; set; }
+        public string Color { get; set; }
+        public Category ProductCategory { get; set; }
+
+    }
+
+    public class Category
+    {
+        public string CategoryName { get; set; }
+        public string[] CategoryProperties { get; set; }
+    }
+
+    public class User
+    {
+
+    }
+    public class Receipts
+    {
+
+    }
+
+    public interface IAddProducts
+    {
+
+    }
 
     class Program
     {
