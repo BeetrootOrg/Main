ce2427fc5229b4066f75c1b0a949c012159d5b76
d.misik <misik.dmitriy@gmail.com>
2022-01-05 19:19:51 +0200

    linq

---
+++ LinqLesson/LinqLesson/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LinqLesson;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+        System.Console.WriteLine(persons.Count());
+    }
+}
