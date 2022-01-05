8c770af889e7c37e519eebf259ba3862a3206556
d.misik <misik.dmitriy@gmail.com>
2022-01-05 19:43:52 +0200

    average age

--- LinqLesson/LinqLesson/Program.cs
+++ LinqLesson/LinqLesson/Program.cs
@@ -17,6 +17,8 @@ class Program
 
         YoungestPerson1(persons);
         YoungestPerson2(persons);
+
+        AverageAgeByGender(persons);
     }
 
     static void CountByGender1(IEnumerable<Person> persons)
@@ -30,13 +32,13 @@ class Program
         var result = persons.GroupBy(person => person.Gender)
             .Select(group => new
             {
-                Key = group.Key,
+                Gender = group.Key,
                 Count = group.Count()
             });
 
         foreach (var item in result)
         {
-            Console.WriteLine($"{item.Key}s: {item.Count}");
+            Console.WriteLine($"{item.Gender}s: {item.Count}");
         }
     }
 
@@ -48,5 +50,31 @@ class Program
     static void YoungestPerson2(IEnumerable<Person> persons)
     {
         Console.WriteLine($"Youngest: {persons.MinBy(x => x.Age)}");
+    }
+
+
+    static void AverageAgeByMale(IEnumerable<Person> persons)
+    {
+        var age = persons.GroupBy(x => x.Gender)
+            .Where(group => group.Key == Gender.Male)
+            .SelectMany(group => group)
+            .Average(person => person.Age);
+
+        Console.WriteLine($"Average Male Age: {age}");
+    }
+
+    static void AverageAgeByGender(IEnumerable<Person> persons)
+    {
+        var average = persons.GroupBy(x => x.Gender)
+            .Select(group => new
+            {
+                Gender = group.Key,
+                AverageAge = group.Average(person => person.Age)
+            });
+
+        foreach (var item in average)
+        {
+            Console.WriteLine($"{item.Gender}s average age: {item.AverageAge}");
+        }
     }
 }
