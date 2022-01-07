fe1419fda4a4eb96dc6a398c4d8f42196f9d7f8a
Harry Mozghoviy <72220654+TemptationHenry@users.noreply.github.com>
2022-01-07 21:48:12 +0200

    commit

---
+++ ConsoleApp18EXTANTION/ConsoleApp18EXTANTION/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp18EXTANTION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Math.Ceiling((double)10/3));
+
+            var test = new List<int>() { 1, 2, 3, 4, 5 };
+            var rest = test.ChunkBy<List<int>, int>(2);
+            Console.WriteLine(rest);
+        }
+    }
+}
