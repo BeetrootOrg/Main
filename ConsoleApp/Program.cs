f4f1cbfc4ee6904730b1a20e7c5da08469a7da71
Rus-amok <36300059+Rus-amok@users.noreply.github.com>
2022-01-08 21:18:43 +0200

    Tdate Extension

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,245 +4,44 @@ using System.Linq;
 
 namespace ConsoleApp
 {
-    delegate bool Dima(int number);
-
-    #region Changes
-    public class ChangesObservable<T>
-    {
-        public delegate void ChangeEventHandler(object sender, ChangeEvetArgs args);
-
-        public record ChangeEvetArgs(T PreviousValue, T NewValue);
-
-        public event ChangeEventHandler ChangeEvent;
-
-        private T _value;
-
-        public string Name { get; init; }
-
-        public T Value
-        {
-            get
-            {
-                return _value;
-            }
-            set
-            {
-                var oldValue = _value;
-                _value = value;
-                RaiseChangedEvent(oldValue, value);
-            }
-        }
-
-        protected virtual void RaiseChangedEvent(T old, T @new) => ChangeEvent?.Invoke(this, new ChangeEvetArgs(old, @new));
-
-    }
-    #endregion
-
-    #region Where Enumerable
-
-    public class WhereEnumerable<T> : IEnumerable<T>
-    {
-        private readonly IEnumerable<T> _collection;
-        private readonly Func<T, bool> _predicate;
-
-        public WhereEnumerable(IEnumerable<T> collection, Func<T, bool> predicate)
-        {
-            _collection = collection;
-            _predicate = predicate;
-        }
-
-        public IEnumerator<T> GetEnumerator() => new WhereEnumerator(_collection.GetEnumerator(), _predicate);
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
-
-        private class WhereEnumerator : IEnumerator<T>
-        {
-            private readonly IEnumerator<T> _enumerator;
-            private readonly Func<T, bool> _predicate;
-
-            public WhereEnumerator(IEnumerator<T> enumerator, Func<T, bool> predicate)
-            {
-                _enumerator = enumerator;
-                _predicate = predicate;
-            }
-
-            public T Current { get; private set; }
-
-            object System.Collections.IEnumerator.Current => Current;
-
-            public void Dispose()
-            {
-                _enumerator.Dispose();
-            }
-
-            public bool MoveNext()
-            {
-                while (_enumerator.MoveNext())
-                {
-                    if (_predicate(_enumerator.Current))
-                    {
-                        Current = _enumerator.Current;
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-
-            public void Reset()
-            {
-                _enumerator.Reset();
-            }
-        }
-    }
-
-    #endregion
-
-    static class StringExtensions
-    {
-        public static int CountWords(this string str) => string.IsNullOrEmpty(str) ? 0 : str.Split(' ').Length;
-    }
-
     static class DateTimeExtensions
     {
-        public static IEnumerable<DateTime> DatesUntil(this DateTime from, DateTime to, TimeSpan step)
+
+
+        public static bool IsWorkday(this DateTime dateTime) => dateTime.DayOfWeek switch
         {
-            if (from < to && step < TimeSpan.Zero ||
-                from > to && step > TimeSpan.Zero)
-            {
-                throw new ArgumentException("Incorrect input");
-            }
-
-            if (from == to)
-            {
-                yield break;
-            }
-
-            Func<DateTime, bool> predicate = from < to
-                ? (dateTime) => dateTime <= to
-                : (dateTime) => dateTime >= to;
-
-            for (DateTime current = from; predicate(current); current = current.Add(step))
-            {
-                yield return current;
-            }
-        }
-
-        public static int Age(this DateTime birthday)
-        {
-            var now = DateTime.Now;
-            if (birthday > now)
-            {
-                throw new ArgumentOutOfRangeException(nameof(birthday), "You are debil");
-            }
-
-            return (DateTime.MinValue + (DateTime.Now - birthday)).Year - 1;
-        }
-
-        public static DateTime NextWorkingDay(this DateTime dateTime) => dateTime.DayOfWeek switch
-        {
-            DayOfWeek.Friday => dateTime.Date.AddDays(3),
-            DayOfWeek.Saturday => dateTime.Date.AddDays(2),
-            _ => dateTime.Date.AddDays(1)
+            DayOfWeek.Sunday => true,
+            DayOfWeek.Saturday => true,
+            _ => false,
         };
+
+        public static bool IsWeekend(this DateTime dateTime) => dateTime.DayOfWeek switch
+        {
+            DayOfWeek.Sunday => false,
+            DayOfWeek.Saturday => false,
+            _ => true,
+        };
+
+
     }
 
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("This is a string".CountWords());
-            Console.WriteLine("Word".CountWords());
-            Console.WriteLine(((string)null).CountWords());
-            Console.WriteLine("".CountWords());
 
-            Console.WriteLine("TAKE FROM ARRAY");
 
-            ShowAll(Take(new[] { 1, 2, 3 }, 2));
-            ShowAll(Take(new[] { 1, 2, 3 }, null));
+            Console.WriteLine("IS WEEKEND DATE");
+            Console.WriteLine(new DateTime(2022,01,8).IsWeekend());
+            Console.WriteLine(new DateTime(2022,01,11).IsWeekend());
 
-            var now = DateTime.Now;
-            Console.WriteLine("FROM MIN TO MAX");
-            ShowAll(now.DatesUntil(now.AddDays(7), TimeSpan.FromHours(5)));
+            Console.WriteLine("IS WORKDAY");
+            Console.WriteLine(new DateTime(2022,01,8).IsWorkday());
+            Console.WriteLine(new DateTime(2022,01,11).IsWorkday());
 
-            Console.WriteLine("FROM MAX TO MIN");
-            ShowAll(now.AddDays(7).DatesUntil(now, TimeSpan.FromHours(5).Negate()));
 
-            Console.WriteLine("EMPTY");
-            ShowAll(now.DatesUntil(now, TimeSpan.FromHours(5)));
-            ShowAll(now.DatesUntil(now, TimeSpan.FromHours(5).Negate()));
-
-            Console.WriteLine("BIRTHDAY");
-            Console.WriteLine(new DateTime(1996, 7, 11).Age());
-            Console.WriteLine(new DateTime(2020, 12, 22).Age());
-            Console.WriteLine(new DateTime(1976, 08, 19).Age());
-
-            Console.WriteLine("WORKING DAY");
-            Console.WriteLine(new DateTime(2021, 12, 22).NextWorkingDay());
-            Console.WriteLine(new DateTime(2021, 12, 23).NextWorkingDay());
-            Console.WriteLine(new DateTime(2021, 12, 24).NextWorkingDay());
-            Console.WriteLine(new DateTime(2021, 12, 25).NextWorkingDay());
-            Console.WriteLine(new DateTime(2021, 12, 26).NextWorkingDay());
         }
 
-        public static IEnumerable<T> Take<T>(IEnumerable<T> collection, int? count = null) =>
-            count.HasValue ? Take(collection, count.Value) : collection;
 
-        public static IEnumerable<T> Take<T>(IEnumerable<T> collection, int count)
-        {
-            using var enumerator = collection.GetEnumerator();
-            var returned = 0;
-
-            while (returned < count && enumerator.MoveNext())
-            {
-                yield return enumerator.Current;
-                ++returned;
-            }
-        }
-
-        public static IEnumerable<int> FilterValues(IEnumerable<int> collection, Dima predicate)
-        {
-            foreach (var item in collection)
-            {
-                if (predicate(item))
-                {
-                    yield return item;
-                }
-            }
-        }
-
-        public static T FirstOrDefault<T>(IEnumerable<T> collection, Func<T, bool> predicate)
-        {
-            foreach (var item in collection)
-            {
-                if (predicate(item))
-                {
-                    return item;
-                }
-            }
-
-            return default;
-        }
-
-        public static bool Any<T>(IEnumerable<T> collection, Func<T, bool> predicate)
-        {
-            foreach (var item in collection)
-            {
-                if (predicate(item))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public static void ShowAll<T>(IEnumerable<T> collection)
-        {
-            foreach (var item in collection)
-            {
-                Console.WriteLine(item);
-            }
-        }
     }
 }
