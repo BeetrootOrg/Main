0d5a413b89cd9ea080f25f990d7775cab3877bea
d.misik <misik.dmitriy@gmail.com>
2022-01-08 10:34:02 +0200

    int property

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 
 namespace ConsoleApp
 {
-    class TestClass
+    record TestClass
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public int Age { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 
-    class ExampleClass
+    record ExampleClass
     {
         public string Example { get; set; }
     }
@@ -29,6 +27,46 @@ namespace ConsoleApp
             var typeToCreate = Type.GetType($"{assemblyName}.{classname}, {assemblyName}", true);
 
             var obj = Activator.CreateInstance(typeToCreate);
+
+            while (true)
+            {
+                Console.WriteLine("Enter property name:");
+                var propertyName = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(propertyName))
+                {
+                    Console.WriteLine("Enter property value:");
+                    var propertyValue = Console.ReadLine();
+
+                    var propertyInfo = typeToCreate.GetProperty(propertyName);
+                    
+                    if (propertyInfo == null)
+                    {
+                        Console.WriteLine($"Missing property info {propertyName}");
+                    }
+                    else
+                    {
+                        if (propertyInfo.PropertyType == typeof(string))
+                        {
+                            propertyInfo.SetValue(obj, propertyValue);
+                        }
+                        else if (propertyInfo.PropertyType == typeof(int) && int.TryParse(propertyValue, out var intVal))
+                        {
+                            propertyInfo.SetValue(obj, intVal);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Cannot convert value to type {propertyInfo.PropertyType}");
+                        }
+                    }
+                } 
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(obj);
         }
     }
 }
