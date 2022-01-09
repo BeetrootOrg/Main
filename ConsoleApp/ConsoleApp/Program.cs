11b4cc1f07b76d5c25da164aec7b1f91511a3f97
Legion59 <legion.59mk@gmail.com>
2022-01-09 21:22:32 +0200

    Download

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -3,233 +3,72 @@ using System.Collections.Generic;
 
 namespace ConsoleApp
 {
-    //ГОЛОСОВАЛКА ЗА ЧТО УГОДНО
-
-    public class Vote
+    public static class DateTimeExtentions
     {
-        public string VoteQuestion { get; set; }
-        public List<Option> VoteOptions = new List<Option>();
-
-        public Vote(string question)
+        public static bool IsWeekend(this DateTime checkDate)
         {
-            VoteQuestion = question;
-        }
-
-        public void AddVoteOption(string optionText)
-        {
-            VoteOptions.Add(new Option(optionText));
-        }
-
-        public void AddVoter(int choise, string name)
-        {
-            VoteOptions[choise].voterList.Add(name);
-        }
-
-        public void ShowVoteQuestionOption()
-        {
-            Console.WriteLine($"{VoteQuestion}");
-
-            int i = 0;
-            foreach (var item in VoteOptions)
+            if (checkDate.DayOfWeek == DayOfWeek.Saturday || checkDate.DayOfWeek == DayOfWeek.Sunday)
             {
-                Console.WriteLine($"{i}) {item.TextOption}");
-                ++i;
+                return true;
             }
+
+            return false;
         }
 
-        public void ShowVoteResult()
-        {
-            Console.WriteLine(VoteQuestion);
-
-            foreach (var item in VoteOptions)
-            {
-                item.ShowVotePeople();
-
-            }
-        }
+        public static bool IsWorkday(this DateTime checkDate) => !IsWeekend(checkDate); 
     }
 
-    public class Option
+    public static class IEnumerableExtentions
     {
-        public string TextOption;
-        public List<string> voterList = new List<string>();
-
-        public Option(string text)
+        public static List<List<T>> ChunkBy<T>(this List<T> collection, int size)
         {
-            TextOption = text;
-        }
+            var resultList = new List<List<T>>();
 
-        public void ShowVotePeople()
-        {
-            if (voterList.Count > 0)
+
+            for (int i = 0; i < collection.Count - collection.Count % size; i += size)
             {
-                Console.WriteLine($"{TextOption}: {voterList.Count} votes");
+                var elementResultList = new List<T>();
+                for (int j = 0; j < size; j++)
+                {
+                    elementResultList.Add(collection[i + j]);
+                }
+
+                resultList.Add(elementResultList);
             }
+
+            if (collection.Count % size != 0)
+            {
+
+                var elementResultList = new List<T>();
+
+                for (int i = collection.Count - collection.Count % size; i < collection.Count; ++i)
+                {
+                    elementResultList.Add(collection[i]);
+                }
+
+                resultList.Add(elementResultList);
+            }
+
+            return resultList;
         }
     }
-
-
 
     public class Program
     {
-        static List<Vote> votes = new List<Vote>();
-
         static void Main()
         {
-            while (true)
+            List<int> listInt = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            var listChunk = listInt.ChunkBy<int>(3);
+
+            foreach (var item in listChunk)
             {
-                Menu();
-            }
-        }
-
-
-        static void Menu()
-        {
-            Console.Clear();
-            Console.WriteLine(" Welkome to vote Aplication!\n Lets try to create some Vote\n");
-
-            Console.WriteLine("\t1. Create Vote");
-            Console.WriteLine("\t2. Show All Votes");
-            Console.WriteLine("\t3. Lets Vote");
-            Console.WriteLine("\t4. Show Results");
-            Console.WriteLine("\t0. Exit");
-
-
-            ConsoleKeyInfo ck = Console.ReadKey();
-
-            switch (ck.Key)
-            {
-                case ConsoleKey.D1:
-                case ConsoleKey.NumPad1:
-                    CreateVote();
-                    break;
-                case ConsoleKey.D2:
-                case ConsoleKey.NumPad2:
-                    ShowAllVotes();
-                    break;
-                case ConsoleKey.D3:
-                case ConsoleKey.NumPad3:
-                    LetsVote();
-                    break;
-                case ConsoleKey.D4:
-                case ConsoleKey.NumPad4:
-                    ShowResults();
-                    break;
-
-                case ConsoleKey.D0:
-                case ConsoleKey.NumPad0:
-                    Environment.Exit(0);
-                    break;
-            }
-        }
-
-        static void CreateVote()
-        {
-            Console.Clear();
-
-            Console.WriteLine("Write a Voting topic:");
-            string question = Console.ReadLine();
-
-            if (!string.IsNullOrEmpty(question))
-            {
-                votes.Add(new Vote(question));
-            }
-            else
-            {
-                Console.WriteLine("Ah Shit, Here We Go Again!");
-                question = Console.ReadLine();
-                votes.Add(new Vote(question));
-            }
-
-            Console.WriteLine("Lets Add some options.");
-
-            do
-            {
-                var option = Console.ReadLine();
-
-                if (!String.IsNullOrEmpty(option) || votes[votes.Count - 1].VoteOptions.Count < 2)
+                foreach (var element in item)
                 {
-                    votes[votes.Count - 1].AddVoteOption(option);
+                    Console.Write($"{element} ");
                 }
-                else
-                {
-                    break;
-                }
-
-            } while (true);
-
-        }
-
-        static void ShowQuestions()
-        {
-            int i = 0;
-            foreach (var vote in votes)
-            {
-                Console.WriteLine($"{i}) {vote.VoteQuestion}");
-                ++i;
-            }
-        }
-
-        static void ShowAllVotes()
-        {
-            Console.Clear();
-            foreach (var vote in votes)
-            {
-                vote.ShowVoteQuestionOption();
-            }
-
-            Wait();
-        }
-
-
-        static void LetsVote()
-        {
-            if (votes.Count > 0)
-            {
-                Console.Clear();
-                ShowQuestions();
-                int voteIndex = 0;
-
-                if (votes.Count > 1)
-                {
-                    Console.WriteLine("What vote you whant?");
-                    voteIndex = Convert.ToInt32(Console.ReadLine());                    
-                }
-
-                Console.Clear();
-                votes[voteIndex].ShowVoteQuestionOption();
-
-                Console.WriteLine("Write your Name:");
-                string name = Console.ReadLine();
-
-                Console.WriteLine("Write what option you choose (write a number):");
-                int optionIndex = Convert.ToInt32(Console.ReadLine());
-
-                votes[voteIndex].AddVoter(optionIndex, name);
-            }
-            else
-            {
-                Console.WriteLine("Create any Vote!!!");
-            }
-        }
-
-        static void ShowResults()
-        {
-            Console.Clear();
-
-            foreach (var vote in votes)
-            {
-                vote.ShowVoteResult();
                 Console.WriteLine();
             }
-
-            Wait();
         }
-
-        static void Wait()
-        {
-            Console.WriteLine("To back to menu type Enter...");
-            Console.ReadLine();
-        }
-    }
+    }    
 }
