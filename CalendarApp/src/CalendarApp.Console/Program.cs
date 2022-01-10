6d80db84a0b748fbdff0ade9346af67f728a82c6
d.misik <misik.dmitriy@gmail.com>
2022-01-10 20:34:55 +0200

    message pack

--- CalendarApp/src/CalendarApp.Console/Program.cs
+++ CalendarApp/src/CalendarApp.Console/Program.cs
@@ -3,12 +3,21 @@
     using CalendarApp.Console.Context;
     using CalendarApp.Console.Controllers;
     using CalendarApp.Console.Controllers.Interfaces;
+    using System.IO;
 
     public class Program
     {
+        private const string Filename = "dump.bin";
+
         private static void Main()
         {
             var context = new CalendarContext();
+
+            if (File.Exists(Filename))
+            {
+                context.ReadFromFile(Filename);
+            }
+
             IController controller = new MainMenuController(context);
 
             while (controller != null)
@@ -16,6 +25,8 @@
                 controller.Render();
                 controller = controller.Action();
             }
+
+            context.WriteToFile(Filename);
         }
     }
 
