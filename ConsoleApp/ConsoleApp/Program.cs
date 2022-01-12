e6ddce24bf397b0d575caf44106d7bf3971c491f
d.misik <misik.dmitriy@gmail.com>
2022-01-12 20:38:27 +0200

    chunk by and other

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,248 +1,89 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace ConsoleApp
 {
-    delegate bool Dima(int number);
-
-    #region Changes
-    public class ChangesObservable<T>
+    public class ChunkByEnumerable<T> : IEnumerable<IEnumerable<T>>
     {
-        public delegate void ChangeEventHandler(object sender, ChangeEvetArgs args);
+        private readonly IEnumerable<T> _items;
+        private readonly int _size;
 
-        public record ChangeEvetArgs(T PreviousValue, T NewValue);
-
-        public event ChangeEventHandler ChangeEvent;
-
-        private T _value;
-
-        public string Name { get; init; }
-
-        public T Value
+        public ChunkByEnumerable(IEnumerable<T> items, int size)
         {
-            get
-            {
-                return _value;
-            }
-            set
-            {
-                var oldValue = _value;
-                _value = value;
-                RaiseChangedEvent(oldValue, value);
-            }
+            _items = items;
+            _size = size;
         }
 
-        protected virtual void RaiseChangedEvent(T old, T @new) => ChangeEvent?.Invoke(this, new ChangeEvetArgs(old, @new));
-
-    }
-    #endregion
-
-    #region Where Enumerable
-
-    public class WhereEnumerable<T> : IEnumerable<T>
-    {
-        private readonly IEnumerable<T> _collection;
-        private readonly Func<T, bool> _predicate;
-
-        public WhereEnumerable(IEnumerable<T> collection, Func<T, bool> predicate)
-        {
-            _collection = collection;
-            _predicate = predicate;
-        }
-
-        public IEnumerator<T> GetEnumerator() => new WhereEnumerator(_collection.GetEnumerator(), _predicate);
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
-
-        private class WhereEnumerator : IEnumerator<T>
+        private class ChunkByEnumerator : IEnumerator<IEnumerable<T>>
         {
             private readonly IEnumerator<T> _enumerator;
-            private readonly Func<T, bool> _predicate;
+            private readonly int _size;
 
-            public WhereEnumerator(IEnumerator<T> enumerator, Func<T, bool> predicate)
+            public ChunkByEnumerator(IEnumerator<T> enumerator, int size)
             {
                 _enumerator = enumerator;
-                _predicate = predicate;
+                _size = size;
             }
 
-            public T Current { get; private set; }
+            public IEnumerable<T> Current { get; private set; }
 
-            object System.Collections.IEnumerator.Current => Current;
+            object IEnumerator.Current => Current;
 
-            public void Dispose()
-            {
-                _enumerator.Dispose();
-            }
+            public void Dispose() => _enumerator.Dispose();
 
             public bool MoveNext()
             {
-                while (_enumerator.MoveNext())
+                var arr = new T[_size];
+                var currentIndex = 0;
+
+                while (currentIndex < _size && _enumerator.MoveNext())
                 {
-                    if (_predicate(_enumerator.Current))
-                    {
-                        Current = _enumerator.Current;
-                        return true;
-                    }
+                    arr[currentIndex++] = _enumerator.Current;
                 }
 
-                return false;
+                if (currentIndex == 0)
+                {
+                    return false;
+                }
+
+                if (currentIndex < _size)
+                {
+                    Array.Resize(ref arr, currentIndex);
+                }
+
+                Current = arr;
+                return true;
             }
 
-            public void Reset()
-            {
-                _enumerator.Reset();
-            }
+            public void Reset() => _enumerator.Reset();
         }
+
+        public IEnumerator<IEnumerable<T>> GetEnumerator() => new ChunkByEnumerator(_items.GetEnumerator(), _size);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 
-    #endregion
-
-    static class StringExtensions
+    public static class DateTimeExtensions
     {
-        public static int CountWords(this string str) => string.IsNullOrEmpty(str) ? 0 : str.Split(' ').Length;
-    }
+        public static bool IsWeekend(this DateTime date) => 
+            date.DayOfWeek == DayOfWeek.Sunday || date.DayOfWeek == DayOfWeek.Saturday;
 
-    static class DateTimeExtensions
-    {
-        public static IEnumerable<DateTime> DatesUntil(this DateTime from, DateTime to, TimeSpan step)
-        {
-            if (from < to && step < TimeSpan.Zero ||
-                from > to && step > TimeSpan.Zero)
-            {
-                throw new ArgumentException("Incorrect input");
-            }
+        public static bool IsWorkday(this DateTime date) => !date.IsWeekend();
 
-            if (from == to)
-            {
-                yield break;
-            }
-
-            Func<DateTime, bool> predicate = from < to
-                ? (dateTime) => dateTime <= to
-                : (dateTime) => dateTime >= to;
-
-            for (DateTime current = from; predicate(current); current = current.Add(step))
-            {
-                yield return current;
-            }
-        }
-
-        public static int Age(this DateTime birthday)
-        {
-            var now = DateTime.Now;
-            if (birthday > now)
-            {
-                throw new ArgumentOutOfRangeException(nameof(birthday), "You are debil");
-            }
-
-            return (DateTime.MinValue + (DateTime.Now - birthday)).Year - 1;
-        }
-
-        public static DateTime NextWorkingDay(this DateTime dateTime) => dateTime.DayOfWeek switch
-        {
-            DayOfWeek.Friday => dateTime.Date.AddDays(3),
-            DayOfWeek.Saturday => dateTime.Date.AddDays(2),
-            _ => dateTime.Date.AddDays(1)
-        };
+        public static IEnumerable<IEnumerable<T>> ChunkBy<T>(this IEnumerable<T> collection, int size)
+            => new ChunkByEnumerable<T>(collection, size);
     }
 
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("This is a string".CountWords());
-            Console.WriteLine("Word".CountWords());
-            Console.WriteLine(((string)null).CountWords());
-            Console.WriteLine("".CountWords());
-
-            Console.WriteLine("TAKE FROM ARRAY");
-
-            ShowAll(Take(new[] { 1, 2, 3 }, 2));
-            ShowAll(Take(new[] { 1, 2, 3 }, null));
-
-            var now = DateTime.Now;
-            Console.WriteLine("FROM MIN TO MAX");
-            ShowAll(now.DatesUntil(now.AddDays(7), TimeSpan.FromHours(5)));
-
-            Console.WriteLine("FROM MAX TO MIN");
-            ShowAll(now.AddDays(7).DatesUntil(now, TimeSpan.FromHours(5).Negate()));
-
-            Console.WriteLine("EMPTY");
-            ShowAll(now.DatesUntil(now, TimeSpan.FromHours(5)));
-            ShowAll(now.DatesUntil(now, TimeSpan.FromHours(5).Negate()));
-
-            Console.WriteLine("BIRTHDAY");
-            Console.WriteLine(new DateTime(1996, 7, 11).Age());
-            Console.WriteLine(new DateTime(2020, 12, 22).Age());
-            Console.WriteLine(new DateTime(1976, 08, 19).Age());
-
-            Console.WriteLine("WORKING DAY");
-            Console.WriteLine(new DateTime(2021, 12, 22).NextWorkingDay());
-            Console.WriteLine(new DateTime(2021, 12, 23).NextWorkingDay());
-            Console.WriteLine(new DateTime(2021, 12, 24).NextWorkingDay());
-            Console.WriteLine(new DateTime(2021, 12, 25).NextWorkingDay());
-            Console.WriteLine(new DateTime(2021, 12, 26).NextWorkingDay());
-        }
-
-        public static IEnumerable<T> Take<T>(IEnumerable<T> collection, int? count = null) => 
-            count.HasValue ? Take(collection, count.Value) : collection;
-
-        public static IEnumerable<T> Take<T>(IEnumerable<T> collection, int count)
-        {
-            using var enumerator = collection.GetEnumerator();
-            var returned = 0;
-
-            while (returned < count && enumerator.MoveNext())
-            {
-                yield return enumerator.Current;
-                ++returned;
-            }
-        }
-
-        public static IEnumerable<int> FilterValues(IEnumerable<int> collection, Dima predicate)
-        {
-            foreach (var item in collection)
-            {
-                if (predicate(item))
-                {
-                    yield return item;
-                }
-            }
-        }
-
-        public static T FirstOrDefault<T>(IEnumerable<T> collection, Func<T, bool> predicate)
-        {
-            foreach (var item in collection)
-            {
-                if (predicate(item))
-                {
-                    return item;
-                }
-            }
-
-            return default;
-        }
-
-        public static bool Any<T>(IEnumerable<T> collection, Func<T, bool> predicate)
-        {
-            foreach (var item in collection)
-            {
-                if (predicate(item))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public static void ShowAll<T>(IEnumerable<T> collection)
-        {
-            foreach (var item in collection)
-            {
-                Console.WriteLine(item);
-            }
+            int[] arr = { 1, 2, 3, 4, 5 };
+            var chunked = arr.ChunkBy(2).ToArray();
+            chunked = arr.ChunkBy(3).ToArray();
+            chunked = arr.ChunkBy(5).ToArray();
+            chunked = arr.ChunkBy(7).ToArray();
         }
     }
 }
