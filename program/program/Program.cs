55f1f0087f881fb428bce6f33789cc326504c417
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2022-01-13 22:49:23 +0200

    IsWeekEnd isWorkday

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -1,164 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
 
 namespace ConsoleApp
 {
-    public class VoteList
+    public static class DateTimeExtensions
     {
-
-        Dictionary<string, int> _voteList = new Dictionary<string, int>();
-
-        public Dictionary<string, int> Parameter
+        public static bool IsWeekend(this DateTime date)
         {
-            get
+            if (date.DayOfWeek == DayOfWeek.Sunday || date.DayOfWeek == DayOfWeek.Saturday)
             {
-                return _voteList;
+                return true;
             }
-            set
+            else
             {
-                _voteList = value;
+                return false;
             }
         }
+
+        public static bool IsWorkday(this DateTime date) => !date.IsWorkday();
+        
+
+
     }
 
     class Program
     {
-        static VoteList voteList = new VoteList();
-
+        
         static void Main()
         {
-            while (true)
-            {
-                MainMenu();
-            }
-
-        }
-        
-        static void MainMenu()
-        {
             
 
-            Console.Clear();
-            Console.WriteLine("1.Create pool");
-            Console.WriteLine("2.Show pool results");
-            Console.WriteLine("3.Vote for something");
-            Console.WriteLine("4.Exit");
-
-            
-
-            ConsoleKeyInfo ck = Console.ReadKey();
-
-            switch (ck.Key)
-            {
-                case ConsoleKey.D1:
-                case ConsoleKey.NumPad1:
-                    CreatePool();
-                    break;
-                case ConsoleKey.D2:
-                case ConsoleKey.NumPad2:
-                    ShowPoolResults();
-                    break;
-                case ConsoleKey.D3:
-                case ConsoleKey.NumPad3:
-                    VoteForSomething();
-                    break;
-                case ConsoleKey.D4:
-                case ConsoleKey.NumPad4:
-                    Exit();
-                    break;
-            }
-
         }
-
-
-        
-        
-
-        static void CreatePool()
-        {
-            Console.Clear();
-            
-           
-
-            Console.WriteLine("Enter your survey text:");
-            string surveyText = Console.ReadLine();
-
-            Console.WriteLine("Write the variant for vote or <Enter> when you finish add:");
-            string newCandidate = Console.ReadLine();
-            int i = 0;
-
-                        
-
-            while (newCandidate != "Enter")
-            {
-                voteList.Parameter.TryAdd(newCandidate, i);
-                newCandidate = Console.ReadLine();
-                
-            }
-            
-        }
-
-        static void VoteForSomething()
-        {
-            Console.Clear();
-            
-
-            ShowAll(voteList.Parameter);
-
-            Console.WriteLine("Pls choose candidate: (For end write <Finish>)");
-
-            string enterVote = Console.ReadLine();
-
-            bool findCandidate = voteList.Parameter.ContainsKey(enterVote);
-
-
-            while (enterVote != "Finish")
-            {
-                if (findCandidate == true && voteList.Parameter.ContainsKey(enterVote))
-                {
-                    voteList.Parameter[enterVote]++;
-                }
-                else
-                {
-                    Console.WriteLine("You have mistake. Pls write a candidate");
-                }
-                enterVote = Console.ReadLine();
-            }
-
-        }
-
-        static void ShowPoolResults()
-        {
-            Console.Clear();
-            
-
-            ShowAll(voteList.Parameter);
-
-            Console.Write("Winer is: ");
-            var result = voteList.Parameter.Max(s => s.Key);
-            
-            Console.WriteLine(result);
-            Console.ReadLine();
-        }
-
-        private static void Exit()
-        {
-            Environment.Exit(4);
-        }
-
-        static void ShowAll<T>(IEnumerable<T> collection)
-        {
-            foreach (var item in collection)
-            {
-                Console.WriteLine(item);
-            }
-        }
-
-        
 
     }
 
