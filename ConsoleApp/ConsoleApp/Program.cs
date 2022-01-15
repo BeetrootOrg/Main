c0b7658943ebef55f60fabd3e2e2114d39d64a38
Mour-Yam <70475271+Mour-Yam@users.noreply.github.com>
2022-01-15 11:12:32 +0200

    i.safontev/classwork/23-async

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,162 +6,131 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ConsoleApp
-    //i.safontev/classwork/20-reflection
+    //i.safontev/classwork/23-async
 {
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-    public class DefaultValueAttribute : Attribute
-    {
-        public object Value { get; init; }
-        public bool NeedSetup { get; init; }
-
-        public DefaultValueAttribute(object defaultValue)
-        {
-            Value = defaultValue;
-        }
-    }
-
-    record TestClass
-    {
-        [DefaultValue("Dima", NeedSetup = true)]
-        public string Name { get; set; }
-
-        public string Description { get; set; }
-
-        [DefaultValue(42)]
-        public int Age { get; set; }
-
-        public DateTime CreatedDate { get; set; }
-
-        public void SayHello() => Console.WriteLine($"Hello, {Name}");
-        public void SaySome(string something) => Console.WriteLine($"Hello, {something}");
-        public void SayInteger(int number) => Console.WriteLine($"Hello, {number}");
-        public void SayALot(string thing, int times, DateTime createdAt)
-            => Console.WriteLine($"{thing} was created {createdAt} ({times} times)");
-    }
-
-    record ExampleClass
-    {
-        public string Example { get; set; }
-    }
-
+    
     class Program
     {
-        static void Main()
+        static async Task Main()
         {
-            var assembly = typeof(StringBuilder).Assembly;
+            var sw = new Stopwatch();
 
-            Console.WriteLine($"Types in assembly {assembly.FullName}:");
-            foreach (var type in assembly.GetTypes())
-            {
-                Console.WriteLine($"\t{type.FullName}");
-            }
+            sw.Start();
+
+            var boilTask = BoilWater();
+            var feedACat = FeedACat();
+            var heatUpAPenTask = HeatUpAPen();
+            var toastTask = MakeAToast();
+            var glassOfWater = PourAGlassOfWater();
+            var screamTask = ScreamASync();
+
+            await feedACat;
+
+            await toastTask;
+            await AddButterToAToast();
+            await AddJamToAToast();
+
+            await heatUpAPenTask;
+            var eggsTask = FryTwoEggs();
+            var baconTask = FryABacon();
+
+            await boilTask;
+            await PourACupOfCoffee();
+
+            await Task.WhenAll(eggsTask, baconTask, glassOfWater);
+
+            await Eat();
+
+            sw.Stop();
+            Console.WriteLine($"Breakfast finished in {sw.Elapsed}");
         }
 
-        static void DynamicInvocation()
+        private static async Task BoilWater()
         {
-            var assemblyName = "ConsoleApp";
+            await Task.Yield();
 
-            Console.WriteLine("Enter class name you want to create:");
-            var classname = Console.ReadLine();
-
-            var typeToCreate = Type.GetType($"{assemblyName}.{classname}, {assemblyName}", true);
-
-            var obj = Activator.CreateInstance(typeToCreate);
-            SetDefaultValues(obj);
-
-            while (true)
-            {
-                Console.WriteLine("Enter property/method name:");
-                var propertyMethodName = Console.ReadLine();
-
-                if (!string.IsNullOrWhiteSpace(propertyMethodName))
-                {
-                    var propertyInfo = typeToCreate.GetProperty(propertyMethodName);
-
-                    if (propertyInfo == null)
-                    {
-                        var methodInfo = typeToCreate.GetMethod(propertyMethodName);
-
-                        if (methodInfo == null)
-                        {
-                            Console.WriteLine($"Missing property/method info {propertyMethodName}");
-                        }
-                        else
-                        {
-                            var paramsInfo = methodInfo.GetParameters();
-
-                            if (paramsInfo.Length == 0)
-                            {
-                                methodInfo.Invoke(obj, null);
-                            }
-                            else
-                            {
-                                var args = new object[paramsInfo.Length];
-                                for (int i = 0; i < paramsInfo.Length; i++)
-                                {
-                                    Console.WriteLine($"Enter {i + 1} argument:");
-                                    var arg = Console.ReadLine();
-                                    args[i] = ConvertTo(arg, paramsInfo[i].ParameterType);
-                                }
-
-                                methodInfo.Invoke(obj, args);
-                            }
-
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Enter property value:");
-                        var propertyValue = Console.ReadLine();
-
-                        propertyInfo.SetValue(obj, ConvertTo(propertyValue, propertyInfo.PropertyType));
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            Console.WriteLine(obj);
+            //long await sync task
+            Task.Delay(5000).Wait();
+            await Task.Delay(5000); 
+            Console.WriteLine($"Water boiled");
         }
 
-        private static void SetDefaultValues(object obj)
+        private static async Task PourACupOfCoffee()
         {
-            var type = obj.GetType();
-
-            foreach (var propertyInfo in type.GetProperties())
-            {
-                var attribute = propertyInfo.GetCustomAttributes(typeof(DefaultValueAttribute), true)
-                    .FirstOrDefault();
-
-                if (attribute is DefaultValueAttribute defaultValueAttribute && defaultValueAttribute.NeedSetup)
-                {
-                    propertyInfo.SetValue(obj, defaultValueAttribute.Value);
-                }
-            }
+            await Task.Delay(1000);
+            Console.WriteLine($"Coffe ready");
         }
 
-        private static object ConvertTo(string value, Type type)
+        private static async Task HeatUpAPen()
         {
-            if (type == typeof(string))
-            {
-                return value;
-            }
+            await Task.Delay(2000);
+            Console.WriteLine($"Pe ready");
+        }
 
-            if (type == typeof(int) && int.TryParse(value, out var intVal))
-            {
-                return intVal;
-            }
+        private static async Task FryTwoEggs()
+        {
+            await Task.Delay(6000);
+            Console.WriteLine($"Eggs ready");
+        }
 
-            if (type == typeof(DateTime) && DateTime.TryParse(value, out var dateTime))
-            {
-                return dateTime;
-            }
+        private static async Task FryABacon()
+        {
+            await Task.Delay(4000);
+            Console.WriteLine($"Bacon ready");
+        }
 
-            throw new ArgumentException($"Cannot convert value to type {type}");
+        private static async Task MakeAToast()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine($"Toast ready");
+        }
+
+        private static async Task AddButterToAToast()
+        {
+            await Task.Delay(500);
+            Console.WriteLine($"Butter added");
+        }
+
+        private static async Task AddJamToAToast()
+        {
+            await Task.Delay(500);
+            Console.WriteLine($"Jam added");
+        }
+
+        private static async Task PourAGlassOfWater()
+        {
+            await Task.Delay(500);
+            Console.WriteLine($"Glass of water ready");
+        }
+
+        private static async Task Eat()
+        {
+            await Task.Delay(10000);
+            Console.WriteLine($"Eat it all");
+        }
+
+        private static Task FeedACat()
+        {
+            Console.WriteLine($"Cat already got food");
+            return Task.CompletedTask;
+        }
+
+        private static void Scream()
+        {
+            ScreamASync().Wait();
+        }
+
+        private static Task ScreamASync()
+        {
+            return Task.Run(() =>
+            {
+                Task.Delay(500).Wait();
+                Console.WriteLine($"AAAAAAAAA");
+            });
         }
     }
 }
