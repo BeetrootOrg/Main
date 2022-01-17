cce13b5af9c320423b562b2fa56593a8e7ff4693
d.misik <misik.dmitriy@gmail.com>
2022-01-17 20:52:11 +0200

    theory

--- ConsoleApp/ConsoleApp.UnitTests/LinkedListTests.cs
+++ ConsoleApp/ConsoleApp.UnitTests/LinkedListTests.cs
@@ -26,5 +26,37 @@ namespace ConsoleApp.UnitTests
             Assert.Single(items);
             Assert.Equal(item, items[0]);
         }
+
+        [Fact]
+        public void CountElementsShouldReturnCorrectValue()
+        {
+            // Arrange
+            // Act
+            var count = _list.Count;
+
+            // Assert
+            Assert.Equal(0, count);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, 2, 3, 4)]
+        [InlineData(1)]
+        public void AddSeveralElementsShouldUpdateList(params int[] data)
+        {
+            // Act
+            foreach (var item in data)
+            {
+                _list.Add(item);
+            }
+
+            // Arrange
+            Assert.Equal(data.Length, _list.Count);
+
+            for (var i = 0; i < data.Length; ++i)
+            {
+                Assert.Equal(data[i], _list[i]);
+            }
+        }
     }
 }
