df8cc7c3f2bc149f4789e5c31de5b946a7b3e6a1
d.misik <misik.dmitriy@gmail.com>
2022-01-19 20:24:27 +0200

    select

--- sql/03 - select.sql
+++ sql/03 - select.sql
@@ -1,1 +1,48 @@
 SELECT * FROM PersonsDB.dbo.Persons;
+
+SELECT FirstName, LastName 
+	FROM PersonsDB.dbo.Persons;
+
+SELECT *
+	FROM PersonsDB.dbo.Persons
+	WHERE Age > 20;
+
+SELECT *
+	FROM PersonsDB.dbo.Persons
+	ORDER BY Age;
+
+SELECT *
+	FROM PersonsDB.dbo.Persons
+	ORDER BY Age DESC;
+
+SELECT *
+	FROM PersonsDB.dbo.Persons
+	ORDER BY LastName DESC, FirstName;
+
+SELECT AVG(Age) AS AverageAge
+	FROM PersonsDB.dbo.Persons;
+
+SELECT MAX(Age) AS MaxAge, MIN(Age) AS MingAge
+	FROM PersonsDB.dbo.Persons;
+
+SELECT FirstName, COUNT(*) AS Number
+	FROM PersonsDB.dbo.Persons
+	GROUP BY FirstName;
+
+SELECT FirstName, COUNT(*) AS Number
+	FROM PersonsDB.dbo.Persons
+	GROUP BY FirstName
+	HAVING FirstName LIKE '%D%';
+
+SELECT FirstName AS FN, COUNT(1) AS Number
+	FROM PersonsDB.dbo.Persons
+	GROUP BY FirstName
+	HAVING COUNT(1) > 1
+	ORDER BY Number DESC;
+
+SELECT *
+	FROM PersonsDB.dbo.Persons
+	WHERE FirstName IN (SELECT FirstName
+		FROM PersonsDB.dbo.Persons
+		GROUP BY FirstName
+		HAVING COUNT(1) > 1);
