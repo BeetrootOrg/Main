7a4f12d9cd2bb607f4c5680e668bc51383891d59
a.tkachenko <some_email@whatever.com>
2022-01-21 11:00:39 +0200

    added unit tests

--- ConsoleApp/ConsoleApp/JamesWebbClient.cs
+++ ConsoleApp/ConsoleApp/JamesWebbClient.cs
@@ -27,7 +27,7 @@ namespace ConsoleApp
         public float tempCoolSide1C { get; set; }
         public float tempCoolSide2C { get; set; }
     }
-    internal class JamesWebbClient
+    public class JamesWebbClient
     {
         private readonly HttpClient _httpClient;
 
@@ -65,7 +65,7 @@ namespace ConsoleApp
             return strResultDeploymentImg;
         }
 
-        private static void ShowParsedData(JWSTTrackingData _jwst)
+        public static void ShowParsedData(JWSTTrackingData _jwst)
         {
             WriteLine($"\r\nLaunch Elapsed Time: {_jwst.launchElapsedTime}");
             WriteLine($"Distance from Earth: {_jwst.distanceEarthKm} Km");
@@ -93,20 +93,3 @@ namespace ConsoleApp
 
     }
 }
-/*
- string jsonString2 = "{" +
-                "\"distanceEarthKm\":1345647.2," +
-                "\"launchElapsedTime\":\"24:06:55:55\"," +
-                "\"distanceL2Km\":100684.3," +
-                "\"percentageCompleted\":93.0384," +
-                "\"speedKmS\":0.2483," +
-                "\"deploymentImgURL\":\"https://webb.nasa.gov/content/webbLaunch/assets/images/mirrorMoves/mirrorAlignmentTracker-1-18-22-1-1000px.png\"," +
-                "\"currentDeploymentStep\":\"Mirror Segment Deployment Tracker - undefined\"," +
-                "\"tempC\":{" +
-                "\"tempWarmSide1C\":56," +
-                "\"tempWarmSide2C\":10," +
-                "\"tempCoolSide1C\":-207," +
-                "\"tempCoolSide2C\":-201}," +
-                "\"timestamp\":\"2022-01-18T19:15:55.975Z\"}";
-            JWSTTrackingData _jwst = System.Text.Json.JsonSerializer.Deserialize<JWSTTrackingData>(jsonString2);
- */
