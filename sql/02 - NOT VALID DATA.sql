452ae1407655aa5dc8699d459c7f44537331a786
d.misik <misik.dmitriy@gmail.com>
2022-01-22 10:41:51 +0200

    not valid sql

---
+++ sql/02 - NOT VALID DATA.sql
@@ -0,0 +1,8 @@
+INSERT INTO PersonsDB.dbo.Persons(FirstName, LastName, Age, Gender)
+	VALUES
+	    (NULL, 'Last', 5, 'M'),
+		('First', NULL, 5, 'M'),
+		('First', 'Last', -1, 'M'),
+		('First', 'Last', 8, 'H'),
+		('First', 'Last', 9, 'FM'),
+		('First', 'Last', 300, 'M')
