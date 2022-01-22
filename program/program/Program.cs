7462198cdb9608a75c70d825ba407ba72e043219
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2022-01-22 11:51:51 +0200

    all task

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
@@ -16,11 +19,21 @@ namespace ConsoleApp
                 return false;
             }
         }
-
         public static bool IsWorkday(this DateTime date) => !date.IsWorkday();
-        
 
+    }
+    static class ChunkByCollection<T>
+    {
+        public static List<List<T>> ChunkBy(List<T> list, int size)
+        {
+            var newList = new List<List<T>>();
+            for (int i = 0; i < list.Count; i += size)
+            {
+                newList.Add(list.GetRange(i, Math.Min(size, list.Count - i)));
+            }
 
+            return newList;
+        }
     }
 
     class Program
@@ -28,10 +41,28 @@ namespace ConsoleApp
         
         static void Main()
         {
-            
 
+            var list = new List<int> { 1, 2, 3, 5, 6, 8, 9, 10, 11, 13 , 14};
+            var chunks = ChunkByCollection<int>.ChunkBy(list, 3);
+            foreach (var chunk in chunks)
+            {
+                Console.Write($"[");
+                ShowAll<int>(chunk);
+                Console.Write($"]");
+            }
         }
 
+        public static void ShowAll<T>(List<T> list)
+        {
+
+            foreach (T item in list)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+
+        }
     }
 
 }
+
---
+++ sql/04 - update.sql
@@ -0,0 +1,4 @@
+UPDATE PersonsDB.dbo.Persons
+	SET FirstName = 'D',
+		Age = age + 1
+	WHERE LastName= 'Misik';
