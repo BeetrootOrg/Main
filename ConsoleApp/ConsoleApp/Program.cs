ab97e2847b682f804bdb929544be976c17f6f90e
Ivan Chicha <94204093+VanSSHH@users.noreply.github.com>
2022-01-22 17:52:06 +0200

    it turned out to be not so difficult once I started writing code, and not just learning obscure terms

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,58 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp
 {
+    interface IUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public int AccountID { get; set; }
+    }
+    interface IProductOperations
+    {
+        void AddProductToBasket(Basket basket, Product product);
+        void DeleteProductFromBasket(Basket basket, Product product);
+        void AddReceipt(Receipt receipt);
+    }
+    public class InternetShop : IProductOperations
+    {
 
+        public void AddProductToBasket(Basket basket, Product product)
+        {
+            basket.quantity.Add(product);
+        }
+        public void DeleteProductFromBasket(Basket basket, Product product)
+        {
+            basket.quantity.Remove(product);
+
+        }
+        public void AddReceipt(Receipt receipt)
+        {
+            receipt.PrintReceipt();
+        }
+    }
+    public class Basket
+    {
+        public List<Product> quantity = new List<Product>();
+    }
+    public class Product
+    {
+        public string NameOfProduct { get; set; }
+        public int PriceOfProduct { get; set; }
+        public string TypeOfProduct { get; set; }
+    }
+    public class Receipt
+    {
+        public void PrintReceipt()
+        {
+            Seller sellerInfo = new Seller();
+            Buyer buyerInfo = new Buyer();
+            Console.WriteLine("The Receipt\n");
+            Console.WriteLine($"Seller`s name: {sellerInfo.FirstName}, {sellerInfo.LastName}");
+            Console.WriteLine($"Buyer`s name: {buyerInfo.FirstName}, {buyerInfo.LastName}");
+        }
+    }
+    public class Buyer : IUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public int AccountID { get; set; }
+    }
+    public class Seller : IUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public int AccountID { get; set; }
+    }
     class Program
     {
         static void Main()
-        { 
-            Console.WriteLine($"Max and Min numbers: {FindMax(-1, 2, 0, 462647643)}, {FindMin(-1, 2, 0, 462647643)}\n");
+        {
 
-            double multiply;
-            if (TryMulIfDividedByTgree(3, 1, out multiply))
-            {
-                Console.WriteLine($"Multiply: {multiply}\n");
-            }
-            else
-            {
-                Console.WriteLine($"Multiply error: {multiply}\n");
-            }
-            Console.WriteLine($"Str task: {Repeat("str", 0)}");
-        }
-        static int FindMin(int a, int b = 2147483647, int c = 2147483647, int d = 2147483647, int e = 2147483647)
-        {
-            return (Math.Min(Math.Min(Math.Min(Math.Min(a, b), c), d), e));
-        }
-        static int FindMax(int a, int b, int c = -2147483648, int d = -2147483648, int e = -2147483648)
-        {
-            return (Math.Max(Math.Max(Math.Max(Math.Max(a, b), c), d), e));
-        }
-        static bool TryMulIfDividedByTgree(int a, int b, out double multiply)
-        {
-            if (a%3 == 0 || b%3 == 0)
-            {
-                multiply = a*b;
-                return true;
-            }
-            else
-            {
-                multiply = 0;
-                return false;
-            }
-        }
-        static string Repeat(string str, int n)
-        {
-            if (n>0)
-            {
-                for (int a= 2; a <= n; ++a)
-                {
-                    str += str;
-                }
-                return str;
-            }
-            return "Something went wrong with string, please fix the problem";
         }
     }
-
 }
