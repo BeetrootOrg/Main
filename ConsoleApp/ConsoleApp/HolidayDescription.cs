33b54ada2de40a030b9177dbd9b3cadd34e6e09a
Viktor-Prokhorov <vicmmx@gmail.com>
2022-01-22 12:32:51 +0200

    does not work

---
+++ ConsoleApp/ConsoleApp/HolidayDescription.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp
+{
+    internal class HolidayDescription
+    {
+        public string id { get; set; }
+        public string date { get; set; }
+        public string nameEn { get; set; }
+        public string nameFr { get; set; }
+        public string federal { get; set; }
+        public string observedDate { get; set; }
+        public object provinces { get; set; }
+
+    }
+   
+}
+
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            using var httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromSeconds(15),
+            };
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = cancellationTokenSource.Token;
+
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                cancellationTokenSource.Cancel();
+            };
+
+            var foodClient = new FoodClient(httpClient);
+
+            Console.WriteLine("Please wait until random image will be generated...");
+            var imageResult = await foodClient.GetRandomImage(cancellationToken);
+            //var image = await foodClient.GetImage(var, cancellationToken);
+
+
+            Console.WriteLine(imageResult);
+            //Console.WriteLine("Enter filename (skip if want to use random name):");
+            //var filename = Console.ReadLine();
+
+            //if (string.IsNullOrEmpty(filename))
+            //{
+            //    filename = $"{Guid.NewGuid()}.jpg";
+            //}
+
+            //await File.WriteAllBytesAsync(filename, image, cancellationToken);
+            //Console.WriteLine($"Image saved to {filename}");
+        }
+    }
+}
