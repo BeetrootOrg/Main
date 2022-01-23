16ebba63b40134a72ed9942bbdfbc44157fb4150
Ivan Chicha <94204093+VanSSHH@users.noreply.github.com>
2022-01-23 14:13:57 +0200

    sdf

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,75 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Aaaa
+namespace ConsoleApp
 {
     public class Program
     {
-        static void Main/*text block*/()
+        static void Main()
         {
-            Console.WriteLine("Counting F(n) and M(n):");
+            Stack<string> peoples = new Stack<string>();
 
-            Console.WriteLine("Write n:");
-            int n = Convert.ToInt32(Console.ReadLine());
+            peoples.Push("Ann");
+            peoples.Push("Dave");
+            peoples.Push("Ivan");
+            peoples.Push("Pedro");
+            peoples.Push("Luis");
+            peoples.Push("Valera");
 
-            Console.WriteLine($"F({n}) = {F(n)}");
-            Console.WriteLine($"M({n}) = {M(n)}\n");
+            foreach (string people in peoples)
+            {
+                Console.WriteLine(people);
+            }
 
-            Console.WriteLine("Counting x^y:");
-            Console.WriteLine("Write x:");
-            int x = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Write y:");
+            peoples.Clear();
 
-            int y = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{x}^{y} = {Pow(x, y)}\n");
-
-            Console.WriteLine("Writing numbers from 1 to n:");
-            Console.WriteLine("Write n:");
-            n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Your row of numbers: \n{Series(n)}\n");
-            Console.WriteLine("         Push any button to exit the programm\n");
+            foreach (string people in peoples)
+            {
+                Console.WriteLine(people);
+            }
 
         }
-        static int F(int n)
-        {
-            if (n >= 0)
-            {
-                n = n -M(F(n-1));
-            }
-            return n;
-        }
-        static int M(int n)
-        {
-            if (n > 0)
-            {
-                n = n -F(M(n-1));
-            }
-            return n;
-        }
-        static int Pow(int x, int y)
-        {
-            if (y > 1)
-            {
-                x = x * Pow(x, y - 1);
-            }
-            else if (y == 0)
-            {
-                x = 1;
-            }
-            return x;
-        }
-        static string Series(int n)
-        {
-
-            if (n > 0)
-            {
-                return Series(n - 1) + n + " ";
-            }
-            else
-            {
-                return null;
-            }
-        }
-
     }
-
 }
