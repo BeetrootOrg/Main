7151d93670d45adeb871f7b7bd8f3afbb571b06d
Harry Mozghoviy <72220654+TemptationHenry@users.noreply.github.com>
2022-01-25 00:05:49 +0200

    update

--- ConsoleApp19Linq/ConsoleApp19Linq/Program.cs
+++ ConsoleApp19Linq/ConsoleApp19Linq/Program.cs
@@ -11,65 +11,62 @@ namespace ConsoleApp19Linq
     {
         static void Main(string[] args)
         {
-
-//            використовуючи код з персонами, що я писав під час уроку, створити наступні методи:
-//            метод, що виводить людей що знаходяться найпівнічніше/ південіше / західніше / східніше
-//            метод, що виводить двох людей, у яких в About найбільше однакових слів
-//            метод, що виводить двох людей, у котрих є спільний друг
-//            метод, що виводить назву компанії в якій працює найбільша кількість людей та кількість людей, що там працює
-            
             var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
-            GetTop2PeopleWithDuplicates(persons);
-            TopPersonsCompany(persons);
         }
 
-        private static IEnumerable<Person> GetFarestPeople(IEnumerable<Person> persons)
+        public IEnumerable<Person> GetFarestPeople(IEnumerable<Person> persons)
         {
-            var east = persons.OrderBy(x => x.Longitude).First();
-            var west = persons.OrderByDescending(x => x.Longitude).First();
-            var north = persons.OrderBy(x => x.Latitude).First();
-            var south = persons.OrderByDescending(x => x.Latitude).First();
-            return new List<Person>() { south, west, north, south};
+            var east = persons.OrderBy(x => x.Latitude).First();
+            var west = persons.OrderByDescending(x => x.Latitude).First();
+            var north = persons.OrderByDescending(x => x.Longitude).First();
+            var south = persons.OrderBy(x => x.Longitude).First();
+            return new List<Person>() { south, west, north, east };
         }
 
-        private static IEnumerable<Person> GetTop2PeopleWithDuplicates(IEnumerable<Person> persons)
+        public IEnumerable<Person> GetTop2PeopleWithDuplicates(IEnumerable<Person> persons)
         {
-            var about = persons.OrderBy(x => CountDuplicates(x.About)).Take(2);
+            var about = persons.OrderByDescending(x => CountDuplicates(x.About)).Take(2);
             return about;
         }
 
 
-        private static int CountDuplicates(string about)
+        public int CountDuplicates(string about)
         {
 
             var list = about.Split(' ').ToList();
             return list.GroupBy(n => n).Count(c => c.Count() > 1);
         }
 
-        private static IEnumerable<Person> TwoPeopleWithOneFriend(IEnumerable<Person> persons)
+        public int TwoPeopleWithOneFriend(IEnumerable<Person> persons)
         {
-            //NOT FINISHED YET
-            var friends = persons.Join(persons,
-                 person => true,
-                 person2 => true,
-                 (person, person2) => new
-                 {
-                     First = person,
-                     Second = person2
-                 })
-                 .Where(twoPersons => twoPersons.First != twoPersons.Second);
-            return null;
-         }
+            var result = persons.Join(persons,
+                person => true,
+                person => true,
+                (p1, p2) => new
+                {
+                    First = p1,
+                    Second = p2
+                }).Where(p => p.First != p.Second).Select(p =>
+                {
+                    var firstFriends = p.First.Friends;
+                    var secondFriends = p.Second.Friends;
+                    var intersection = firstFriends.Intersect(secondFriends);
+                    return new { p.First, p.Second, CommonFriends = intersection };
+                });
+            return result.Count() / 2;
 
-        private static void TopPersonsCompany(IEnumerable<Person> persons)
+        }
+
+        public string TopPersonsCompany(IEnumerable<Person> persons)
         {
-            var topCompany = persons.SelectMany(person => person.Company)
-               .GroupBy(company => company).Select(company => new
+            var topCompany = persons
+               .GroupBy(p => p.Company).Select(company => new
                {
                    Company = company.Key,
                    Count = company.Count()
                }).OrderByDescending(x => x.Count).First();
             Console.WriteLine($"{topCompany.Company} {topCompany.Count}");
+            return topCompany.Company;
         }
 
     }
