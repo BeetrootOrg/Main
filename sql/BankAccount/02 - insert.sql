4a51cb90ad1f162b29f57848a723d6a432a55f2f
a.tkachenko <some_email@whatever.com>
2022-01-25 21:30:00 +0200

    added "BankAccount"

---
+++ sql/BankAccount/02 - insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO BankAccountsDB.dbo.BankAccounts(AccountName, AccountCode, BankName, AccountNumber, Balance, Status)
+	VALUES ('Petrov', '0001', 'Private', '1234', 10000, 'Active'),
+		('Ivanov', '0002', 'UkrSim', '5678', 20000, 'Active'),
+		('Sidorov', '0003', 'Ochad', '9012', 30000, 'Active');
