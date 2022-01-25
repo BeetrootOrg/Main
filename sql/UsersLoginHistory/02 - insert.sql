2f7d5169155626d4330c30f00f2ba00a47a39a8d
a.tkachenko <some_email@whatever.com>
2022-01-25 20:58:38 +0200

    added "UsersLoginHistoryDB"

---
+++ sql/UsersLoginHistory/02 - insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO UsersLoginHistoryDB.dbo.UsersLoginHistory(IpAddr, UserName, OS, LogInTime, LogOutTime)
+	VALUES ('192.168.0.2', 'Petrov', 'Linux', '2022-01-23 11:45:37', '2022-01-23 13:45:37'),
+		('192.168.0.3', 'Ivanov', 'Windows', '2022-01-23 12:45:37', '2022-01-23 15:45:37'),
+		('192.168.0.4', 'Sidorov', 'Mac OS', '2022-01-23 13:45:37', '2022-01-23 17:45:37');
+
