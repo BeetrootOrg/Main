eb439e1a314f0e69d551d1ab4bd6c7ddc84ce599
d.misik <misik.dmitriy@gmail.com>
2022-01-26 19:21:55 +0200

    hello ef

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello, EF");
+        }
+    }
+}
