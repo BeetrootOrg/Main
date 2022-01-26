126d136d380f6a8a15f086201b7596f9519eb356
d.misik <misik.dmitriy@gmail.com>
2022-01-26 20:52:12 +0200

    script to generate migrations

--- ConsoleApp/database-generate.bat
+++ ConsoleApp/database-generate.bat
@@ -1,3 +1,4 @@
 dotnet new tool-manifest
 dotnet tool install dotnet-ef
-dotnet ef dbcontext scaffold "Server=localhost;Database=OrdersDB;Trusted_Connection=True;" "Microsoft.EntityFrameworkCore.SqlServer" --context OrderDBContext --context-dir Database --output-dir Models
+dotnet ef migrations add Initial
+dotnet ef database update
