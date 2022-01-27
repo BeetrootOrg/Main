daf280055160678b614d8b0c2f8ae5b758ff95bd
SilaSamuraya <arthurgolban@gmail.com>
2022-01-27 16:36:43 +0200

    TEST WORK,NEED FINISH//

--- FirstProject/Library/Library/Program.cs
+++ FirstProject/Library/Library/Program.cs
@@ -6,20 +6,27 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-
     public class Program
     {
         private static async Task Main()
         {
-            //await using var dbContext = new LibraryDBContext();
-            //var book = await dbContext.Books.
-            //    Where(b => b.NameBook == "Руслан и Людмила").
-            //    ToArrayAsync();
-            //foreach(var b in book)
-            //{
-            //    Console.WriteLine($"History: {b.AuthorId}");
-            //}
 
+            await using var dbContext = new LibraryDBContext();
+            var list = dbContext.Histories.Include(c=>c.Customer).ToList()
+                .Where(c => c.Id ==3);
+            var listBook =dbContext.Histories.Include(b=>b.Book).ToList().Where(b=>b.Id==3);
+            var book = dbContext.Books.Include(a => a.Author).ToList().Where(c=>c.AuthorId==1);
+            foreach(var item in list)
+            {
+                foreach (var itemBook in listBook)
+                {
+                    foreach (var items in book) {
+                        Console.WriteLine($"Данные читателя :{item.Customer.FirstName} {item.Customer.LastName}" +
+                            $" | Дата выдачи: { item.DateWhenTaken}| Название Книги: {item.Book.NameBook} Имя Автора:" +
+                            $" {items.Author.FirstName} {items.Author.LastName} ");
+                    }
+                }
+            }
         }
   
       
