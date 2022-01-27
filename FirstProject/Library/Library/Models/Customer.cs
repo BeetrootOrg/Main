9ffd4ce45cbca45bdfe48959e37ebee60673fbc6
SilaSamuraya <arthurgolban@gmail.com>
2022-01-27 15:13:51 +0200

    //FOR EXAMPLE TO CHECK !

---
+++ FirstProject/Library/Library/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Histories = new HashSet<History>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<History> Histories { get; set; }
+    }
+}
---
+++ FirstProject/Library/Library/Program.cs
@@ -0,0 +1,28 @@
+﻿using Library.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library
+{
+
+    public class Program
+    {
+        private static async Task Main()
+        {
+            //await using var dbContext = new LibraryDBContext();
+            //var book = await dbContext.Books.
+            //    Where(b => b.NameBook == "Руслан и Людмила").
+            //    ToArrayAsync();
+            //foreach(var b in book)
+            //{
+            //    Console.WriteLine($"History: {b.AuthorId}");
+            //}
+
+        }
+  
+      
+
+    }
+}
