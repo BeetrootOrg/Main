b02dd8c9895d01b1ddd51fc852a154397f5618fc
a.tkachenko <some_email@whatever.com>
2022-01-29 20:36:01 +0200

    completed?

--- sql/02 - insert.sql
+++ sql/02 - insert.sql
@@ -1,7 +1,9 @@
-INSERT INTO dbo.Authors(Name, Title, Published, Quantity)
-	VALUES ('Leo Tolstoy', 'War and Peace', '2001-01-01 00:00:00', 5),
-		('Alexander Pushkin', 'Eugene Onegin', '2002-01-01 00:00:00', 10),
-		('Fyodor Dostoevsky', 'The Gambler', '2003-01-01 00:00:00', 20);
+INSERT INTO dbo.Books(Title, Published)
+	VALUES ('War and Peace', '2001-01-01 00:00:00'),
+		('Eugene Onegin', '2002-01-01 00:00:00'),
+		('The Gambler', '2003-01-01 00:00:00');
 
-ALTER TABLE dbo.Books ADD FOREIGN KEY (AuthorId) REFERENCES dbo.Authors(Id);
-ALTER TABLE dbo.BookCount ADD FOREIGN KEY (BookId) REFERENCES dbo.Books(Id);
+INSERT INTO dbo.Authors(Name)
+	VALUES ('Leo Tolstoy'),
+		('Alexander Pushkin'),
+		('Fyodor Dostoevsky');
--- sql/04 - update.sql
+++
@@ -1,3 +0,0 @@
-UPDATE dbo.Authors
-	SET Quantity = Quantity + 1
-	WHERE Name = 'Alexander Pushkin' AND Title = 'Eugene Onegin';
