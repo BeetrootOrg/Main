a39aea816c56333971a081c66fecab9d7d2e0139
d.misik <misik.dmitriy@gmail.com>
2022-01-30 13:54:02 +0200

    init solution

---
+++ UrlShortener/src/UrlShortener.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortener.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ UrlShortener/tests/UrlShortener.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace UrlShortener.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
