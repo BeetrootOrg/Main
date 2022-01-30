7e760a6f12c03f3962521f6ac181ffc08ae26811
Legion59 <legion.59mk@gmail.com>
2022-01-30 23:04:53 +0200

    Homework

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 using System.IO;
+using System.Net.Http;
+using ConsoleApp;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LinqLesson;
 
@@ -11,11 +15,28 @@ static class WeatherAPI
 
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
+            using var httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromSeconds(5),
+            };
 
+            var cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = cancellationTokenSource.Token;
 
+            var forecast = new WeatherClient(httpClient);
 
+            Console.WriteLine("Please wait until random image will be generated...");
+            var forecastResult = await forecast.GetForecast(cancellationToken);
+
+            
+            Console.WriteLine($"Weather in Kiev on {forecastResult.Applicable_date.Date}:");
+            Console.WriteLine($"Max temp {forecastResult.Max_temp}");
+            Console.WriteLine($"Min temp {forecastResult.Min_temp}");
+            Console.WriteLine($"Wind speed {forecastResult.Wind_speed}");
+            Console.WriteLine($"Predictability {forecastResult.Predictability}%");
+            Console.Read();
         }
     }
 }
---
+++ ConsoleApp/ConsoleApp/WeatherHttpResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class WeatherHttpResult
+    {
+        public string Id { get; set; }
+        public string Weather_state_name { get; set; }
+        public string Weather_state_abbr { get; set; }
+        public string Wind_direction_compass { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Applicable_date { get; set; }
+        public string Min_temp { get; set; }
+        public string Max_temp { get; set; }
+        public string The_temp { get; set; }
+        public string Wind_speed { get; set; }
+        public string Wind_direction { get; set; }
+        public string Air_pressure { get; set; }
+        public string Humidity { get; set; }
+        public string Visibility { get; set; }
+        public string Predictability { get; set; }             
+    } 
+}
