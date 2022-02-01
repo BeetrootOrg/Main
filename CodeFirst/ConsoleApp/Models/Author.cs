fcd844d9221383307de617ad8c467c0cfcb17095
a.tkachenko <some_email@whatever.com>
2022-02-01 19:52:36 +0200

    first complete

---
+++ CodeFirst/ConsoleApp/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ConsoleApp.Models
+{
+    [Table("Authors", Schema = "dbo")]
+    public record Author
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        [MinLength(1)]
+        public string Name { get; set; }
+    }
+}
---
+++ CodeFirst/ConsoleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using ConsoleApp.Database;
+using ConsoleApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        private static async Task Main()
+        {
+            /*
+            1. dotnet new tool-manifest
+            2. dotnet tool install dotnet-ef
+            3. dotnet ef dbcontext scaffold "Server=localhost;Database=BookLibraryDB;Trusted_Connection=True;" "Microsoft.EntityFrameworkCore.SqlServer" --context BookLibraryDBContext --context-dir Database --output-dir Models
+            4. dotnet ef migrations add Initial
+            5. dotnet ef database update
+            */
+            Console.WriteLine("\r\n a.tkachenko/homework/28-EF \r\n");
+
+            await using var dbContext = new BookLibraryDBContext();
+
+            await dbContext.BookCounts.AddAsync(new BookCount
+            {
+                Book = new Book
+                {
+                    Title = "Title " + Guid.NewGuid().ToString(),
+                    Edition = DateTime.Now,
+                },
+                Author = new Author
+                {
+                    Name = "Author " + Guid.NewGuid().ToString(),
+                }
+            });
+
+            await dbContext.SaveChangesAsync();
+            await foreach (var bc in dbContext.BookCounts)
+            {
+                Console.WriteLine(bc);
+            }
+
+            var result = dbContext.BookCounts.Join(dbContext.Books,
+                bookCount => bookCount.BookId,
+                book => book.Id,
+                (bookCount, book) => new
+                {
+                    bookCount.Quantity,
+                    book.Title
+                });
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            // update
+            var maxBookCounts = await dbContext.BookCounts.MaxAsync(x => x.Quantity);
+            var maxBooks = await dbContext.BookCounts.FirstAsync(x => x.Quantity == maxBookCounts);
+            maxBooks.Quantity += 1;
+            dbContext.BookCounts.Update(maxBooks);
+
+            // delete
+            var minBookCounts = await dbContext.BookCounts.MinAsync(x => x.Quantity);
+            var minBooks = await dbContext.BookCounts.FirstAsync(x => x.Quantity == minBookCounts);
+
+            dbContext.BookCounts.Remove(minBooks);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
