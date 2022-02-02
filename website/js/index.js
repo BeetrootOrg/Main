3d0868d3ad3e1cb1403ab149a44029fb808f4843
d.misik <misik.dmitriy@gmail.com>
2022-02-02 19:56:46 +0200

    form

--- website/js/index.js
+++ website/js/index.js
@@ -2,8 +2,30 @@ const emailInput = document.getElementById('emailInput');
 const passwordInput = document.getElementById('passwordInput');
 const rememberMeCheck = document.getElementById('rememberMeCheck');
 const submitForm = document.getElementById('submitForm');
+const loginForm = document.getElementById('loginForm');
+const signInBlock = document.getElementById('signInBlock');
+const userEmail = document.getElementById('userEmail');
+const passwordLength = document.getElementById('passwordLength');
 
-let id = 0;
+const hideLogin = () => {
+	loginForm.style.display = 'none';
+};
+
+const showSignIn = () => {
+	signInBlock.style.display = 'block';
+};
+
+const signIn = (creds) => {
+	localStorage.setItem('credentials', JSON.stringify(creds));
+};
+
+const restoreCreds = () => {
+	const creds = JSON.parse(localStorage.getItem('credentials'));
+
+	userEmail.innerText = creds.email;
+	passwordLength.innerText = creds.password.length;
+};
+
 submitForm.onclick = () => {
 	const email = emailInput.value;
 	const password = passwordInput.value;
@@ -15,5 +37,14 @@ submitForm.onclick = () => {
 		rememberMe
 	}
 
-	localStorage.setItem(`credentials${id++}`, JSON.stringify(creds));
+	signIn(creds);
+	restoreCreds();
+	hideLogin();
+	showSignIn();
+};
+
+if (localStorage.getItem('credentials')) {
+	restoreCreds();
+	hideLogin();
+	showSignIn();
 }
