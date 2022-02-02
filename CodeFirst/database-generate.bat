8bdb662ee1d75a1da93e7801e37e1c5f0ec353ef
a.tkachenko <some_email@whatever.com>
2022-02-02 18:48:16 +0200

    init

--- CodeFirst/database-generate.bat
+++
@@ -1,5 +0,0 @@
-dotnet new tool-manifest
-dotnet tool install dotnet-ef
-dotnet ef dbcontext scaffold "Server=localhost;Database=BookLibraryDB;Trusted_Connection=True;" "Microsoft.EntityFrameworkCore.SqlServer" --context BookLibraryDBContext --context-dir Database --output-dir Models
-dotnet ef migrations add Initial
-dotnet ef database update
--- sql/01 - select.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM NewBookLibraryDB.dbo.Books;
-SELECT * FROM NewBookLibraryDB.dbo.Authors;
-SELECT * FROM NewBookLibraryDB.dbo.BookCounts;
