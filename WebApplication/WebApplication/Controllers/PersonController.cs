710708f6196f0e0a99a85f3bbbc7c47b60736e5e
d.misik <misik.dmitriy@gmail.com>
2022-02-07 20:04:35 +0200

    asp.net

---
+++ WebApplication/WebApplication/Controllers/PersonController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class PersonController : Controller
+    {
+        private static readonly IEnumerable<Person> _persons = new List<Person>
+        {
+            new Person
+            {
+                Id = 1,
+                FirstName = "D",
+                LastName = "M"
+            }
+        };
+
+        // GET: PersonController
+        public ActionResult Index()
+        {
+            return View(_persons);
+        }
+
+        // GET: PersonController/Details/5
+        public ActionResult Details(int id)
+        {
+            var person = _persons.First(person => person.Id == id);
+            return View(person);
+        }
+
+        // GET: PersonController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
