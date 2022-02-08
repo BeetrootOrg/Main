7b94d16049341287e70644cd0d1e55f4a4371f9f
Legion59 <legion.59mk@gmail.com>
2022-02-08 19:06:14 +0200

    Done

---
+++ ConsoleApp/ConsoleApp/Database/OrderDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Database
+{
+    public class OrderDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<CountOfEachBook> BookCount { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<HistoryBook> HistoryBooks { get; set; }
+
+        public OrderDbContext() : base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-HR3E54O\SQLEXPRESS;Database=LiberaryDb;Trusted_Connection=True");
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    [Table("Books", Schema = "dbo")]
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(1)]
+        public string NameBook { get; set; }
+
+        public int AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public Author BookAuthor { get; set; }
+    }
+}
