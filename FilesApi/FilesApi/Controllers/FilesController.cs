1ea0dfcfad6b487eec55a11114e7ecd65d7b787a
d.misik <misik.dmitriy@gmail.com>
2022-02-19 13:39:39 +0200

    download files

---
+++ FilesApi/FilesApi/Controllers/FilesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IOFile = System.IO.File;
+
+namespace FilesApi.Controllers
+{
+    [Route("api/files")]
+    public class FilesController : ControllerBase
+    {
+        private static readonly string Directory = Path.Combine("C:", "files");
+
+        [HttpGet("{filename}")]
+        public async Task<IActionResult> GetFile([FromRoute] string filename)
+        {
+            var fullPath = Path.Combine(Directory, filename);
+            if (!IOFile.Exists(fullPath))
+            {
+                return NotFound(new
+                {
+                    Filename = filename,
+                    Reason = "Not exists"
+                });
+            }
+
+            var readStream = IOFile.OpenRead(fullPath);
+
+            Response.Headers.ContentDisposition = $"attachment; filename=\"{filename}\"";
+            return File(readStream, "application/octet-stream");
+        }
+    }
+}
