9988157fc7a251137d73d658ba1348c281c8e86b
Viktor-Prokhorov <vicmmx@gmail.com>
2022-04-09 10:10:12 +0300

    5

--- WebApplication/WebApplication/Program.cs
+++ WebApplication/WebApplication/Program.cs
@@ -8,21 +8,8 @@ var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddMvc();
 builder.Services.AddControllersWithViews();
-
-//builder.Configuration.AddJsonFile("appsettings.json", optional: false);
-//public void ConfigureServices(IServiceCollection services)
-//{
-//builder.Services.AddControllers();
-
-//builder.Services.AddDbContext<OrderDbContext>(
-//        options => options.UseSqlServer(("Server=AMIGA\\SQLEXPRESS;Database=OrderDB;Trusted_Connection=True;")));
-
 builder.Services.AddDbContext<OrderDbContext>(
         options => options.UseSqlServer(builder.Configuration.GetConnectionString("OrderDatabase")));
-
-//}
-//builder.Services.AddDbContext<OrderDbContext>(sp => new SqlConnection("Server=AMIGA\\SQLEXPRESS;Database=OrderDB;Trusted_Connection=True;"));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
