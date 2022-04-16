99c70ebe9ef9d27ca3a787ffad6b96f8802b5a05
Viktor-Prokhorov <vicmmx@gmail.com>
2022-04-17 02:13:14 +0300

    6

--- WebApplication/WebApplication/Controllers/OrdersController.cs
+++ WebApplication/WebApplication/Controllers/OrdersController.cs
@@ -1,10 +1,13 @@
 ﻿#nullable disable
 using System.Data;
+using System.Drawing;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
 using WebApplication.Database;
 using WebApplication.Models;
-
+using System.IO;
 
 
 namespace WebApplication.Controllers
@@ -162,5 +165,36 @@ namespace WebApplication.Controllers
         {
             return _context.Orders.Any(e => e.Id == id);
         }
+
+
+        public void CreatePdf(long id)
+        {
+            var ind = _context.Orders
+                .AsEnumerable()
+                .TakeWhile(x => x.Id != id)
+                .Count();
+            Order ord = _context.Orders
+                 .AsEnumerable()
+                 .ElementAt(ind);
+            string manag=(ord.Manager).ToString();
+            string oper = (ord.Operator).ToString();
+            PdfDocument pdf = new PdfDocument();
+            PdfPageBase page = pdf.Pages.Add();
+            page.Canvas.DrawString(manag, new PdfTrueTypeFont(new Font("Arial", 20f), true), new PdfSolidBrush(Color.Black), 10, 10);
+            page.Canvas.DrawString(oper, new PdfTrueTypeFont(new Font("Arial", 20f), true), new PdfSolidBrush(Color.Black), 10, 25);
+
+            //pdf.LoadFromStream(Stream stream);
+            pdf.SaveToFile("c:\\Temp 2\\blank.pdf");
+            //pdf.SaveToFile("c:\\Temp 2\\" + Guid.NewGuid().ToString() + ".pdf");
+            //< asp:Button ID = "btnHtml2PDF" runat = "server" Text = "Create Pdf" OnClick = "btnHtml2PDF_Click" />
+
+            View(ord);
+        }
+
+
+
+
     }
+
+
 }
