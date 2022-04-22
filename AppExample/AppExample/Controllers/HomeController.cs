d82489c4e6e69b1e11d4263e5d421c2a5d42ec00
Dmytro Misik <d.misik@draftkings.com>
2022-04-22 20:37:31 +0300

    no deps example

---
+++ AppExample/AppExample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppExample.Controllers;
+
+public class HomeController : ControllerBase
+{
+    [HttpGet("/api/resource")]
+    public IActionResult Get()
+    {
+        var random = new Random();
+        var randomNumber = random.Next();
+        var randomNumber2 = random.Next();
+        return Ok(new
+        {
+            Mul = randomNumber * randomNumber2
+        });
+    }
+}
